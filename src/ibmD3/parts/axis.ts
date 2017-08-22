7e83fdb1ff95e6c887feaf769cf241748ae9a78b
Hailey Ryu <hailey@ca.ibm.com>
2017-08-22 10:19:30 -0400

    Added second y axis function.

--- src/ibmD3/parts/axis.ts
+++ src/ibmD3/parts/axis.ts
@@ -15,12 +15,38 @@ export namespace Axis {
 
 		g.append("text")
 		  .attr("fill", "#586464")
-		  .attr("transform", "translate(-"+ 80 +","+(options.height/2 - 10)+")rotate(-90)")
+		  .attr("transform", "translate(-80,"+(options.height/2 - 40)+")rotate(-90)")
 		  .attr("dy", "0.71em")
 		  .attr("text-anchor", "middle")
 		  .attr("class", "y axis-label")
 		  .text(options.yDomain.join(", "));
 	}
+
+  export function drawY2Axis(svg, yScale, options, data) {
+  	console.log(" - - - -- -", svg, yScale, options, data)
+  	svg.append("g")
+  		.attr("class", "y2 axis")
+		let maxWidth = 0;
+		const yAxis = d3.axisRight(yScale)
+			.tickSizeInner(0)
+			.tickSizeOuter(0)
+			.tickPadding(10)
+			.ticks(options.y2Ticks);
+		let g = svg.select(".y2.axis")
+			.attr("transform", "translate("+ options.chartSize.width + ", 0)")
+			.call(yAxis);
+		g.select(".domain").remove();
+
+		g.append("text")
+		  .attr("fill", "#586464")
+		  .attr("transform", "translate(80,"+(options.chartSize.height/2)+")rotate(90)")
+		  .attr("dy", "0.71em")
+		  .attr("text-anchor", "middle")
+		  .attr("class", "y2 axis-label")
+		  .text(options.y2Domain.join(", "));
+		  console.log(g)
+		}
+
   export function drawXAxis(svg, xScale, options, data) {
 		let xAxis = d3.axisBottom(xScale)
 			.tickSizeInner(0)
