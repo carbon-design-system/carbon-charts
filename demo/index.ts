6cb924507b09da59709e93705f3c437d11cb25d8
Hailey Ryu <hailey@ca.ibm.com>
2017-09-19 16:01:31 -0400

    Cleaned demo index file.

--- demo/index.ts
+++ demo/index.ts
@@ -1,6 +1,5 @@
 
 import './index.scss'
-import * as d3 from 'd3'
 import {Charts} from '../src/charts/index.ts'
 import {Combo} from '../src/charts/types/combo.ts'
 import {DoubleAxis} from '../src/charts/types/doubleAxis.ts'
@@ -240,49 +239,12 @@ const chartTypes = [
 	}
 ];
 
-// let typeSelections = d3.select('body').insert('ul', 'div').classed('chart-type-selection', true);
-// chartTypes.forEach(type => {
-// 	d3.select('.chart-type-selection').append('li').attr('id', type.id).text(type.name)
-
-// 	d3.select("#" + type.id)
-// 		.classed("disabled", !type.avail)
-// 		.on('click', () => {
-// 			let btn = d3.select("#" + type.id)
-// 			if (!btn.classed("disabled")) {
-// 				d3.selectAll(".chart-type-selection li")
-// 				  .classed("active", false);
-// 				btn.classed("active", !btn.classed("active"));
-// 			}
-// 			if (btn.classed('active')) {
-// 				Charts.removeChart(container)
-// 				options.type = d3.select(".chart-type-selection .active").attr("id");
-// 				// Charts.renderChart(data, container, options)
-// 				switch (type.id) {
-// 					case "bar":
-// 						Bars.drawChart(data, container, options);
-// 						break;
-// 					case "stackedBar":
-// 						StackedBars.drawChart(data, container, options);
-// 						break;
-// 					case "line":
-// 						Lines.drawChart(data, container, options);
-// 						break;
-// 					case "doubleAxis":
-// 						renderDoubleAxis(data, container, doubleYAxisOptions, "line");
-// 						break;
-// 					case "combo":
-// 						renderCombo(data, container, doubleYAxisOptions);
-// 						break;
-// 					default:
-// 						Bars.drawChart(data, container, options);
-// 						break;
-// 				}
-// 			}
-// 		})
-// })Object.assign({}, options, {yDomain: [options.yDomain[0]]});
-
 chartTypes.forEach(type => {
 	let container = document.getElementById(type.id + '-chart-holder')
+	const header = document.createElement("h2");
+	header.className = "chart-title";
+	header.appendChild(document.createTextNode(type.name));
+	container.appendChild(header);
 	switch (type.id) {
 		case "bar":
 			Bars.drawChart(type.data, container, Object.assign({}, type.options, {type: type.id}));
