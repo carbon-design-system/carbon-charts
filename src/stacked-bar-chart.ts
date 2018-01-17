62aff038a7668672a77b553d773cec79d419202d
Hailey Ryu <hailey@ca.ibm.com>
2018-01-17 15:19:15 -0500

    Check height of svg to check if it's ready to draw data ponits.

--- src/stacked-bar-chart.ts
+++ src/stacked-bar-chart.ts
@@ -39,6 +39,7 @@ export class StackedBarChart extends BarChart {
 
 	update() {
 		const yHeight = this.getActualChartSize().height - this.svg.select(".x.axis").node().getBBox().height;
+		if (yHeight > 0) {
 		const activeSeries = this.getActiveDataSeries();
 		const keys = activeSeries ? activeSeries : this.options.yDomain;
 		const color = d3.scaleOrdinal().range(this.options.colors).domain(this.options.yDomain);
@@ -84,54 +85,57 @@ export class StackedBarChart extends BarChart {
 			.attr("height", d => this.yScale(d.y0) - this.yScale(d.y1));
 		s.exit()
 			.remove();
+		}
 	}
 
 	draw() {
 		const yHeight = this.getActualChartSize().height - this.svg.select(".x.axis").node().getBBox().height;
-		const activeSeries = this.getActiveDataSeries();
-		const keys = activeSeries ? activeSeries : this.options.yDomain;
-		this.xScale.paddingInner(0.2);
-		const color = d3.scaleOrdinal().range(this.options.colors).domain(this.options.yDomain);
-		this.data.forEach(d => {
-			let y0 = 0;
-			const key = d[this.options.xDomain];
-			const xAxis = this.options.xDomain;
-			d.series = keys.map(seriesVal => {
-				return {
-					xAxis,
-					series: seriesVal,
-					key,
-					y0: y0,
-					y1: y0 += +d[seriesVal],
-					value: d[seriesVal],
-					formatter: this.options.yFormatter,
-					color: color(seriesVal)
-				};
+		if (yHeight > 0) {
+			const activeSeries = this.getActiveDataSeries();
+			const keys = activeSeries ? activeSeries : this.options.yDomain;
+			this.xScale.paddingInner(0.2);
+			const color = d3.scaleOrdinal().range(this.options.colors).domain(this.options.yDomain);
+			this.data.forEach(d => {
+				let y0 = 0;
+				const key = d[this.options.xDomain];
+				const xAxis = this.options.xDomain;
+				d.series = keys.map(seriesVal => {
+					return {
+						xAxis,
+						series: seriesVal,
+						key,
+						y0: y0,
+						y1: y0 += +d[seriesVal],
+						value: d[seriesVal],
+						formatter: this.options.yFormatter,
+						color: color(seriesVal)
+					};
+				});
+				d.total = this.options.yDomain.map(val => d[val]).reduce((acc, cur) => acc + cur, 0);
 			});
-			d.total = this.options.yDomain.map(val => d[val]).reduce((acc, cur) => acc + cur, 0);
-		});
 
-		const bars = this.svg.append("g").attr("class", "stacked-bars");
+			const bars = this.svg.append("g").attr("class", "stacked-bars");
 
-		const series = bars.selectAll(".series")
-			.data(this.data)
-			.enter()
-			.append("g")
-			.attr("class", "series")
-			.attr("transform", d => `translate(${this.xScale(d[this.options.xDomain])},0)`);
+			const series = bars.selectAll(".series")
+				.data(this.data)
+				.enter()
+				.append("g")
+				.attr("class", "series")
+				.attr("transform", d => `translate(${this.xScale(d[this.options.xDomain])},0)`);
 
-		series.selectAll("rect")
-			.data(d => d.series)
-			.enter()
-			.append("rect")
-			.attr("width", this.xScale.bandwidth())
-			.style("fill", d => d.color)
-			.attr("y", yHeight)
-			.attr("height", 0)
-			.transition()
-			.delay((d, i) => i * 30)
-			.ease(d3.easePolyOut, 0.5)
-			.attr("y", d => this.yScale(d.y1))
-			.attr("height", d => this.yScale(d.y0) - this.yScale(d.y1));
+			series.selectAll("rect")
+				.data(d => d.series)
+				.enter()
+				.append("rect")
+				.attr("width", this.xScale.bandwidth())
+				.style("fill", d => d.color)
+				.attr("y", yHeight)
+				.attr("height", 0)
+				.transition()
+				.delay((d, i) => i * 30)
+				.ease(d3.easePolyOut, 0.5)
+				.attr("y", d => this.yScale(d.y1))
+				.attr("height", d => this.yScale(d.y0) - this.yScale(d.y1));
+		}
 	}
 }
