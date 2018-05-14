a5938bc625cdb90df88f506d78850a1f4a579f2e
Eliad Moosavi <iliadm@ca.ibm.com>
2018-05-14 13:37:53 -0400

    "Should work" test case for ComboChart

---
+++ src/combo-chart.spec.ts
@@ -0,0 +1,89 @@
+import { ComboChart } from "./index";
+import { createClassyContainer, grabClassyContainer, mainSVGSelector, colors } from "./test-tools";
+
+const chartType = "combo";
+describe("Combo Chart", () => {
+	beforeEach(() => {
+		// Append the chart container to DOM
+		const classyContainer = createClassyContainer(chartType);
+		document.body.appendChild(classyContainer);
+
+		const data = [
+			{
+				"Part number": "2V2N-9KYPM",
+				"Total": 100000,
+				"Returned": 60000,
+				"Defects": 9230,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 90000
+			},
+			{
+				"Part number": "L22I-P66EP",
+				"Total": 200000,
+				"Returned": 50000,
+				"Defects": 9230,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 70000
+			},
+			{
+				"Part number": "JQAI-2M4L1",
+				"Total": 100000,
+				"Returned": 9000,
+				"Defects": 2980,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 6000
+			},
+			{
+				"Part number": "J9DZ-F37AP",
+				"Total": 150000,
+				"Returned": 8000,
+				"Defects": 12230,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 11000
+			},
+			{
+				"Part number": "Q6XK-YEL48",
+				"Total": 230000,
+				"Returned": 4000,
+				"Defects": 8230,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 300000
+			},
+			{
+				"Part number": "773C-XKB5L",
+				"Total": 390000,
+				"Returned": 35000,
+				"Defects": 5230,
+				"This is an extra long long long legend": 12345,
+				"Number of sold products": 190000
+			}
+		];
+
+		const options = {
+			xDomain: "Part number",
+			yDomain: ["Total"],
+			y2Domain: ["Returned", "Defects", "This is an extra long long long legend", "Number of sold products"],
+			yTicks: 5,
+			y2Ticks: 10,
+			legendClickable: true,
+			containerResizable: true,
+			colors
+		};
+
+		// Instantiate chart object & draw on DOM
+		const classyComboChart = new ComboChart(
+			classyContainer,
+			Object.assign({}, options, {type: chartType}),
+			data
+		);
+		classyComboChart.drawChart();
+	});
+
+	it("Should work", () => {
+		// Grab chart container in DOM
+		const classyContainer = grabClassyContainer(chartType);
+
+		// Expect chart container to contain the main chart SVG element
+		expect(classyContainer.querySelector(mainSVGSelector)).toBeTruthy();
+	});
+});
