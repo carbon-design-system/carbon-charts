096b004fe61c4768f2bee958f505a262cd218850
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-09-24 12:34:18 -0400

    chore(angular react core): prepare packages for lerna hoist

--- packages/angular/demo/.storybook/addons.js
+++ packages/angular/demo/.storybook/addons.js
@@ -1,5 +1,1 @@
-// import '@storybook/addon-actions/register';
-// import '@storybook/addon-links/register';
-// import '@storybook/addon-notes/register';
-// import '@storybook/addon-knobs/register'
 import '@storybook/addon-options/register';
