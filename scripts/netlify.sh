c889443fac17d28594c660d91dc9cc70aa75c23d
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-11-20 13:47:25 -0500

    Fix incorrect script paths in Netlify scripts

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,5 +1,4 @@
-../.travis/before_install
-npm run postintall
+./.travis/before_install.sh
 
 # This script builds all package bundles (for NPM) & demos (for gh-pages)
-../scripts/build-packages-and-demos.sh
+./scripts/build-packages-and-demos.sh
