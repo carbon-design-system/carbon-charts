a4b9ba293ee9c9c4a734c9f74029b0dc85ded7eb
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-11-20 12:03:17 -0500

    Move all package build & demo scripts into 1 file

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -23,33 +23,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
 
-  # Generate all assets
-  # needed for push to gh-page
-  mkdir pages
-  touch pages/.nojekyll
-  echo "charts.carbondesignsystem.com" > pages/CNAME
-
-  # Build Core demos and copy to `pages` directory
-  cd packages/core
-  npm run build
-  npm run demo:build
-  typedoc --out ./demo/bundle/documentation ./src/index.ts
-  cp -a demo/bundle/. ../../pages
-
-  # Build Angular demos and copy to `pages` directory
-  cd ../angular/demo
-  npm run build-storybook
-  cp -a storybook-dist/. ../../../pages/angular
-  cd ..
-  rm -rf demo
-
-  # Build React demos
-  cd ../react
-  npm run build-storybook
-  cp -a storybook-dist/. ../../pages/react
-
-  # Go back to project root folder
-  cd ../..
+  ../scripts/build-packages-and-demos.sh
   
   # Perform git & npm publish
   git update-index --assume-unchanged `git diff --name-only`
