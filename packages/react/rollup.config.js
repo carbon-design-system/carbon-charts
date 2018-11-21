fe7324cc47337ee0bdc01339741511c241718f0a
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-11-20 23:46:20 -0500

    chore(react): Use rollup for bundling rather than webpack

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,16 @@
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+
+module.exports = {
+    input: "./src/index.js",
+    output: {
+        file: "./dist/bundle.js",
+        format: "cjs" 
+    },
+    plugins: [
+        babel({
+            exclude: "node_modules/**"
+        }),
+        commonjs()
+    ],
+};
--- packages/react/src/line-chart.js
+++ packages/react/src/line-chart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import { LineChart as LC } from "@carbon/charts";
 import BaseChart from "./base-chart";
