dc5408be355b264f0833efce0f34d2921690ea17
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-11-30 14:06:49 -0500

    Code style fixess

--- packages/angular/index.js
+++ packages/angular/index.js
@@ -1,3 +1,2 @@
-// export all modules from an entrypoint
-// To be used with ts-loader & angular loaders in projects
+// export all modules from the bundle
 export * from "./dist/bundles/carbon-charts-angular.umd.min.js";
