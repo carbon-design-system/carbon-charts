23cda435c74f0a02c3da96ccb94e383e8fc6cf15
Eliad Moosavi <iliadmoosavi@gmail.com>
2018-12-03 11:39:54 -0500

    Use correct bash syntax

--- scripts/build-packages-and-demos.sh
+++ scripts/build-packages-and-demos.sh
@@ -9,13 +9,13 @@ cd packages/core
 npm run build
 npm run demo:build
 typedoc --out ./demo/bundle/documentation ./src/index.ts
-cp -a demo/bundle/* ../../pages
+cp -a demo/bundle/. ../../pages
 
 # Build Angular demos and copy to `pages` directory
 cd ../angular
 # Build angular demos
 npm run build-storybook
-cp -a storybook-dist/* ../../pages/angular
+cp -a storybook-dist/. ../../pages/angular
 # Build angular bundle for release
 npm run build
 mv dist ../charts-angular-dist
