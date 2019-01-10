2b4ffebe9a8c93f8af3734b0de8568316eafebfa
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-01-09 22:23:21 -0500

    Fix lint issues

--- packages/core/src/pie-chart.spec.ts
+++ packages/core/src/pie-chart.spec.ts
@@ -71,8 +71,8 @@
 // 		const classyContainer = grabClassyContainer(chartType);
 
 		// setTimeout(() => {
-        //     // (Configuration.pie.sliceLimit + 1) because of the auto-generated
-        //     // "Other" slice when (# of datapoints > Configuration.pie.sliceLimit)
+			//     // (Configuration.pie.sliceLimit + 1) because of the auto-generated
+			//     // "Other" slice when (# of datapoints > Configuration.pie.sliceLimit)
 // 			expect(getNumberOfSlices(classyContainer)).toBeLessThanOrEqual(Configuration.pie.sliceLimit + 1);
 // 		});
 // 	});
