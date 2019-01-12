762e58a8285790271e433cba8c500b16831bb930
Justin Licari <Justin@Justins-MacBook-Air.local>
2019-01-12 17:22:14 -0500

    create aria labels for each chart data element with the x value, y value, and dataset

--- packages/core/src/bar-chart.ts
+++ packages/core/src/bar-chart.ts
@@ -108,7 +108,9 @@ export class BarChart extends BaseAxisChart {
 						.attr("stroke-width", Configuration.bars.default.strokeWidth)
 						.attr("stroke-opacity", d => this.options.accessibility ? 1 : 0)
 						// Make the data element accessible and pass x and y values for the data point, and the dataset label
-						.call(this.makeAccessible, d => "Label: " + this.x1(d.datasetLabel).toString() + ", Value: " + this.y(Math.max(0, d.value)).toString() + ", belongs to " + d.datasetLabel, d => this.y(Math.max(0, d.value)));
+						.call(this.makeAccessible, d => "Label: " + this.x1(d.datasetLabel).toString()
+						+ ", Value: " + this.y(Math.max(0, d.value)).toString()
+						+ ", belongs to " + d.datasetLabel, d => this.y(Math.max(0, d.value)));
 
 		// Hide the overlay
 		this.updateOverlay().hide();
