4698efd690f48a06b8a7466e5ce4cebb80258f17
Justin Licari <Justin@Justins-MacBook-Air.local>
2019-02-16 11:56:49 -0500

    fix($browser): Fix duplication of titles during title resize
    Only allow a single y title for every axis chart

--- packages/core/src/base-axis-chart.ts
+++ packages/core/src/base-axis-chart.ts
@@ -397,6 +397,7 @@ export class BaseAxisChart extends BaseChart {
 				.attr("transform", `rotate(-90) translate(${yAxisTitleTranslate.x}, ${yAxisTitleTranslate.y})`)
 				.attr("text-align", "center")
 				.text(this.options.scales.y.title);
+
 			this.svg.attr("transform", `translate(${this.innerWrap.select(".axis-label.y").node().getBBox().height}, 0)`);
 		}
 
