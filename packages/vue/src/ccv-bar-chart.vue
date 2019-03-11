eb5358bbca8335c340acea39b40391a7267f4131
moores2 <>
2019-03-11 15:44:53 +0000

    Initial Vue.js wrapper and story for the bar chart component.

---
+++ packages/vue/src/ccv-bar-chart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ccv-bar-chart"></div>
+</template>
+
+<script>
+  import { BarChart } from '@carbon/charts';
+  import CcvBaseChart from './ccv-base-chart.vue';
+
+  export default {
+    name: 'CcvBarChart',
+    extends: CcvBaseChart,
+
+    mounted() {
+      this.coreChart = new BarChart(
+        this.$el,
+        {
+          data: this.data,
+          options: this.options
+        }
+      );
+    },
+  };
+</script>
