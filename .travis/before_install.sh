fb512cbd538a7d2506ade7fd4e394314dd8463af
Callum Smith <1744185+cal-smith@users.noreply.github.com>
2019-03-26 10:07:17 -0400

    fix(build): refactor deploy and update package.jsons  (#223)
    * fix(build): refactor deploy and update package.jsons
    
    * add build-demos to try and stay within netlifys time limit
    
    * have netlify build-all-demos instead of build-all

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
-#!/bin/bash
 
-# Note: do not do set -x or the passwords will leak!
-
-# @angular/cli should be removed when the angular dmeo project has been removed
-npm install -g lerna@3.2.1 @storybook/cli @angular/cli rollup@0.67.3
+npm install -g lerna@3.2.1
