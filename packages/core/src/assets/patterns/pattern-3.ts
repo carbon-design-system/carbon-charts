9fb486c0763281e22e0187c324a433312bc5cfd1
Cal Smith <calsmith@protonmail.com>
2019-03-26 15:13:18 -0400

    fix(core): remove suffixes to use regular module resolution

--- packages/core/src/assets/patterns/pattern-3.ts
+++ packages/core/src/assets/patterns/pattern-3.ts
@@ -5,12 +5,18 @@ export default `<?xml version="1.0" encoding="utf-8"?>
 <style type="text/css">
 	.st0{fill:none;}
 	.st1{fill:#231F20;}
-	.st2{fill:url(#Square_diamond);}
+	.st2{fill:url(#Triangle);}
 </style>
-<pattern  width="40" height="40" patternUnits="userSpaceOnUse" id="Square_diamond" viewBox="0 -40 40 40" style="overflow:visible;">
+<pattern  width="100" height="100" patternUnits="userSpaceOnUse" id="Triangle" viewBox="15 -112.99 100 100" style="overflow:visible;">
 	<g id="XMLID_10_">
-		<polygon id="XMLID_14_" class="st0" points="0,-40 40,-40 40,0 0,0 		"/>
-		<path id="XMLID_11_" class="st1" d="M5-35v30h30v-30H5z M20-9.393L9.393-20L20-30.607L30.607-20L20-9.393z"/>
+		<polygon id="XMLID_23_" class="st0" points="15,-112.99 115,-112.99 115,-12.99 15,-12.99 		"/>
+		<polygon id="XMLID_21_" class="st1" points="100,-25 115,-50.981 130,-25 		"/>
+		<polygon id="XMLID_20_" class="st1" points="50,0 65,-25.981 80,0 		"/>
+		<polygon id="XMLID_19_" class="st1" points="0,-25 15,-50.981 30,-25 		"/>
+		<polygon id="XMLID_15_" class="st1" points="100,-75 115,-100.981 130,-75 		"/>
+		<polygon id="XMLID_14_" class="st1" points="50,-50 65,-75.981 80,-50 		"/>
+		<polygon id="XMLID_13_" class="st1" points="0,-75 15,-100.981 30,-75 		"/>
+		<polygon id="XMLID_12_" class="st1" points="50,-100 65,-125.981 80,-100 		"/>
 	</g>
 </pattern>
 <rect id="XMLID_1_" class="st2" width="100" height="100"/>
--- packages/core/src/assets/patterns/pattern-8.ts
+++ packages/core/src/assets/patterns/pattern-8.ts
@@ -5,19 +5,16 @@ export default `<?xml version="1.0" encoding="utf-8"?>
 <style type="text/css">
 	.st0{fill:none;}
 	.st1{fill:#231F20;}
-	.st2{fill:url(#hex_pattern);}
+	.st2{fill:url(#tight_diamond);}
 </style>
-<pattern  width="100" height="100"
-patternUnits="userSpaceOnUse" id="hex_pattern" viewBox="20 -117.32 100 100" style="overflow:visible;">
-	<g id="XMLID_10_">
-		<polygon id="XMLID_23_" class="st0" points="20,-117.32 120,-117.32 120,-17.32 20,-17.32 		"/>
-		<polygon id="XMLID_21_" class="st1" points="110,-25 100,-42.32 110,-59.641 130,-59.641 140,-42.32 130,-25 		"/>
-		<polygon id="XMLID_20_" class="st1" points="60,0 50,-17.32 60,-34.641 80,-34.641 90,-17.32 80,0 		"/>
-		<polygon id="XMLID_19_" class="st1" points="10,-25 0,-42.32 10,-59.641 30,-59.641 40,-42.32 30,-25 		"/>
-		<polygon id="XMLID_15_" class="st1" points="110,-75 100,-92.32 110,-109.641 130,-109.641 140,-92.32 130,-75 		"/>
-		<polygon id="XMLID_14_" class="st1" points="60,-50 50,-67.32 60,-84.641 80,-84.641 90,-67.32 80,-50 		"/>
-		<polygon id="XMLID_13_" class="st1" points="10,-75 0,-92.32 10,-109.641 30,-109.641 40,-92.32 30,-75 		"/>
-		<polygon id="XMLID_12_" class="st1" points="60,-100 50,-117.32 60,-134.641 80,-134.641 90,-117.32 80,-100 		"/>
+<pattern  width="70" height="45" patternUnits="userSpaceOnUse" id="tight_diamond" viewBox="21.213 -66.213 70 45" style="overflow:visible;">
+	<g id="XMLID_9_">
+		<polygon id="XMLID_16_" class="st0" points="21.213,-66.213 91.213,-66.213 91.213,-21.213 21.213,-21.213 		"/>
+		<polygon id="XMLID_15_" class="st1" points="91.213,0 70,-21.213 91.213,-42.427 112.427,-21.213 		"/>
+		<polygon id="XMLID_14_" class="st1" points="21.213,0 0,-21.213 21.213,-42.427 42.427,-21.213 		"/>
+		<polygon id="XMLID_13_" class="st1" points="91.213,-45 70,-66.213 91.213,-87.427 112.427,-66.213 		"/>
+		<polygon id="XMLID_12_" class="st1" points="56.213,-22.5 35,-43.713 56.213,-64.927 77.427,-43.713 		"/>
+		<polygon id="XMLID_11_" class="st1" points="21.213,-45 0,-66.213 21.213,-87.427 42.427,-66.213 		"/>
 	</g>
 </pattern>
 <rect id="XMLID_1_" class="st2" width="100" height="100"/>
