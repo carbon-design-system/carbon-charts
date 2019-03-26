14391ae9eb04caf072498f698db9433cbb021806
Callum Smith <1744185+cal-smith@users.noreply.github.com>
2019-03-26 14:33:55 -0400

    fix(core): export patterns as strings of svgs (#227)
    * fix(core): export patterns as strings of svgs
    
    * remove console log
    
    * add publishConfig to vue

--- packages/core/src/assets/patterns/index.ts
+++ packages/core/src/assets/patterns/index.ts
@@ -1,14 +1,14 @@
-const PATTERN_1 = require("./pattern-1.svg");
-const PATTERN_2 = require("./pattern-2.svg");
-const PATTERN_3 = require("./pattern-3.svg");
-const PATTERN_4 = require("./pattern-4.svg");
-const PATTERN_5 = require("./pattern-5.svg");
-const PATTERN_6 = require("./pattern-6.svg");
-const PATTERN_7 = require("./pattern-7.svg");
-const PATTERN_8 = require("./pattern-8.svg");
-const PATTERN_9 = require("./pattern-9.svg");
-const PATTERN_10 = require("./pattern-10.svg");
-const PATTERN_11 = require("./pattern-11.svg");
+const PATTERN_1 = require("./pattern-1.ts");
+const PATTERN_2 = require("./pattern-2.ts");
+const PATTERN_3 = require("./pattern-3.ts");
+const PATTERN_4 = require("./pattern-4.ts");
+const PATTERN_5 = require("./pattern-5.ts");
+const PATTERN_6 = require("./pattern-6.ts");
+const PATTERN_7 = require("./pattern-7.ts");
+const PATTERN_8 = require("./pattern-8.ts");
+const PATTERN_9 = require("./pattern-9.ts");
+const PATTERN_10 = require("./pattern-10.ts");
+const PATTERN_11 = require("./pattern-11.ts");
 
 export default [
 	PATTERN_1,
--- packages/core/src/assets/patterns/pattern-8.ts
+++ packages/core/src/assets/patterns/pattern-8.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+export default `<?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
@@ -7,7 +7,8 @@
 	.st1{fill:#231F20;}
 	.st2{fill:url(#hex_pattern);}
 </style>
-<pattern  width="100" height="100" patternUnits="userSpaceOnUse" id="hex_pattern" viewBox="20 -117.32 100 100" style="overflow:visible;">
+<pattern  width="100" height="100"
+patternUnits="userSpaceOnUse" id="hex_pattern" viewBox="20 -117.32 100 100" style="overflow:visible;">
 	<g id="XMLID_10_">
 		<polygon id="XMLID_23_" class="st0" points="20,-117.32 120,-117.32 120,-17.32 20,-17.32 		"/>
 		<polygon id="XMLID_21_" class="st1" points="110,-25 100,-42.32 110,-59.641 130,-59.641 140,-42.32 130,-25 		"/>
@@ -20,4 +21,4 @@
 	</g>
 </pattern>
 <rect id="XMLID_1_" class="st2" width="100" height="100"/>
-</svg>
+</svg>`;
