2e33ca64c50cdb1b403a73ce851476681999e4b9
Callum Smith <1744185+cal-smith@users.noreply.github.com>
2019-03-26 16:10:40 -0400

    fix(core): remove suffixes to use regular module resolution, and force publish all packages with every release (#229)
    * fix(core): remove suffixes to use regular module resolution
    
    * specify the correct config import path
    
    * add force publish to align all the versions
    
    * add descriptive comment

--- packages/core/src/assets/patterns/index.ts
+++ packages/core/src/assets/patterns/index.ts
@@ -1,25 +1,19 @@
-const PATTERN_1 = require("./pattern-1.ts");
-const PATTERN_2 = require("./pattern-2.ts");
-const PATTERN_3 = require("./pattern-3.ts");
-const PATTERN_4 = require("./pattern-4.ts");
-const PATTERN_5 = require("./pattern-5.ts");
-const PATTERN_6 = require("./pattern-6.ts");
-const PATTERN_7 = require("./pattern-7.ts");
-const PATTERN_8 = require("./pattern-8.ts");
-const PATTERN_9 = require("./pattern-9.ts");
-const PATTERN_10 = require("./pattern-10.ts");
-const PATTERN_11 = require("./pattern-11.ts");
+const PATTERN_1 = require("./pattern-1");
+const PATTERN_2 = require("./pattern-2");
+const PATTERN_3 = require("./pattern-3");
+const PATTERN_4 = require("./pattern-4");
+const PATTERN_5 = require("./pattern-5");
+const PATTERN_6 = require("./pattern-6");
+const PATTERN_7 = require("./pattern-7");
+const PATTERN_8 = require("./pattern-8");
 
 export default [
 	PATTERN_1,
 	PATTERN_2,
-	// PATTERN_3,
+	PATTERN_3,
 	PATTERN_4,
 	PATTERN_5,
-	// PATTERN_6,
+	PATTERN_6,
 	PATTERN_7,
-	PATTERN_8,
-	// PATTERN_9,
-	PATTERN_10,
-	PATTERN_11
+	PATTERN_8
 ];
