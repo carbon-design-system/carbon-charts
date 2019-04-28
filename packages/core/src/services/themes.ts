3b6d14ae2b5e33b52b10474267826df1702a3bf5
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-04-28 00:57:03 -0400

    Fix lint issues

--- packages/core/src/services/themes.ts
+++ packages/core/src/services/themes.ts
@@ -3,22 +3,22 @@ import colors from "./colors";
 // Generates array of color values
 // For all available colors within the shades provided
 const generateTheme = (firstShade, secondShade) => {
-    const firstPalette = [];
-    const secondPalette = [];
+	const firstPalette = [];
+	const secondPalette = [];
 
-    const colorKeys = Object.keys(colors);
-    for (let i = 0; i < 2; i++) {
-        colorKeys.forEach((colorKey, j) => {
-            const shadeToUse = (i + j) % 2 === 0 ? secondShade : firstShade;
-            console.log("shadeToUse", colorKey, shadeToUse)
-            firstPalette.push(
-                colors[colorKey](shadeToUse)
-            );
-        });
-    }
+	const colorKeys = Object.keys(colors);
+	for (let i = 0; i < 2; i++) {
+		colorKeys.forEach((colorKey, j) => {
+			const shadeToUse = (i + j) % 2 === 0 ? secondShade : firstShade;
+			console.log("shadeToUse", colorKey, shadeToUse);
+			firstPalette.push(
+				colors[colorKey](shadeToUse)
+			);
+		});
+	}
 
-    return firstPalette.concat(secondPalette);
-}
+	return firstPalette.concat(secondPalette);
+};
 
 export const HC_1 = generateTheme(80, 50);
 export const HC_2 = generateTheme(70, 40);
