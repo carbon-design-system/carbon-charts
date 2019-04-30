5caed2dddf0c53c13d1e5ec118f3d48f25209c01
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-04-29 21:00:53 -0400

    Fix lint issues

--- packages/core/src/components/overlay.ts
+++ packages/core/src/components/overlay.ts
@@ -1,35 +1,35 @@
 import * as Configuration from "../configuration";
 
 export class ChartOverlay {
-    holder: HTMLElement;
-    overlayElement: HTMLElement;
-    overlayOptions: Configuration.ChartOverlayOptions;
+	holder: HTMLElement;
+	overlayElement: HTMLElement;
+	overlayOptions: Configuration.ChartOverlayOptions;
 
 	constructor(holder: Element, options: Configuration.ChartOverlayOptions) {
-        this.holder = <HTMLElement>holder;
-        this.overlayElement = this.holder.querySelector("div.chart-overlay");
+		this.holder = <HTMLElement>holder;
+		this.overlayElement = this.holder.querySelector("div.chart-overlay");
 
-        if (options) {
-            this.overlayOptions = options;
-        } else {
-            this.overlayOptions = Configuration.options.BASE.overlay;
-        }
-    }
+		if (options) {
+			this.overlayOptions = options;
+		} else {
+			this.overlayOptions = Configuration.options.BASE.overlay;
+		}
+	}
 
-    show(type?: string) {
-        if (this.overlayElement) {
-            this.overlayElement.parentNode.removeChild(this.overlayElement);
-        }
+	show(type?: string) {
+		if (this.overlayElement) {
+			this.overlayElement.parentNode.removeChild(this.overlayElement);
+		}
 
-        const overlay = document.createElement("div");
+		const overlay = document.createElement("div");
 
-        overlay.classList.add("chart-overlay");
-        overlay.innerHTML = this.overlayOptions.innerHTML[type ? type : "loading"];
+		overlay.classList.add("chart-overlay");
+		overlay.innerHTML = this.overlayOptions.innerHTML[type ? type : "loading"];
 
-        this.overlayElement = this.holder.appendChild(overlay);
-    }
+		this.overlayElement = this.holder.appendChild(overlay);
+	}
 
-    hide() {
-        this.overlayElement.style.display = "none";
-    }
+	hide() {
+		this.overlayElement.style.display = "none";
+	}
 }
