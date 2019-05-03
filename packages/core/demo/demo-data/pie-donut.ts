b34a85340fc99f26b598b334f6cdc1e8acd2a6df
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-05-03 15:24:57 -0400

    fix(core): Fix tooltip positioning

--- packages/core/demo/demo-data/pie-donut.ts
+++ packages/core/demo/demo-data/pie-donut.ts
@@ -19,8 +19,8 @@ export const donutOptions = {
 };
 
 export const pieData = {
-	labels: ["2V2N-9KYPM version 1", "L22I-P66EP-L22I-P66EP-L22I-P66EP", "JQAI-2M4L1", "J9DZ-F37AP",
-		"YEL48-Q6XK-YEL48", "P66EP-L22I-L22I", "Q6XK-YEL48", "XKB5-L6EP", "YEL48-Q6XK", "L22I-P66EP-L22I"],
+	labels: ["2V2N 9KYPM version 1", "L22I P66EP L22I P66EP L22I P66EP", "JQAI 2M4L1", "J9DZ F37AP",
+		"YEL48 Q6XK YEL48", "P66EP L22I L22I", "Q6XK YEL48", "XKB5 L6EP", "YEL48 Q6XK", "L22I P66EP L22I"],
 	datasets: [
 		{
 			label: "Dataset 1",
