221b7421528a169c0a8298f3de3a4cf69e086c06
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-06-04 11:41:56 -0400

    Remove lerna postinstall hook

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,20 +1,20 @@
 # Netlify will run this anyways, this is to override the cache
 # yarn --offline
 
-# ./.travis/before_install.sh
-
-# Grab netlify app type from the netlify app URL
-# e.g. core, angular or react (translate to ./packages/NAME
-PKG_NAME=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
-
-if [ $PKG_NAME == "core" ]; then
-    PKG_TO_BUILD="@carbon/charts"
-else
-    PKG_TO_BUILD="@carbon/charts-$PKG_NAME"
-fi
+./.travis/before_install.sh
 
 if [ $CONTEXT == "deploy-preview" ]; then
     echo "We're in a PR preview"
+
+    # Grab netlify app type from the netlify app URL
+    # e.g. core, angular or react (translate to ./packages/NAME
+    PKG_NAME=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
+
+    if [ $PKG_NAME == "core" ]; then
+        PKG_TO_BUILD="@carbon/charts"
+    else
+        PKG_TO_BUILD="@carbon/charts-$PKG_NAME"
+    fi
 
     # create the folder we'll deploy in netlify
     mkdir -p pages
