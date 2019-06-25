fc2fbd7205ff9b8c6f16131a113ddda25e005119
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-06-25 15:10:45 -0400

    Update main bundle for react, and use umd bundles from core

--- packages/react/src/donut-chart.js
+++ packages/react/src/donut-chart.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { DonutChart as DC } from "@carbon/charts";
+import { DonutChart as DC } from "@carbon/charts/dist/index.umd.js";
 import BaseChart from "./base-chart";
 
 export default class DonutChart extends BaseChart {
