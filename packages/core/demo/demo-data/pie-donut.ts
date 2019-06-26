5502af98f4e20b38fd51e0a6af62046c66f9fdcd
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-06-26 19:31:39 -0400

    Resize the hoverArc on chart resize events

--- packages/core/demo/demo-data/pie-donut.ts
+++ packages/core/demo/demo-data/pie-donut.ts
@@ -20,12 +20,12 @@ export const donutOptions = {
 
 export const pieData = {
 	labels: ["2V2N 9KYPM version 1", "L22I P66EP L22I P66EP L22I P66EP", "JQAI 2M4L1", "J9DZ F37AP",
-		"YEL48 Q6XK YEL48", "P66EP L22I L22I", "Q6XK YEL48", "XKB5 L6EP", "YEL48 Q6XK", "L22I P66EP L22I"],
+		"YEL48 Q6XK YEL48", "P66EP L22I L22I"],
 	datasets: [
 		{
 			label: "Dataset 1",
 			backgroundColors: colors,
-			data: [70000, 40000, 90000, 50000, 60000, 45000, 90000, 70000, 80000, 120000]
+			data: [70000, 60000, 90000, 20000, 25000, 10000]
 		}
 	]
 };
