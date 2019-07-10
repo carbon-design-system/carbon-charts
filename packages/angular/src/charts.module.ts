5e8158fa2363aabbd60f625af2e32ba4e0c95f01
sparikh1594 <sparikh@us.ibm.com>
2019-07-10 11:57:15 -0700

    fix(Angular): Export base chart in Angular module

--- packages/angular/src/charts.module.ts
+++ packages/angular/src/charts.module.ts
@@ -22,6 +22,7 @@ import { LineChartComponent } from "./line-chart.component";
 		LineChartComponent
 	],
 	exports: [
+		BaseChart,
 		DonutChartComponent,
 		PieChartComponent,
 		BarChartComponent,
