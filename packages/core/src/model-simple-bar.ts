c22144dd90e72e0ec0786f0ed732ffdbaf947b20
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-09-29 00:08:56 -0400

    add legend filtering and hover listeners in simple bar

---
+++ packages/core/src/model-simple-bar.ts
@@ -0,0 +1,79 @@
+// Internal Imports
+import * as Configuration from "./configuration";
+import { ChartModel } from "./model";
+import { Tools } from "./tools";
+import { colorPalettes } from "./index";
+
+// D3 Imports
+import { scaleOrdinal } from "d3-scale";
+
+/** The charting model layer which includes mainly the chart data and options,
+ * as well as some misc. information to be shared among components */
+export class SimpleBarChartModel extends ChartModel {
+	generateDataLabels(newData) {
+		const dataLabels = {};
+		newData.labels.forEach(label => {
+			dataLabels[label] = Configuration.legend.items.status.ACTIVE;
+		});
+
+		return dataLabels;
+	}
+
+	getDisplayData() {
+		const { ACTIVE } = Configuration.legend.items.status;
+		const dataLabels = this.get("dataLabels");
+
+		if (this.get("data")) {
+			// Remove datasets that have been disabled
+			const displayData = Tools.clone(this.get("data"));
+			const dataset = displayData.datasets[0];
+
+			// Remove data values that correspond to labels that are disabled
+			dataset.data = dataset.data.filter((datum, i) => {
+				const label = displayData.labels[i];
+
+				return dataLabels[label] === ACTIVE;
+			});
+
+			// Remove labels that are disabled
+			displayData.labels = displayData.labels.filter(label => dataLabels[label] === ACTIVE);
+
+			return displayData;
+		}
+
+		return null;
+	}
+
+	/*
+	 * Fill scales
+	 *
+	*/
+	setColorScale() {
+		const dataset = this.getDisplayData().datasets[0];
+		if (dataset.backgroundColors) {
+			this.colorScale = scaleOrdinal().range(dataset.backgroundColors).domain(this.allDataLabels);
+		} else {
+			const colors = colorPalettes.DEFAULT;
+			this.colorScale = scaleOrdinal().range(colors).domain(this.allDataLabels);
+		}
+	}
+
+
+	getFillColor(label: string) {
+		const options = this.getOptions();
+		if (options.getFillColor) {
+			return options.getFillColor(label);
+		} else {
+			return this.getFillScale()(label);
+		}
+	}
+
+	getStrokeColor(label: string) {
+		const options = this.getOptions();
+		if (options.getStrokeColor) {
+			return options.getStrokeColor(label);
+		} else {
+			return this.colorScale(label);
+		}
+	}
+}
