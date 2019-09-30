01a82b9ff3890149560d6c3a312f9bb121be49a4
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-09-30 12:53:22 -0400

    switch to lodash merge

--- packages/core/src/tools.ts
+++ packages/core/src/tools.ts
@@ -1,3 +1,7 @@
+import {
+	merge as lodashMerge
+} from "lodash-es";
+
 // Functions
 export namespace Tools {
 	export function debounce(func, wait, immediate) {
@@ -43,26 +47,7 @@ export namespace Tools {
 	}
 
 	// custom deep object merge
-	export const merge = (target, ...objects) => {
-		for (const object of objects) {
-			for (const key in object) {
-				if (object.hasOwnProperty(key)) {
-					// since we're dealing relatively simple objects this should work fine
-					if (object[key] && typeof object[key] === "object") {
-						if (!target[key]) {
-							target[key] = {};
-						}
-						// recursively merge into the target
-						// configs only run 3 or 4 levels deep, so no stack explosions
-						target[key] = merge(target[key], object[key]);
-					} else {
-						target[key] = object[key];
-					}
-				}
-			}
-		}
-		return target;
-	};
+	export const merge = lodashMerge;
 
 	/**************************************
 	 *  DOM-related operations            *
