e0083f32db1af5c3e965ccb7e7cae6b925b7fbd9
Eliad Moosavi <theiliad@users.noreply.github.com>
2019-09-30 16:09:04 -0400

    Update index.js

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,11 +1,13 @@
 import PieChart from "./pie-chart";
 import DonutChart from "./donut-chart";
 import LineChart from "./line-chart";
+import ScatterChart from "./scatter-chart";
 import BarChart from "./bar-chart";
 
 export {
 	PieChart,
 	DonutChart,
 	LineChart,
+	ScatterChart,
 	BarChart
 };
