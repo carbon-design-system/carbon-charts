91d83ab97d298654d6d1a8ecea61d7fee4fc9edc
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-09-30 18:44:51 -0400

    add angular stories

---
+++ packages/angular/stories/step.stories.ts
@@ -0,0 +1,44 @@
+import { storiesOf } from "@storybook/angular";
+
+import { ChartsModule } from "../src/charts.module";
+
+import {
+	// Step
+	stepOptions,
+	stepData,
+	stepTimeSeriesOptions,
+	stepTimeSeriesData
+} from "@carbon/charts/demo/demo-data";
+import { addWidthAndHeight } from "./commons";
+
+const template = `
+<n-line-chart
+	class="n-chart"
+	[data]="data"
+	[options]="options"
+	#lineChart>
+</n-line-chart>
+`;
+
+const stories = storiesOf("Step", module);
+stories.add(stepOptions.title, () => ({
+	template,
+	moduleMetadata: {
+		imports: [ChartsModule]
+	},
+	props: {
+		data: stepData,
+		options: addWidthAndHeight(stepOptions)
+	}
+}));
+
+stories.add(stepTimeSeriesOptions.title, () => ({
+	template,
+	moduleMetadata: {
+		imports: [ChartsModule]
+	},
+	props: {
+		data: stepTimeSeriesData,
+		options: addWidthAndHeight(stepTimeSeriesOptions)
+	}
+}));
