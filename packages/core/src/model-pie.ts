0db1e6377687de03367742b11aa334a306c85428
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-01 19:09:19 -0400

    if disabling last active legend item, activate all

--- packages/core/src/model-pie.ts
+++ packages/core/src/model-pie.ts
@@ -54,9 +54,19 @@ export class PieChartModel extends ChartModel {
 		const dataLabels = this.get("dataLabels");
 
 		const hasDeactivatedItems = Object.keys(dataLabels).some(label => dataLabels[label] === DISABLED);
+		const activeItems = Object.keys(dataLabels).filter(label => dataLabels[label] === ACTIVE);
 		// If there are deactivated items, toggle "changedLabel"
 		if (hasDeactivatedItems) {
-			dataLabels[changedLabel] = dataLabels[changedLabel] === DISABLED ? ACTIVE : DISABLED;
+			// If the only active item is being toggled
+			// Activate all items
+			if (activeItems.length === 1 && activeItems[0] === changedLabel) {
+				// If every item is active, then enable "changedLabel" and disable all other items
+				Object.keys(dataLabels).forEach(label => {
+					dataLabels[label] = ACTIVE;
+				});
+			} else {
+				dataLabels[changedLabel] = dataLabels[changedLabel] === DISABLED ? ACTIVE : DISABLED;
+			}
 		} else {
 			// If every item is active, then enable "changedLabel" and disable all other items
 			Object.keys(dataLabels).forEach(label => {
