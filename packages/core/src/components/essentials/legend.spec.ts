902ec91584014aa8c3a185143b4b5f729026913e
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-01 19:31:15 -0400

    fix tests

--- packages/core/src/components/essentials/legend.spec.ts
+++ packages/core/src/components/essentials/legend.spec.ts
@@ -12,30 +12,24 @@ describe("legend component", () => {
 	});
 
 	describe("content", () => {
-		it("should have same amount of datasets", function(done) {
-			setTimeout(() => {
-				const data = this._testEnvironment.chartData;
-				const numberOfDatasets = data.datasets.length;
+		it("should have same amount of datasets", async function(done) {
+			const data = this._testEnvironment.chartData;
+			const numberOfDatasets = data.datasets.length;
 
-				const chartEventsFragment = this._chart.services.events.getDocumentFragment();
+			const chartEventsFragment = this._chart.services.events.getDocumentFragment();
 
-				const renderCb = () => {
-					// Remove render event listener
-					chartEventsFragment.removeEventListener("render-finished", renderCb);
-					
-					const numberOfLegendItems = select("g.cc-legend").selectAll("g.legend-item").size();
-					expect(numberOfLegendItems).toEqual(numberOfDatasets);
+			const renderCb = () => {
+				// Remove render event listener
+				chartEventsFragment.removeEventListener("render-finished", renderCb);
+				
+				const numberOfLegendItems = select("g.cc-legend").selectAll("g.legend-item").size();
+				expect(numberOfLegendItems).toEqual(numberOfDatasets);
 
-					done();
-				};
+				done();
+			};
 
-				// Add event listener for when chart render is finished
-				chartEventsFragment.addEventListener("render-finished", renderCb);
-			});
+			// Add event listener for when chart render is finished
+			chartEventsFragment.addEventListener("render-finished", renderCb);
 		});
-	});
-
-	afterEach(function() {
-		this._testEnvironment.destroy();
 	});
 });
