864798eaf73ec4f74ef60971c7e189c873ae20e3
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-08 15:05:11 -0400

    chore(core): change UMD bundle filename, and update @carbon/utils-position version

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,10 @@
 require("./polyfills");
 
+// Base classes
 import { BaseChart } from "./base-chart";
 import { BaseAxisChart } from "./base-axis-chart";
 
+// Chart components
 import { PieChart } from "./pie-chart";
 import { DonutChart, DonutCenter } from "./donut-chart";
 import { BarChart } from "./bar-chart";
@@ -10,11 +12,18 @@ import { LineChart } from "./line-chart";
 import { ComboChart } from "./combo-chart";
 import { ScatterChart } from "./scatter-chart";
 
+import * as Configurations from "./configuration";
+
 import * as colorPalettes from "./services/colorPalettes";
 // TODO 1.0 - Remove deprecated API
 const defaultColors = colorPalettes.DEFAULT;
 
+// Expose important ENUMs here
+const enums = {
+	ChartTheme: Configurations.ChartTheme
+};
 export {
+	enums,
 	defaultColors,
 	colorPalettes,
 	BaseChart,
