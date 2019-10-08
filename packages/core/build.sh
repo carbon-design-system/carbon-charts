541767a41d62629341c7c01500728a3fab91536a
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-08 16:33:03 -0400

    use --include-paths in node-sass for @carbon imports in scss

--- packages/core/build.sh
+++ packages/core/build.sh
@@ -7,4 +7,4 @@ tsc
 cp *.md dist/
 cp package.json dist/
 cp -a src/styles/. dist/styles
-node-sass dist/styles/styles.scss > dist/styles.css
+node-sass --include-path ../../node_modules dist/styles/styles.scss > dist/styles.css
