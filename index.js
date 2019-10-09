9f33c7968fab9fdfe9ee6e1f6741d88af40f3830
carbon-bot (from Travis CI) <carbon@us.ibm.com>
2019-10-09 19:53:21 +0000

    Deploy carbon-design-system/carbon-charts to github.com/carbon-design-system/carbon-charts.git:gh-pages

--- index.js
+++ index.js
@@ -3350,10 +3350,6 @@ var ChartTooltip = /** @class */ (function () {
         // Apply the event listeners to close the tooltip
         // setTimeout is there to avoid catching the click event that opened the tooltip
         setTimeout(function () {
-            // When ESC is pressed
-            if (typeof window !== "undefined") {
-                window.addEventListener("keydown", _this.handleTooltipEvents);
-            }
             // If clicked outside
             _this.container.addEventListener("click", _this.handleTooltipEvents);
             // Stop clicking inside tooltip from bubbling up to window
@@ -3363,10 +3359,6 @@ var ChartTooltip = /** @class */ (function () {
         }, 0);
     };
     ChartTooltip.prototype.removeEventLinteners = function () {
-        if (typeof window !== "undefined") {
-            // Remove eventlistener to close tooltip when ESC is pressed
-            window.removeEventListener("keydown", this.handleTooltipEvents);
-        }
         // Remove eventlistener to close tooltip when clicked outside
         this.container.removeEventListener("click", this.handleTooltipEvents);
     };
