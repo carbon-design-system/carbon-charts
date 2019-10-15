35b88832160bc4994a8c5f3c13943d848f8c9247
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-15 11:38:11 -0400

    code style optimizations

--- packages/react/src/scatter-chart.js
+++ packages/react/src/scatter-chart.js
@@ -14,12 +14,12 @@ export default class ScatterChart extends BaseChart {
 		);
 	}
 
-    render() {
-      return (
-		<div
-			ref={chartRef => this.chartRef = chartRef}
-			className="chart-holder">
-		</div>
-      )
-    }
+	render() {
+		return (
+			<div
+				ref={chartRef => this.chartRef = chartRef}
+				className="chart-holder">
+			</div>
+		);
+	}
 }
