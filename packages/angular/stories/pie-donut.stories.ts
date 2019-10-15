25b2cf9e4e723eb54b9d45a6ec44c1b0bdc76e88
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-15 10:49:49 -0400

    clean up demo site initialization logic

--- packages/angular/stories/pie-donut.stories.ts
+++ packages/angular/stories/pie-donut.stories.ts
@@ -13,12 +13,13 @@ import { addWidthAndHeight } from "./commons";
 
 const pieStories = storiesOf("Pie", module);
 pieStories.add(pieOptions.title, () => ({
-	template: `<ibm-pie-chart
-	class="n-chart"
-	[data]="data"
-	[options]="options"
-	#pieChart>
-</ibm-pie-chart>`,
+	template: `
+		<ibm-pie-chart
+			class="n-chart"
+			[data]="data"
+			[options]="options"
+			#pieChart>
+		</ibm-pie-chart>`,
 	moduleMetadata: {
 		imports: [ChartsModule]
 	},
@@ -30,12 +31,13 @@ pieStories.add(pieOptions.title, () => ({
 
 const donutStories = storiesOf("Donut", module);
 donutStories.add(donutOptions.title, () => ({
-	template: `<ibm-donut-chart
-	class="n-chart"
-	[data]="data"
-	[options]="options"
-	#donutChart>
-</ibm-donut-chart>`,
+	template: `
+		<ibm-donut-chart
+			class="n-chart"
+			[data]="data"
+			[options]="options"
+			#donutChart>
+		</ibm-donut-chart>`,
 	moduleMetadata: {
 		imports: [ChartsModule]
 	},
