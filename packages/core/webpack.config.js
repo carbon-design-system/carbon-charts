bc02012305a49feb56768b6f0cecea2966897d5b
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-10-28 15:26:19 -0400

    feat(core): date/time locale support + tick formatting

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 	},
 	resolve: {
 		// Add ".ts" and ".tsx" as a resolvable extension.
-		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".json", ".js"]
 	},
 	module: {
 		rules: [
