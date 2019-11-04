c87a5c0c6428ca127449649ecbb6200c8065386f
Eliad Moosavi <iliadmoosavi@gmail.com>
2019-11-04 14:24:05 -0500

    fix copy-demos-to-deploy-dir.sh script

--- scripts/copy-demos-to-deploy-dir.sh
+++ scripts/copy-demos-to-deploy-dir.sh
@@ -2,6 +2,6 @@
 # And will copy the demo folder of a package (e.g. angular/demo/bundle)
 # Into ($LERNA_ROOT_PATH/pages/angular)
 
-DEST_DIR=`echo $LERNA_PACKAGE_NAME | sed s/"@sterling\/charts"// | sed s/"-"//`
+DEST_DIR=`echo $LERNA_PACKAGE_NAME | sed s/"@cui\/charts"// | sed s/"-"//`
 
 cp -a demo/bundle/. $LERNA_ROOT_PATH/pages/$DEST_DIR
