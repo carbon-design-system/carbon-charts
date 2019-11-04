dc23934c367b0f420d86f31d9ffa1367755e10ee
Scott Dickerson <swiftfoot@hotmail.com>
2019-11-04 08:28:59 -0600

    fix(build-vendor): make conditional

--- packages/core/build-vendor.sh
+++ packages/core/build-vendor.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-cp -r ../../node_modules/@carbon/layout src/styles/vendor/@carbon
-cp -r ../../node_modules/@carbon/motion src/styles/vendor/@carbon
-cp -r ../../node_modules/@carbon/type src/styles/vendor/@carbon
-cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
-cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
+if [ -e ../../node_modules/@carbon/layout ]
+then
+	cp -r ../../node_modules/@carbon/layout src/styles/vendor/@carbon
+	cp -r ../../node_modules/@carbon/motion src/styles/vendor/@carbon
+	cp -r ../../node_modules/@carbon/type src/styles/vendor/@carbon
+	cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
+	cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
+fi
