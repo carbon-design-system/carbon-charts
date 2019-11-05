a2b876acb42ec9389a570fb5d13e550683149a1d
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2019-11-05 17:38:59 +0000

    chore: scaffold out logic for card functional component

--- packages/core/src/components/graphs/network.ts
+++ packages/core/src/components/graphs/network.ts
@@ -2,36 +2,43 @@
 import { Component } from "../component";
 
 // Add a network card and link class here initially? then move to another file
-
-/**
-class NetworkCard extends Component {
-	type = "network-card";
-
-	render() {
-
-	}
-} */
+const NetworkCards = ({svg, selector, data, accessor, height, width, fill, strokeColor}) => {
+	const cards = svg.selectAll(selector)
+			.data(data, accessor)
+			.enter()
+			.append("rect")
+			.classed("dataset", true)
+			.attr("height", height)
+			.attr("width", width)
+			.attr("focusable", true)
+			.attr("fill", fill)
+			.attr("stroke-width", "1px")
+			.attr("stroke", strokeColor);
+	return cards;
+};
 
 export class Network extends Component {
 	type = "network";
+
+	drawCards(svg) {
+		NetworkCards({
+			svg,
+			selector: "rect.network-card",
+			data: this.model.getDisplayData().datasets,
+			accessor: dataset => dataset.label,
+			height: 80,
+			width: 240,
+			fill: "#eeeeee",
+			strokeColor: "#000"
+		});
+	}
 
 	render(animate: boolean) {
 		// Define some mock data here
 		// Select all 'network-card'
 		// Enter, append
-
 		const svg = this.getContainerSVG();
+		this.drawCards(svg);
 
-		const cards = svg.selectAll("rect.network-card")
-			.data(this.model.getDisplayData().datasets, dataset => dataset.label)
-			.enter()
-			.append("rect")
-			.classed("dataset", true)
-			.attr("height", 80)
-			.attr("width", 240)
-			.attr("focusable", true)
-			.attr("fill", "#eeeeee")
-			.attr("stroke-width", "1px")
-			.attr("stroke", "#000");
 	}
 }
