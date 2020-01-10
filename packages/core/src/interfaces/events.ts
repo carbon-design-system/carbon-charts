284012ca7a95c20ffe50a7abd9c9636373e45c01
Eliad Moosavi <iliadmoosavi@gmail.com>
2020-01-10 14:57:04 -0500

    feat(core): add event dispatches for mouseover, mousemove, click & mouseout for all graphs

---
+++ packages/core/src/interfaces/events.ts
@@ -0,0 +1,29 @@
+/**
+ * enum of all pie graph events
+ */
+export enum Pie {
+	SLICE_MOUSEOVER = "pie-slice-mouseover",
+	SLICE_MOUSEMOVE = "pie-slice-mousemove",
+	SLICE_CLICK = "pie-slice-click",
+	SLICK_MOUSEOUT = "pie-slice-mouseout"
+}
+
+/**
+ * enum of all bar graph events
+ */
+export enum Bar {
+	BAR_MOUSEOVER = "bar-mouseover",
+	BAR_MOUSEMOVE = "bar-mousemove",
+	BAR_CLICK = "bar-click",
+	BAR_MOUSEOUT = "bar-mouseout"
+}
+
+/**
+ * enum of all scatter graph events
+ */
+export enum Scatter {
+	SCATTER_MOUSEOVER = "scatter-mouseover",
+	SCATTER_MOUSEMOVE = "scatter-mousemove",
+	SCATTER_CLICK = "scatter-click",
+	SCATTER_MOUSEOUT = "scatter-mouseout"
+}
