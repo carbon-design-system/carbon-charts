4323e9edf19d3a548970ad077f51a58bf4f5ad42
Ilaria Venturini <ilaria.venturini@accurat.it>
2020-02-17 16:29:43 +0100

    Timeseries axes: Move constants in a new file

--- packages/core/src/components/axes/utils.ts
+++ packages/core/src/components/axes/utils.ts
@@ -1,15 +1,5 @@
 import { min } from "d3-array";
-
-export const TIME_INTERVALS = [
-	["15seconds", 15 * 1000],
-	["minute", 60 * 1000],
-	["30minutes", 30 * 60 * 1000],
-	["hourly", 60 * 60 * 1000],
-	["daily", 24 * 60 * 60 * 1000],
-	["monthly", 30 * 24 * 60 * 60 * 1000],
-	["quarterly", 3 * 30 * 24 * 60 * 60 * 1000],
-	["yearly", 12 * 30 * 24 * 60 * 60 * 1000]
-];
+import { TIME_INTERVALS } from "./constants";
 
 // return true if the day of the month (D = 1-31) is changed, false otherwise
 function isDChanged(timestamp: number): boolean {
