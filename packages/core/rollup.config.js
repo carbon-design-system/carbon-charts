024a173e142f3c48269f54b9e2454faf6da11e21
Cal Smith <calsmith@protonmail.com>
2020-02-18 19:35:22 -0500

    fix(build): fix bundle output for core

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,9 +1,11 @@
+import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
-	input: "dist/index.js",
+	input: "./src/index.ts",
 	output:	{
 		file: `./dist/bundle.js`,
 		format: "umd",
@@ -17,13 +19,19 @@ export default {
 			"d3-interpolate": "d3Interpolate",
 			"d3-axis": "d3Axis",
 			"d3-array": "d3Array",
-			"d3-hierarchy": "d3Hierarchy"
+			"d3-hierarchy": "d3Hierarchy",
+			"d3-time-format": "d3TimeFormat"
 		}
 	},
 	plugins: [
-		terser(),
 		resolve(),
-		commonjs()
+		commonjs(),
+		json(),
+		typescript({
+			typescript: require("typescript"),
+			tsconfig: "./src/tsconfig.json"
+		}),
+		terser()
 	],
 	onwarn(warning, next) {
 		// logs the circular dependencies inside the d3 codebase
