f1fb4981a922a00f69807f2fe72266438e5def46
Luca Falasco <falasco.luca@gmail.com>
2020-03-03 16:00:25 +0100

    add more test data

--- packages/core/demo/data/line.ts
+++ packages/core/demo/data/line.ts
@@ -198,6 +198,204 @@ export const lineTimeSeriesOptions = {
 	curve: "curveMonotoneX"
 };
 
+export const lineTimeSeriesDenseData = {
+	labels: ["Qty", "More", "Sold", "Restocking", "Misc"],
+	datasets: [
+		{
+			label: "Dataset 1",
+			data: [
+				{
+					date: new Date(2019, 0, 1),
+					value: 10000
+				},
+				{
+					date: new Date(2019, 0, 1, 5),
+					value: 12000
+				},
+				{
+					date: new Date(2019, 0, 1, 10),
+					value: 14000
+				},
+				{
+					date: new Date(2019, 0, 2),
+					value: 25000
+				},
+				{
+					date: new Date(2019, 0, 2, 2),
+					value: 26000
+				},
+				{
+					date: new Date(2019, 0, 3),
+					value: 10000
+				},
+				{
+					date: new Date(2019, 0, 3, 5),
+					value: 10000
+				},
+				{
+					date: new Date(2019, 0, 3, 10),
+					value: 12000
+				},
+				{
+					date: new Date(2019, 0, 5),
+					value: 45000
+				},
+				{
+					date: new Date(2019, 0, 7),
+					value: 49000
+				},
+				{
+					date: new Date(2019, 0, 7, 15),
+					value: 45000
+				},
+				{
+					date: new Date(2019, 0, 9),
+					value: 50000
+				},
+				{
+					date: new Date(2019, 0, 9, 5),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 9, 15),
+					value: 55000
+				},
+				{
+					date: new Date(2019, 0, 10),
+					value: 50000
+				},
+				{
+					date: new Date(2019, 0, 12),
+					value: 65000
+				},
+				{
+					date: new Date(2019, 0, 13),
+					value: 80000
+				},
+				{
+					date: new Date(2019, 0, 14, 10),
+					value: 85000
+				},
+				{
+					date: new Date(2019, 0, 15, 7),
+					value: 90000
+				},
+				{
+					date: new Date(2019, 0, 15, 18),
+					value: 70000
+				}
+			]
+		},
+		{
+			label: "Dataset 2",
+			data: [
+				{
+					date: new Date(2019, 0, 1),
+					value: 20000
+				},
+				{
+					date: new Date(2019, 0, 1, 3),
+					value: 22000
+				},
+				{
+					date: new Date(2019, 0, 1, 16),
+					value: 24000
+				},
+				{
+					date: new Date(2019, 0, 2),
+					value: 35000
+				},
+				{
+					date: new Date(2019, 0, 2, 7),
+					value: 36000
+				},
+				{
+					date: new Date(2019, 0, 3),
+					value: 20000
+				},
+				{
+					date: new Date(2019, 0, 3, 6),
+					value: 20000
+				},
+				{
+					date: new Date(2019, 0, 3, 18),
+					value: 22000
+				},
+				{
+					date: new Date(2019, 0, 5),
+					value: 62000
+				},
+				{
+					date: new Date(2019, 0, 6),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 7),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 7, 15),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 7),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 7, 15),
+					value: 52000
+				},
+				{
+					date: new Date(2019, 0, 9),
+					value: 60000
+				},
+				{
+					date: new Date(2019, 0, 9, 5),
+					value: 62000
+				},
+				{
+					date: new Date(2019, 0, 9, 10),
+					value: 62000
+				},
+				{
+					date: new Date(2019, 0, 12),
+					value: 65000
+				},
+				{
+					date: new Date(2019, 0, 14),
+					value: 40000
+				},
+				{
+					date: new Date(2019, 0, 15, 5),
+					value: 45000
+				},
+				{
+					date: new Date(2019, 0, 15, 10),
+					value: 35000
+				},
+				{
+					date: new Date(2019, 0, 15, 18),
+					value: 30000
+				}
+			]
+		},
+	]
+};
+
+export const lineTimeSeriesDenseOptions = {
+	title: "Line (dense time series)",
+	axes: {
+		left: {
+			secondary: true
+		},
+		bottom: {
+			scaleType: "time",
+			primary: true
+		}
+	},
+	curve: "curveMonotoneX"
+};
+
 export const lineTimeSeriesDataRotatedTicks = {
 	labels: ["Qty"],
 	datasets: [
