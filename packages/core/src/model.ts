9a43bec79d7668e593bd24749c9d57fbfb43e9ce
Natasha DeCoste <natasha.decoste@gmail.com>
2020-03-27 10:55:43 -0400

    dispatch event on legend items update

--- packages/core/src/model.ts
+++ packages/core/src/model.ts
@@ -180,6 +180,11 @@ export class ChartModel {
 			});
 		}
 
+		// dispatch legend filtering event with the status of all the dataLabels
+		this.services.events.dispatchEvent("legend-items-update", {
+			dataLabels
+		});
+
 		// Update model
 		this.set({
 			dataLabels
