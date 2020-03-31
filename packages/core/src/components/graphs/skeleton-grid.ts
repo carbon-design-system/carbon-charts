82e5d0277cd856c8ee795c59807aa18acb31cd9d
Ilaria Venturini <ilaria.venturini@accurat.it>
2020-03-31 11:32:41 +0200

    Add horizontal grid to scatter

--- packages/core/src/components/graphs/skeleton-grid.ts
+++ packages/core/src/components/graphs/skeleton-grid.ts
@@ -4,7 +4,7 @@ import { Skeleton } from "./skeleton";
 import { DOMUtils } from "../../services";
 
 // D3 Imports
-import { axisBottom, scaleLinear } from "d3";
+import { axisBottom, axisLeft, scaleLinear } from "d3";
 
 export class SkeletonGrid extends Skeleton {
 	type = "skeleton-grid";
@@ -13,8 +13,6 @@ export class SkeletonGrid extends Skeleton {
 	yScale: any;
 
 	render() {
-		console.log(`%c skeleton-grid - init`, "background: #8934eb; color: #FFFFFF");
-
 		const data = this.model.getData();
 		const areDataEmpty = !dataExistsFn(data);
 
@@ -27,9 +25,7 @@ export class SkeletonGrid extends Skeleton {
 		}
 	}
 
-	renderSkeleton() {
-		console.log(`%c skeleton-grid - render`, "background: #3493eb; color: #FFFFFF");
-
+	renderSkeleton(animate = true) {
 		const svg = this.parent;
 		this.backdrop = DOMUtils.appendOrSelect(svg, "svg.chart-skeleton");
 
@@ -39,8 +35,6 @@ export class SkeletonGrid extends Skeleton {
 		this.yScale = scaleLinear().domain([0, 1]).range(yRange);
 		const [xScaleStart, xScaleEnd] = xRange;
 		const [yScaleEnd, yScaleStart] = yRange;
-		console.log(`  xScale: [${this.xScale.domain()}] -> [${this.xScale.range()}]`);
-		console.log(`  yScale: [${this.yScale.domain()}] -> [${this.yScale.range()}]`);
 
 		this.backdrop
 			.attr("x", xScaleStart)
@@ -51,26 +45,88 @@ export class SkeletonGrid extends Skeleton {
 		DOMUtils.appendOrSelect(this.backdrop, "g.x.grid-skeleton");
 		DOMUtils.appendOrSelect(this.backdrop, "g.y.grid-skeleton");
 
-		this.drawXGrid();
+		this.drawXGrid(animate);
+		this.drawYGrid(animate);
 	}
 
-	drawXGrid() {
+	drawXGrid(animate: boolean) {
 		const svg = this.parent;
 		const height = this.backdrop.attr("height");
 		const x = this.backdrop.attr("x");
-		const ticksNumber = 10;
+		const ticksNumber = 5;
+
+		// const xs = this.services.cartesianScales.getMainXScale().domain(["More", "b", "a", "c", "Sold", "Qty"]);
+		// const xGridGenerator = axisBottom(xs)
 
 		const xGridGenerator = axisBottom(this.xScale)
 			.tickSizeInner(-height)
 			.tickSizeOuter(0)
 			.ticks(ticksNumber);
 
-		const xGrid = svg.select(".x.grid-skeleton")
-			.attr("transform", `translate(${-x}, ${height})`)
-			.call(xGridGenerator);
+		const xGridG = svg
+			.select(".x.grid-skeleton")
+			.attr("transform", `translate(${-x}, ${height})`);
 
-		xGrid.selectAll("text").remove();
-		xGrid.selectAll("path").remove();
+		if (animate) {
+			const transition = this.services.transitions.getTransition("skeleton-update");
+			xGridG.transition(transition)
+				.call(xGridGenerator);
+		} else {
+			xGridG.call(xGridGenerator);
+		}
+
+		this.setStyle(xGridG);
+		this.clean(xGridG, 0);
+	}
+
+	drawYGrid(animate: boolean) {
+		const svg = this.parent;
+		const width = this.backdrop.attr("width");
+		const y = this.backdrop.attr("y");
+		const ticksNumber = 4;
+
+		const yGridGenerator = axisLeft(this.yScale)
+			.tickSizeInner(-width)
+			.tickSizeOuter(0)
+			.ticks(ticksNumber);
+
+		const yGridG = svg
+			.select(".y.grid-skeleton")
+			.attr("transform", `translate(0, ${-y})`);
+
+		if (animate) {
+			const transition = this.services.transitions.getTransition("skeleton-update");
+			yGridG.transition(transition)
+				.call(yGridGenerator);
+		} else {
+			yGridG.call(yGridGenerator);
+		}
+
+		this.setStyle(yGridG);
+		this.clean(yGridG, 10);
+	}
+
+	setStyle(holder: any) {
+		const options = this.model.getOptions();
+		// TODO: get the right option that, for now, it doesn't exist
+		const strokeColor = options.grid.strokeColor ? options.grid.strokeColor : "red";
+		holder
+			.selectAll("line")
+			.attr("stroke", strokeColor);
+	}
+
+	// remove unnecessary elements like:
+	// 	- ticks labels
+	//  - axis path
+	// 	- line of index idxToRemove because it stays over the axis
+	clean(holder: any, idxToRemove: number) {
+		holder.select("path")
+			.remove();
+		holder.selectAll("text")
+			.remove();
+		// holder.selectAll(".tick")
+		// 	.filter((d, i) => i === idxToRemove)
+		// 	.remove();
 	}
 
 	removeSkeleton() {
