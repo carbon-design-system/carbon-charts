faa24945988c6c8e49989f875ad3230d73a898ec
Natasha DeCoste <natasha.decoste@gmail.com>
2020-04-07 19:19:00 -0400

    reset demo data

--- packages/core/demo/data/pie.ts
+++ packages/core/demo/data/pie.ts
@@ -1,5 +1,5 @@
 export const pieData = [
-	{ group: "2V2N 9KYPM version 1", value: 21000 },
+	{ group: "2V2N 9KYPM version 1", value: 20000 },
 	{ group: "L22I P66EP L22I P66EP L22I P66EP", value: 65000 },
 	{ group: "JQAI 2M4L1", value: 75000 },
 	{ group: "J9DZ F37AP", value: 1200 },
