7b2416353893959f357bbe44934fc103907e66b7
Eliad Moosavi <iliadmoosavi@gmail.com>
2020-04-13 14:13:22 -0400

    optimize axis tick formatting

--- packages/core/src/components/axes/axis.ts
+++ packages/core/src/components/axes/axis.ts
@@ -103,6 +103,7 @@ export class Axis extends Component {
 		fakeTick.remove();
 
 		const isTimeScaleType = this.scaleType === ScaleTypes.TIME || axisOptions.scaleType === ScaleTypes.TIME;
+		const scaleType = this.scaleType || axisOptions.scaleType || ScaleTypes.LINEAR;
 
 		// Initialize axis object
 		const axis = axisFunction(scale).tickSizeOuter(0);
@@ -139,19 +140,33 @@ export class Axis extends Component {
 
 				axis.tickValues(tickValues);
 			}
+		}
 
-			// create the right ticks formatter
-			let formatter;
-			if (isTimeScaleType) {
-				const timeInterval = computeTimeIntervalName(axis.tickValues());
+		// create the right ticks formatter
+		let formatter;
+		const userProvidedFormatter = Tools.getProperty(axisOptions, "ticks", "formatter");
+		if (isTimeScaleType) {
+			const timeInterval = computeTimeIntervalName(axis.tickValues());
+			if(userProvidedFormatter === null) {
 				formatter = (t: number, i: number) => formatTick(t, i, timeInterval, timeScaleOptions);
 			} else {
-				formatter = Tools.getProperty(axisOptions, "ticks", "formatter");
+				formatter = (t: number, i: number) => {
+					const defaultFormattedValue = formatTick(t, i, timeInterval, timeScaleOptions);
+					return userProvidedFormatter(defaultFormattedValue, i);
+				};
 			}
-
-			// Set ticks formatter
-			axis.tickFormat(formatter);
+		} else {
+			if (userProvidedFormatter === null) {
+				if (scaleType === ScaleTypes.LINEAR) {
+					formatter = t => t.toLocaleString();
+				}
+			} else {
+				formatter = userProvidedFormatter;
+			}
 		}
+
+		// Set ticks formatter
+		axis.tickFormat(formatter);
 
 		// Position and transition the axis
 		switch (axisPosition) {
