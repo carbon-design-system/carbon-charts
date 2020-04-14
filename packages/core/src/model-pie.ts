96673852522d5fca79d1df331f167ff5e6dd186c
Natasha DeCoste <natasha.decoste@gmail.com>
2020-04-14 15:34:04 -0400

    fix(core): fix pie model to ignore dataset name when converting to tabular

--- packages/core/src/model-pie.ts
+++ packages/core/src/model-pie.ts
@@ -11,6 +11,17 @@ export class PieChartModel extends ChartModel {
 	sanitize(data) {
 		const tabularData = this.getTabularData(data);
 
+		// if the data was changed to tabular format
+		// update the group to the key so the slices render with the correct tooltips and colors
+		if (data !== tabularData) {
+			// If a label was set for the overall dataset, reassign it to key value
+			tabularData.forEach(d => {
+				if (d.key && d.key !== d.group) {
+					d.group = d.key;
+				}
+			});
+		}
+
 		// Sort data based on value
 		return tabularData.sort((a, b) => b.value - a.value);
 	}
