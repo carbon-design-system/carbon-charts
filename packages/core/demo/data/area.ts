e04316e5f813f145d967ec800ae65ff4aac6c6ea
Luca Falasco <falasco.luca@gmail.com>
2020-04-16 12:58:43 +0200

    update area data to tabular format

--- packages/core/demo/data/area.ts
+++ packages/core/demo/data/area.ts
@@ -1,83 +1,20 @@
-export const stackedAreaTimeSeriesData = {
-	labels: ["Qty", "More", "Sold", "Restocking", "Misc"],
-	datasets: [
-		{
-			label: "Dataset 1",
-			data: [
-				{
-					date: new Date(2019, 0, 1),
-					value: 10000
-				},
-				{
-					date: new Date(2019, 0, 5),
-					value: 65000
-				},
-				{
-					date: new Date(2019, 0, 8),
-					value: 10000
-				},
-				{
-					date: new Date(2019, 0, 13),
-					value: 49213
-				},
-				{
-					date: new Date(2019, 0, 17),
-					value: 51213
-				}
-			]
-		},
-		{
-			label: "Dataset 2",
-			data: [
-				{
-					date: new Date(2019, 0, 1),
-					value: 20000
-				},
-				{
-					date: new Date(2019, 0, 5),
-					value: 25000
-				},
-				{
-					date: new Date(2019, 0, 8),
-					value: 60000
-				},
-				{
-					date: new Date(2019, 0, 13),
-					value: 30213
-				},
-				{
-					date: new Date(2019, 0, 17),
-					value: 55213
-				}
-			]
-		},
-		{
-			label: "Dataset 3",
-			data: [
-				{
-					date: new Date(2019, 0, 1),
-					value: 30000
-				},
-				{
-					date: new Date(2019, 0, 5),
-					value: 20000
-				},
-				{
-					date: new Date(2019, 0, 8),
-					value: 40000
-				},
-				{
-					date: new Date(2019, 0, 13),
-					value: 60213
-				},
-				{
-					date: new Date(2019, 0, 17),
-					value: 25213
-				}
-			]
-		}
-	]
-};
+export const stackedAreaTimeSeriesData = [
+	{ group: "Dataset 1", date: new Date(2019, 0, 1), value: 10000 },
+	{ group: "Dataset 1", date: new Date(2019, 0, 5), value: 65000 },
+	{ group: "Dataset 1", date: new Date(2019, 0, 8), value: 10000 },
+	{ group: "Dataset 1", date: new Date(2019, 0, 13), value: 49213 },
+	{ group: "Dataset 1", date: new Date(2019, 0, 17), value: 51213 },
+	{ group: "Dataset 2", date: new Date(2019, 0, 1), value: 20000 },
+	{ group: "Dataset 2", date: new Date(2019, 0, 5), value: 25000 },
+	{ group: "Dataset 2", date: new Date(2019, 0, 8), value: 60000 },
+	{ group: "Dataset 2", date: new Date(2019, 0, 13), value: 30213 },
+	{ group: "Dataset 2", date: new Date(2019, 0, 17), value: 55213 },
+	{ group: "Dataset 3", date: new Date(2019, 0, 1), value: 30000 },
+	{ group: "Dataset 3", date: new Date(2019, 0, 5), value: 20000 },
+	{ group: "Dataset 3", date: new Date(2019, 0, 8), value: 40000 },
+	{ group: "Dataset 3", date: new Date(2019, 0, 13), value: 60213 },
+	{ group: "Dataset 3", date: new Date(2019, 0, 17), value: 25213 }
+];
 
 export const stackedAreaTimeSeriesOptions = {
 	title: "Area Stacked (time series)",
