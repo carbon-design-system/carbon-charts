24e49fc5f45c296caf2d1db9c46d93fc356c8f15
Eric Liu <ericyl.us@gmail.com>
2020-05-01 07:43:43 -0700

    Add Svelte wrappers (#569)
    * feat: add svelte wrappers
    
    * chore(svelte): format babelrc, rollup config
    
    * chore(deps-dev): lock @storybook/svelte version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(deps-dev): lock svelte version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(deps-dev): lock svelte-loader version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * docs(svelte): fill in Codesandbox section
    
    * chore(svelte): define maintainers, contributors in package.json
    
    * docs(svelte): add codesandbox
    
    * chore(svelte): update package version to 0.30.18
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(yarn): rebuild lockfile to fix errored ci
    
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

---
+++ packages/svelte/src/PieChart.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { PieChart } from "@carbon/charts";
+  import BaseChart from "./BaseChart.svelte";
+</script>
+
+<BaseChart {...$$restProps} Chart={PieChart} on:load on:update on:destroy />
