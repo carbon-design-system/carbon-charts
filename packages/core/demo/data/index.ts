63ec05c3f8cd387b2ad1ed96e8fcfda32862bc4d
Eric Liu <ericyl.us@gmail.com>
2020-05-01 07:43:43 -0700

    Add Svelte wrappers (#569)
    * feat: add svelte wrappers
    
    * chore(svelte): format babelrc, rollup config
    
    * chore(deps-dev): lock @storybook/svelte version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(deps-dev): lock svelte version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(deps-dev): lock svelte-loader version
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * docs(svelte): fill in Codesandbox section
    
    * chore(svelte): define maintainers, contributors in package.json
    
    * docs(svelte): add codesandbox
    
    * chore(svelte): update package version to 0.30.18
    
    Co-Authored-By: Eliad Moosavi <theiliad@users.noreply.github.com>
    
    * chore(yarn): rebuild lockfile to fix errored ci
    
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

--- packages/core/demo/data/index.ts
+++ packages/core/demo/data/index.ts
@@ -20,7 +20,8 @@ import {
 	createReactChartApp,
 	createAngularChartApp,
 	createVueChartApp,
-	createVanillaChartApp
+	createVanillaChartApp,
+	createSvelteChartApp
 } from "./create-codesandbox";
 
 import { Tools } from "@carbon/charts/tools";
@@ -368,6 +369,7 @@ allDemoGroups = allDemoGroups.map(demoGroup => {
 		demo.codesandbox.react = createChartSandbox(createReactChartApp(demo));
 		demo.codesandbox.vue = createChartSandbox(createVueChartApp(demo));
 		demo.codesandbox.vanilla = createChartSandbox(createVanillaChartApp(demo));
+		demo.codesandbox.svelte = createChartSandbox(createSvelteChartApp(demo));
 
 		if (!demo.code) {
 			demo.code = {};
---
+++ packages/svelte/src/index.js
@@ -0,0 +1,19 @@
+import BarChartGrouped from "./BarChartGrouped.svelte";
+import BarChartSimple from "./BarChartSimple.svelte";
+import BarChartStacked from "./BarChartStacked.svelte";
+import BubbleChart from "./BubbleChart.svelte";
+import DonutChart from "./DonutChart.svelte";
+import LineChart from "./LineChart.svelte";
+import PieChart from "./PieChart.svelte";
+import ScatterChart from "./ScatterChart.svelte";
+
+export {
+	BarChartGrouped,
+	BarChartSimple,
+	BarChartStacked,
+	BubbleChart,
+	DonutChart,
+	LineChart,
+	PieChart,
+	ScatterChart
+};
