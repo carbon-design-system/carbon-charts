7066dc2c8765cd4b2d2d2dc1e4fd5e615b7c8995
Ilaria Venturini <ilaria.venturini@accurat.it>
2020-05-06 11:14:38 +0200

    Radar: fix tooltip data order

--- packages/core/src/components/essentials/tooltip-radar.ts
+++ packages/core/src/components/essentials/tooltip-radar.ts
@@ -3,15 +3,15 @@ import { Tools } from "../../tools";
 
 export class TooltipRadar extends Tooltip {
 	getMultilineTooltipHTML(data: any) {
+		const options = this.model.getOptions();
+		const { groupMapsTo } = options.data;
+		const { angle, value } = options.radar.axes;
+
 		// sort them so they are in the same order as the graph
-		data.sort((a, b) => b.value - a.value);
+		data.sort((a, b) => b[value] - a[value]);
 
 		return  "<ul class='multi-tooltip'>" +
 			data.map(datum => {
-				const options = this.model.getOptions();
-				const { groupMapsTo } = options.data;
-				const { angle, value } = options.radar.axes;
-
 				const userProvidedValueFormatter = Tools.getProperty(this.model.getOptions(), "tooltip", "valueFormatter");
 				const formattedValue = userProvidedValueFormatter
 					? userProvidedValueFormatter(datum[value])
