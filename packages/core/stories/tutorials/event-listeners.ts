e0bb54486cc60983b747a388145b43c6dd4361f0
Natasha DeCoste <natasha.decoste@gmail.com>
2020-05-08 10:29:35 -0400

    add example for event listening

--- packages/core/stories/tutorials/event-listeners.ts
+++ packages/core/stories/tutorials/event-listeners.ts
@@ -6,5 +6,15 @@ Event dispatching for chart elements allows applications to trigger custom UI ac
 
 You can see the current dispatched events [here](https://github.com/carbon-design-system/carbon-charts/tree/master/packages/core/src/interfaces/events.ts).
 
+&nbsp;
+### Listening for events
+To listen for event just use a reference to the chart to add an event listener for one of the dispatched events above.
+This is an example for adding an event listener for a mouseover event on bar chart \`rect\`s.
+
+\`\`\`js
+barChart.services.events.addEventListener("bar-onmouseover", e => {
+	console.log(e.detail);
+});
+\`\`\`
 
 `);
