a81a0cb4ec4cf00ade230757ec5b181d983b8bb4
natashadecoste <natasha.decoste@gmail.com>
2020-05-08 10:33:52 -0400

    Update packages/core/stories/tutorials/event-listeners.ts
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

--- packages/core/stories/tutorials/event-listeners.ts
+++ packages/core/stories/tutorials/event-listeners.ts
@@ -4,7 +4,7 @@ export const eventListenersTutorial = marked(`
 # Events in Carbon Charts
 Event dispatching for chart elements allows applications to trigger custom UI actions and states when users interact with the charts.
 
-You can see the current dispatched events [here](https://github.com/carbon-design-system/carbon-charts/tree/master/packages/core/src/interfaces/events.ts).
+You can see the current dispatched events [here](https://carbon-design-system.github.io/carbon-charts/documentation/modules/_interfaces_events_.html).
 
 &nbsp;
 ### Listening for events
