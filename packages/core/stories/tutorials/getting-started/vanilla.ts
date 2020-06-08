b6425e5618f8096d279474949b2ec2a9c193d309
Eliad Moosavi <iliadmoosavi@gmail.com>
2020-06-08 11:32:16 -0400

    chore(core, react, angular, vue, svelte): fix codesandbox examples

--- packages/core/stories/tutorials/getting-started/vanilla.ts
+++ packages/core/stories/tutorials/getting-started/vanilla.ts
@@ -21,6 +21,8 @@ yarn add @carbon/charts d3
 npm install --save @carbon/charts d3
 \`\`\`
 
+**Note:** you'd also need to install \`carbon-components\` if you're not using a bundled version of the library.
+
 &nbsp;
 ## Getting started
 
