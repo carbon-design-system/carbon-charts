bdc47c7ade8de5445872b96cba428d3364f2382b
Luca Falasco <falasco.luca@gmail.com>
2020-06-11 18:31:51 +0200

    Merge pull request #549 from accurat/area-chart
    * add area chart component - basic
    
    * feat(core): Axes configuration options defaultable, Add option to not include 0 on axes
    
    * add area chart component - basic
    
    * Merge pull request #464 from accurat/storybook-in-core
    
    * Storybook in core: Configuration almost done
    
    * Storybook in core: Add examples (WIP)
    
    * Storybook in core: Fix styles.scss require
    
    * Storybook in core: Rename variables
    
    * Storybook in core: Filter the list of { options, data } cases from the demo ones adding an isDemoExample boolean: only those will be visible in the demo page, the rest will only be visible in the storybooks
    
    * add vanilla codesandbox
    
    * Storybook in core: Minor changes
    
    * fix vanilla sandbox issue
    
    * remove core internal demo site
    
    * globalize all demo styling within core
    
    * fix core story file path
    
    * add one page demo story
    
    * addition of themes to the core storybook
    
    * fix storybook build
    
    * remove unnecessary import from storybooks
    
    * downgrade sass version
    
    * fix tests
    
    * add correct NODE_ENV var in the netlify script
    
    * fix tests on Travis
    
    * fix tests
    
    * add correct classname to wrapper storybooks
    
    * add welcome story
    
    * update storybook default theme
    
    Co-authored-by: Cal Smith <1744185+cal-smith@users.noreply.github.com>
    Co-authored-by: carbon-bot <carbon@us.ibm.com>
    Co-authored-by: Cal Smith <calsmith@protonmail.com>
    Co-authored-by: Adam Rybinski <adam@rybinski.info>
    Co-authored-by: cesare.soldini <cesare.soldini@accurat.it>
    Co-authored-by: Eliad Moosavi <iliadmoosavi@gmail.com>
    
    * fix merge upstream
    
    * Fix area options merge
    
    * basic stacked area chart
    
    * remove spacing
    
    * add legend item hover
    
    * refactor
    
    * add event listeners
    
    * hide tooltip for now, clean up not used imports
    
    * fix rebase
    
    * add mix-blend-mode
    
    * rename area > stacked-area
    
    * Fix area-stacked scss
    
    * remove stroke
    
    * fix rebase, use model.getStackedData
    
    * fix spaceOnEdges
    
    * update area data to tabular format
    
    * fix legend highlighting
    
    * add area stacked percentage
    
    * add lines to stacked area chart
    
    * add ruler and tooltip
    
    * add wrappers for stacked area chart
    
    * hide lines when filtering is active
    
    * feat(core, react, vue, angular): add stacked area chart
    
    * format code
    
    * fix react wrapper for stacked area
    
    * fix import for svelte stacked area
    
    * add fade-in flag in scatter graph
    
    * format
    
    * format
    
    Co-authored-by: Serena Girardi <serena.girardi@accurat.it>
    Co-authored-by: Ilaria Venturini <ilaria.venturini@accurat.it>
    Co-authored-by: Cal Smith <1744185+cal-smith@users.noreply.github.com>
    Co-authored-by: carbon-bot <carbon@us.ibm.com>
    Co-authored-by: Cal Smith <calsmith@protonmail.com>
    Co-authored-by: Adam Rybinski <adam@rybinski.info>
    Co-authored-by: cesare.soldini <cesare.soldini@accurat.it>
    Co-authored-by: Eliad Moosavi <iliadmoosavi@gmail.com>
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

--- packages/core/src/components/graphs/area.ts
+++ packages/core/src/components/graphs/area.ts
@@ -10,7 +10,6 @@ export class Area extends Component {
 	type = "area";
 
 	init() {
-
 		const eventsFragment = this.services.events;
 
 		// Highlight correct area on legend item hovers
@@ -31,48 +30,52 @@ export class Area extends Component {
 		const { cartesianScales } = this.services;
 
 		const orientation = cartesianScales.getOrientation();
-		const areaGenerator = area()
-			.curve(this.services.curves.getD3Curve());
+		const areaGenerator = area().curve(this.services.curves.getD3Curve());
 
 		if (orientation === CartesianOrientations.VERTICAL) {
-			areaGenerator.x((d, i) => cartesianScales.getDomainValue(d, i))
+			areaGenerator
+				.x((d, i) => cartesianScales.getDomainValue(d, i))
 				.y0(cartesianScales.getRangeValue(0))
 				.y1((d, i) => cartesianScales.getRangeValue(d, i));
 		} else {
-			areaGenerator.x0(cartesianScales.getRangeValue(0))
+			areaGenerator
+				.x0(cartesianScales.getRangeValue(0))
 				.x1((d, i) => cartesianScales.getRangeValue(d, i))
 				.y((d, i) => cartesianScales.getDomainValue(d, i));
 		}
 
 		// Update the bound data on area groups
 		const groupedData = this.model.getGroupedData();
-		const areas = svg.selectAll("path.area")
-			.data(groupedData, group => group.name);
+		const areas = svg
+			.selectAll("path.area")
+			.data(groupedData, (group) => group.name);
 
 		// Remove elements that need to be exited
 		// We need exit at the top here to make sure that
 		// Data filters are processed before entering new elements
 		// Or updating existing ones
-		areas.exit()
-			.attr("opacity", 0)
-			.remove();
+		areas.exit().attr("opacity", 0).remove();
 
 		const self = this;
 
 		// Enter paths that need to be introduced
-		const enteringAreas = areas.enter()
-			.append("path")
-			.attr("opacity", 0);
+		const enteringAreas = areas.enter().append("path").attr("opacity", 0);
 
 		// Apply styles and datum
-		enteringAreas.merge(areas)
-			.attr("fill", group => {
-				return this.model.getFillColor(group.name)
+		enteringAreas
+			.merge(areas)
+			.attr("fill", (group) => {
+				return this.model.getFillColor(group.name);
 			})
-			.transition(this.services.transitions.getTransition("area-update-enter", animate))
+			.transition(
+				this.services.transitions.getTransition(
+					"area-update-enter",
+					animate
+				)
+			)
 			.attr("opacity", Configuration.area.opacity.selected)
 			.attr("class", "area")
-			.attr("d", group => {
+			.attr("d", (group) => {
 				const { data } = group;
 				return areaGenerator(data);
 			});
