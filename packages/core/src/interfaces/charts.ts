7a85a521afec40d7fc85d18dddc8ff899c325e00
Carmen Tan <carmiele@users.noreply.github.com>
2020-06-16 09:28:34 -0700

    Merge pull request #669 from carmiele/issue-668-missing-timescale-option
    Co-authored-by: Carmen Tan <carmentan@ibm.com>
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

--- packages/core/src/interfaces/charts.ts
+++ packages/core/src/interfaces/charts.ts
@@ -10,6 +10,7 @@ import {
 	BarOptions,
 	StackedBarOptions,
 } from "./components";
+import { TimeScaleOptions } from "./axis-scales";
 
 /**
  * Base chart options common to any chart
@@ -109,6 +110,7 @@ export interface BaseChartOptions {
 export interface AxisChartOptions extends BaseChartOptions {
 	axes?: AxesOptions;
 	grid?: GridOptions;
+	timeScale?: TimeScaleOptions;
 	tooltip?: AxisTooltipOptions;
 }
 
