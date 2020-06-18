5353f241796cacaed52bac5251098ec32e5819aa
Fei <fei-fei.zheng@ibm.com>
2020-06-18 13:57:56 -0400

    style: format code

--- packages/core/src/model.ts
+++ packages/core/src/model.ts
@@ -72,8 +72,7 @@ export class ChartModel {
 
 						// Filter out data outside domain
 						displayData = displayData.filter(
-							(datum) =>
-								datum[mapsTo] >= start && datum[mapsTo] <= end
+							(datum) => datum[mapsTo] >= start && datum[mapsTo] <= end
 						);
 					}
 				}
