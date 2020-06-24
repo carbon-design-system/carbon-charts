a8a1a4b70fdda86d17987933f5a44dd41bffbfbf
Fei <fei-fei.zheng@ibm.com>
2020-06-24 16:10:34 -0400

    test: update test

--- packages/core/src/selectedGroups.spec.ts
+++ packages/core/src/selectedGroups.spec.ts
@@ -19,8 +19,9 @@ describe("selectedGroups option", () => {
 
 	describe("selected groups", () => {
 		it("should match the selected legend labels", function (done) {
-			const selectedGroups = this.chart.model.getOptions().data.selectedGroups;
 			const chartEventsService = this.chart.services.events;
+
+			const sampleSelectedGroups = ['Dataset 1', 'Dataset 3'];
 
 			const renderCb = () => {
 				// Remove render event listener
@@ -32,71 +33,13 @@ describe("selectedGroups option", () => {
 				const legendGroup = select(
 					`g.${settings.prefix}--${options.chart.style.prefix}--legend`
 				);
-
-				const numberOfTotalItems = legendGroup
-					.selectAll('g.legend-item')
-					.size();
-
-				const numberOfActiveItems = legendGroup
-					.selectAll('g.legend-item.active')
-					.size();
 				
-				const activeLegendNodes = legendGroup
+				const selectedLegendLabels = legendGroup
 					.selectAll('g.legend-item.active > text')
-					.nodes();
+					.nodes()
+					.map(item => item['innerHTML']);
 
-				const selectedLegendLabels = activeLegendNodes.map(item => item['innerHTML']);
-
-				const preselectedLegendLabels =
-					numberOfTotalItems === numberOfActiveItems
-					? []
-					: selectedLegendLabels;
-
-				expect(selectedGroups).toEqual(preselectedLegendLabels);
-
-				done();
-			};
-
-			// Add event listener for when chart render is finished
-			chartEventsService.addEventListener(
-				Events.Chart.RENDER_FINISHED,
-				renderCb
-			);
-		})
-	})
-
-	describe("selected groups", () => {
-		it("should match the legend labels after click event", function (done) {
-			const chartEventsService = this.chart.services.events;
-			const dataGroups = this.chart.model.getDataGroups();
-			const firstDatasetName = dataGroups[0].name;
-
-			const isOnlySelectedItem = dataGroups[0].status && 
-				dataGroups.filter(group => group.status).length === 1;
-
-			this.chart.model.toggleDataLabel(firstDatasetName);
-			chartEventsService.removeEventListener(Events.Legend.ITEMS_UPDATE, {
-				dataGroups
-			});
-
-			const renderCb = () => {
-				// Remove render event listener
-				chartEventsService.removeEventListener(
-					Events.Chart.RENDER_FINISHED,
-					renderCb
-				);
-				const selectedGroups = this.chart.model.getOptions().data.selectedGroups;
-
-				const selectedLegendLabels = [];
-
-				if (!isOnlySelectedItem) {
-					dataGroups.forEach(dataGroup => {
-						if (dataGroup.status){
-							selectedLegendLabels.push(dataGroup.name);
-						}
-					});
-				}
-				expect(selectedGroups).toEqual(selectedLegendLabels);
+				expect(selectedLegendLabels).toEqual(sampleSelectedGroups);
 
 				done();
 			};
