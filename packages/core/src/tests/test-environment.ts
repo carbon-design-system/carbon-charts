79cf2574902ec9453fe352778b19b3636bc161dc
Fei <fei-fei.zheng@ibm.com>
2020-06-25 10:58:54 -0400

    test: add more tests

--- packages/core/src/tests/test-environment.ts
+++ packages/core/src/tests/test-environment.ts
@@ -11,7 +11,7 @@ export const data = groupedBarData as ChartData;
 export const options = Object.assign(groupedBarOptions, {
 	title: "My chart",
 	data: {
-		selectedGroups: ['Dataset 1', 'Dataset 3']
+		selectedGroups: ['Dataset 1']
 	}
 }) as any;
 
