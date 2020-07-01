636d2b5b4b131ad34a35cded6c4c9fa873ce598a
EricYang <hlyang397@gmail.com>
2020-07-01 14:28:08 +0800

    refactor: delete unused axis-zoom service

--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -4,6 +4,5 @@ export * from "./essentials/events";
 export * from "./essentials/transitions";
 
 // MISC
-export * from "./axis-zoom";
 export * from "./scales-cartesian";
 export * from "./curves";
