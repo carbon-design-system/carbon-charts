06d9757c0d932708bb79ba7c2237335cc3a2fbc0
linhenry <linhenry@tw.ibm.com>
2020-07-17 02:07:06 +0800

    adjust the bar width to prevent bars from overlapping

--- packages/core/src/components/graphs/bar.ts
+++ packages/core/src/components/graphs/bar.ts
@@ -1,16 +1,20 @@
 // Internal Imports
 import { Component } from "../component";
+import { DOMUtils } from "../../services";
 
 export class Bar extends Component {
 	// Gets the correct width for bars based on options & configurations
 	protected getBarWidth(allDataLabels?: string[]) {
 		const options = this.model.getOptions();
 		const mainXScale = this.services.cartesianScales.getMainXScale();
+		const width = DOMUtils.getSVGElementSize(this.parent, {
+			useAttrs: true
+		}).width;
 
 		if (!mainXScale.step) {
 			return Math.min(
 				options.bars.maxWidth,
-				(5 / mainXScale.ticks().length) * options.bars.maxWidth
+				width * 0.9 / mainXScale.ticks().length
 			);
 		}
 
