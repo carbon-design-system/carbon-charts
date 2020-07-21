9ad904600b2e7a050e057eb0c000e99e5a2cc82c
Fei <fei-fei.zheng@ibm.com>
2020-07-21 11:50:56 -0400

    fix: keep labels on the edges for custom domain

--- packages/core/src/components/axes/axis.ts
+++ packages/core/src/components/axes/axis.ts
@@ -217,7 +217,7 @@ export class Axis extends Component {
 
 					// Remove labels on the edges
 					// If there are more than 2 labels to show
-					if (addSpaceOnEdges && tickValues.length > 2) {
+					if (addSpaceOnEdges && tickValues.length > 2 && !customDomain) {
 						tickValues.splice(tickValues.length - 1, 1);
 						tickValues.splice(0, 1);
 					}
