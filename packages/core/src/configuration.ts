a1b5a6a3b85bd6788a0f09ee2834983412f3bf54
JennChao <jennchao91@gmail.com>
2020-09-01 16:54:50 +0800

    fix: set default gradientEnabled to false

--- packages/core/src/configuration.ts
+++ packages/core/src/configuration.ts
@@ -208,7 +208,7 @@ const lineChart: LineChartOptions = Tools.merge({}, axisChart, {
  * options specific to area charts
  */
 const areaChart: AreaChartOptions = Tools.merge({}, lineChart, {
-	gradientEnabled: true,
+	gradientEnabled: false,
 	timeScale: Tools.merge(timeScale, {
 		addSpaceOnEdges: 0
 	} as TimeScaleOptions)
