bad808b0bc5c2df0cbdb2195a53d763f7d3f8006
Fei <fei-fei.zheng@ibm.com>
2020-09-03 15:50:48 -0400

    fix(core, angular, react, vue, svelt): show legend while data is loading

--- packages/core/src/chart.ts
+++ packages/core/src/chart.ts
@@ -142,14 +142,8 @@ export class Chart {
 			}
 		};
 
-		// if the chart is loading but has data, don't enable legend until loading is false
-		const isDataLoading = Tools.getProperty(
-			this.model.getOptions(),
-			"data",
-			"loading"
-		);
 		const isLegendEnabled =
-			this.model.getOptions().legend.enabled !== false && !isDataLoading;
+			this.model.getOptions().legend.enabled !== false;
 
 		// TODORF - REUSE BETWEEN AXISCHART & CHART
 		// Decide the position of the legend in reference to the chart
