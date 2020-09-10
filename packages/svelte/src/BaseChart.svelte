9049ba5e7c3657108570a1e0ece4ff6f1994e45f
Eric Liu <ericyl.us@gmail.com>
2020-09-10 16:48:43 -0700

    fix(svelte): rewrite BaseChart to reload chart instance
    Fixes #799

--- packages/svelte/src/BaseChart.svelte
+++ packages/svelte/src/BaseChart.svelte
@@ -2,38 +2,44 @@
   export let Chart = undefined;
   export let data = {};
   export let options = {};
+  export let id = "chart-" + Math.random().toString(36);
 
-  import { onMount, createEventDispatcher } from "svelte";
+  import { onMount, afterUpdate, createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
-  const id = "chart-" + Math.random().toString(36);
 
-  let ref = undefined;
-  let chart = undefined;
+  let ref = null;
+  let chart = null;
+  let element = null;
+  let prevChart = undefined;
 
   onMount(() => {
     /**
      * CodeSandbox does not resolve Svelte components from the `svelte` package.json entry.
      * This causes `bind:ref` to be `undefined`; the chart can't mount to the element.
-     * 
+     *
      * We fallback to manually querying the DOM for the chart holder element because
      * CodeSandbox does not use uncompiled Svelte source code.
-     * 
+     *
      * See https://github.com/sveltejs/svelte/issues/2937
      */
-    const element = ref || document.getElementById(id);
-
-    if (element) {
-      chart = new Chart(element, { data, options });
-      dispatch("load", chart);
-    }
+    element = ref || document.getElementById(id);
 
     return () => {
       if (chart) {
-        chart.destroy();
+        chart.components.forEach((component) => component.destroy());
+        chart = null;
         dispatch("destroy");
       }
     };
+  });
+
+  afterUpdate(() => {
+    if (Chart && prevChart !== Chart) {
+      chart = new Chart(element, { data, options });
+      dispatch("load", chart);
+      prevChart = Chart;
+    }
   });
 
   $: if (chart) {
@@ -43,4 +49,4 @@
   }
 </script>
 
-<div {...$$restProps} bind:this={ref} {id} />
+<div {...$$restProps} bind:this={ref} {id}></div>
