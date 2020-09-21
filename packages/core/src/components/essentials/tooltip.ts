6eebac582645bc84ef8b3157aa4bae8d1f337dcc
Eliad Moosavi <iliadmoosavi@gmail.com>
2020-09-21 12:01:33 -0400

    fix(core): set aria-hidden attribute on tooltips

--- packages/core/src/components/essentials/tooltip.ts
+++ packages/core/src/components/essentials/tooltip.ts
@@ -33,28 +33,24 @@ export class Tooltip extends Component {
 		const defaultHTML = this.getTooltipHTML(e);
 
 		const tooltipTextContainer = DOMUtils.appendOrSelect(
-				this.tooltip,
-				"div.content-box"
-			);
+			this.tooltip,
+			"div.content-box"
+		);
 
 		// if there is a provided tooltip HTML function call it
 		if (
-			Tools.getProperty(
-				this.model.getOptions(),
-				"tooltip",
-				"customHTML"
-			)
+			Tools.getProperty(this.model.getOptions(), "tooltip", "customHTML")
 		) {
 			if (e.detail.content) {
-						const labelHTML = `<div class="title-tooltip">${e.detail.content}</div>`;
-						tooltipTextContainer.html(labelHTML);
-					} else {
-						tooltipTextContainer.html(
-						this.model
-							.getOptions()
-							.tooltip.customHTML(data, defaultHTML)
-						);
-					}
+				const labelHTML = `<div class="title-tooltip">${e.detail.content}</div>`;
+				tooltipTextContainer.html(labelHTML);
+			} else {
+				tooltipTextContainer.html(
+					this.model
+						.getOptions()
+						.tooltip.customHTML(data, defaultHTML)
+				);
+			}
 		} else {
 			// Use default tooltip
 			tooltipTextContainer.html(defaultHTML);
@@ -64,12 +60,12 @@ export class Tooltip extends Component {
 		this.positionTooltip(e);
 
 		// Fade in
-		this.tooltip.classed("hidden", false);
-	}
+		this.tooltip.classed("hidden", false).attr("aria-hidden", false);
+	};
 
 	handleHideTooltip = () => {
-		this.tooltip.classed("hidden", true);
-	}
+		this.tooltip.classed("hidden", true).attr("aria-hidden", true);
+	};
 
 	addTooltipEventListener() {
 		// listen to move-tooltip Custom Events to move the tooltip
@@ -95,10 +91,7 @@ export class Tooltip extends Component {
 
 	removeTooltipEventListener() {
 		// remove move-tooltip Custom Events
-		this.services.events.removeEventListener(
-			Events.Tooltip.MOVE,
-			null
-		);
+		this.services.events.removeEventListener(Events.Tooltip.MOVE, null);
 
 		// remove show-tooltip Custom Events
 		this.services.events.removeEventListener(
@@ -189,9 +182,9 @@ export class Tooltip extends Component {
 							<div class="datapoint-tooltip ${item.bold ? "bold" : ""}">
 								${
 									item.color
-										? "<a style=\"background-color: " +
+										? '<a style="background-color: ' +
 										  item.color +
-										  "\" class=\"tooltip-color\"></a>"
+										  '" class="tooltip-color"></a>'
 										: ""
 								}
 								<p class="label">${item.label}</p>
@@ -231,11 +224,7 @@ export class Tooltip extends Component {
 		if (isTooltipEnabled) {
 			// Grab the tooltip element
 			const holder = select(this.services.domUtils.getHolder());
-			const chartprefix = Tools.getProperty(
-				options,
-				"style",
-				"prefix"
-			);
+			const chartprefix = Tools.getProperty(options, "style", "prefix");
 			this.tooltip = DOMUtils.appendOrSelect(
 				holder,
 				`div.${settings.prefix}--${chartprefix}--tooltip`
