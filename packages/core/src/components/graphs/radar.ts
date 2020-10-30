6f64cecae7a11708e7931f3b965b1471c90c7eda
Fei <fei-fei.zheng@ibm.com>
2020-10-29 21:29:51 -0400

    fix(core, angular, react, vue, svelt): update radar color when option changes

--- packages/core/src/components/graphs/radar.ts
+++ packages/core/src/components/graphs/radar.ts
@@ -453,7 +453,10 @@ export class Radar extends Component {
 					.append("path")
 					.attr("class", (group) =>
 						this.model.getColorClassName({
-							classNameTypes: [ColorClassNameTypes.FILL, ColorClassNameTypes.STROKE],
+							classNameTypes: [
+								ColorClassNameTypes.FILL,
+								ColorClassNameTypes.STROKE
+							],
 							dataGroupName: group.name,
 							originalClassName: "blob"
 						})
@@ -478,7 +481,20 @@ export class Radar extends Component {
 								radialLineGenerator(group.data)
 							)
 					),
-			(update) =>
+			(update) => {
+				update
+					.attr("class", (group) =>
+						this.model.getColorClassName({
+							classNameTypes: [
+								ColorClassNameTypes.FILL,
+								ColorClassNameTypes.STROKE
+							],
+							dataGroupName: group.name,
+							originalClassName: "blob"
+						})
+					)
+					.attr("fill", (group) => colorScale(group.name))
+					.attr("stroke", (group) => colorScale(group.name));
 				update.call((selection) =>
 					selection
 						.transition(
@@ -490,7 +506,8 @@ export class Radar extends Component {
 						.attr("opacity", 1)
 						.attr("transform", `translate(${c.x}, ${c.y})`)
 						.attr("d", (group) => radialLineGenerator(group.data))
-				),
+				);
+			},
 			(exit) =>
 				exit.call((selection) =>
 					selection
