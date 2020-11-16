cc72c6bf4bb1830c388bb3c49fc21031ac1d34b1
Natasha DeCoste <natasha.decoste@gmail.com>
2020-11-16 16:57:14 -0500

    docs(axis-charts): tutorial for dual axes

---
+++ packages/core/stories/tutorials/dual-axes.ts
@@ -0,0 +1,16 @@
+import marked from "marked";
+
+export const dualAxesTutorial = {
+	name: "Dual Axes Tutorial",
+	content: marked(`
+# Dual Axes
+If a chart is considered to be a **dual axes** chart, it means that the chart uses 2 **different** axes for visually mapping out ranges for the data.
+Careful consideration should be taken when using Dual Axes charts because they can more often misrepresent the data.
+
+Please refer to dataviz guidelines here for more information on IBM's dataviz guidelines.
+
+
+
+___
+`)
+};
