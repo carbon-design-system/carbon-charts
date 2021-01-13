25a732b58510d1d1d96db0fd980d4b6da36986ca
natashadecoste <natasha.decoste@gmail.com>
2021-01-13 16:13:14 -0500

    Apply suggestions from code review
    Co-authored-by: Eliad Moosavi <theiliad@users.noreply.github.com>

--- packages/core/src/components/graphs/pie.ts
+++ packages/core/src/components/graphs/pie.ts
@@ -61,9 +61,7 @@ export class Pie extends Component {
 
 		// remove any slices that are valued at 0 because they dont need to be rendered and will create extra padding
 		const displayData = this.model.getDisplayData()
-			.filter(data =>{
-				return data.value !== 0;
-		});
+			.filter(data => data.value > 0);
 		const options = this.getOptions();
 		const { groupMapsTo } = options.data;
 
