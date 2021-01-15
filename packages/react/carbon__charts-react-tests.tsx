ffff19380e391fc603600e2630f7bb583a6a335b
Eric Liu <ericyl.us@gmail.com>
2021-01-15 11:34:38 -0800

    fix(types): add peak, status properties to MeterChartOptions interface (#924)
    #920

--- packages/react/carbon__charts-react-tests.tsx
+++ packages/react/carbon__charts-react-tests.tsx
@@ -1,6 +1,21 @@
-import * as React from 'react';
-import { ScaleTypes } from '@carbon/charts/interfaces';
-import { LineChart } from './src';
+import * as React from "react";
+import { MeterChartOptions, ScaleTypes, Statuses } from "@carbon/charts/interfaces";
+import { LineChart, MeterChart } from "./src";
+
+const meterOptions: MeterChartOptions = {
+	title: "Meter Chart - with statuses",
+	meter: {
+		peak: 80,
+		status: {
+			ranges: [
+				{ range: [0, 50], status: Statuses.SUCCESS },
+				{ range: [50, 60], status: Statuses.WARNING },
+				{ range: [60, 100], status: Statuses.DANGER }
+			]
+		}
+	},
+	height: "100px"
+};
 
 class App extends React.Component {
 	ref: React.RefObject<LineChart> = React.createRef();
@@ -24,35 +39,40 @@ class App extends React.Component {
 			console.log(this.ref.current.data);
 		}
 
-		return (
-			<LineChart
-				ref={this.ref}
-				data={this.state.data}
-				options={{
-					title: 'Line (discrete)',
-					axes: {
-						bottom: {
-							title: '2019 Annual Sales Figures',
-							mapsTo: 'key',
-							scaleType: ScaleTypes.LABELS,
-						},
-						left: {
-							mapsTo: 'value',
-							title: 'Conversion rate',
-							scaleType: ScaleTypes.LABELS,
-						},
-					},
-					color: {
-						scale: {
-							'Dataset 1': '#925699',
-							'Dataset 2': '#525669',
-							'Dataset 3': '#725699',
-							'Dataset 4': '#ccc',
-						},
-					},
-					height: '400px',
-				}}
-			/>
-		);
-	}
+    return (
+      <>
+        <LineChart
+          ref={this.ref}
+          data={this.state.data}
+          options={{
+            title: "Line (discrete)",
+            axes: {
+              bottom: {
+                title: "2019 Annual Sales Figures",
+                mapsTo: "key",
+                scaleType: ScaleTypes.LABELS,
+              },
+              left: {
+                mapsTo: "value",
+                title: "Conversion rate",
+                scaleType: ScaleTypes.LABELS,
+              },
+            },
+            color: {
+              scale: {
+                "Dataset 1": "#925699",
+                "Dataset 2": "#525669",
+                "Dataset 3": "#725699",
+                "Dataset 4": "#ccc",
+              },
+            },
+            height: "400px",
+          }}
+        />
+        <MeterChart
+          options={meterOptions}
+          data={[{ "group": "Dataset 1", "value": 56 }]} />
+      </>
+    );
+  }
 }
