350a07bf4191fdbb87f609cd9b813f07c451f9ef
Eliad Moosavi <theiliad@users.noreply.github.com>
2021-05-10 16:20:19 -0400

    release - fix(core): set a min-2px size for simple bars (#1010)

--- packages/core/src/components/graphs/bar-simple.ts
+++ packages/core/src/components/graphs/bar-simple.ts
@@ -1,6 +1,11 @@
 // Internal Imports
 import { Bar } from './bar';
-import { Events, Roles, ColorClassNameTypes } from '../../interfaces';
+import {
+	Events,
+	Roles,
+	ColorClassNameTypes,
+	CartesianOrientations,
+} from '../../interfaces';
 import { Tools } from '../../tools';
 
 // D3 Imports
@@ -33,6 +38,8 @@ export class SimpleBar extends Bar {
 		const svg = this.getContainerSVG({ withinChartClip: true });
 
 		const data = this.model.getDisplayData(this.configs.groups);
+
+		const orientation = this.services.cartesianScales.getOrientation();
 
 		// Update data on all bars
 		const bars = svg
@@ -90,6 +97,21 @@ export class SimpleBar extends Bar {
 					y1 = this.services.cartesianScales.getRangeValue(d, i);
 				}
 
+				const difference = Math.abs(y1 - y0);
+				// Set a min-2px size for the bar
+				if (difference !== 0 && difference < 2) {
+					if (
+						(value > 0 &&
+							orientation === CartesianOrientations.VERTICAL) ||
+						(value < 0 &&
+							orientation === CartesianOrientations.HORIZONTAL)
+					) {
+						y1 = y0 - 2;
+					} else {
+						y1 = y0 + 2;
+					}
+				}
+
 				// don't show if part of bar is out of zoom domain
 				if (this.isOutsideZoomedDomain(x0, x1)) {
 					return;
@@ -97,7 +119,7 @@ export class SimpleBar extends Bar {
 
 				return Tools.generateSVGPathString(
 					{ x0, x1, y0, y1 },
-					this.services.cartesianScales.getOrientation()
+					orientation
 				);
 			})
 			.attr('opacity', 1)
