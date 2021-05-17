e3da3a2050a8b6a06c111ca5cf8a2dc92e9a7c9f
Eliad Moosavi <iliadmoosavi@gmail.com>
2021-05-17 11:34:25 -0400

    remove unnecessary plex files
    Former-commit-id: 24c629a73b4171c03391ef4a8cff20e0ffccb7cd

--- packages/core/build-vendor.sh
+++ packages/core/build-vendor.sh
@@ -9,4 +9,11 @@ cp -r ../../node_modules/@carbon/type src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
 cp -r ../../node_modules/carbon-components src/styles/vendor
-cp -r ../../node_modules/@ibm/plex src/styles/vendor
+
+# plex
+mkdir src/styles/vendor/plex
+cp -r ../../node_modules/@ibm/plex/css src/styles/vendor/plex/css
+cp -r ../../node_modules/@ibm/plex/LICENSE.txt src/styles/vendor/plex
+cp -r ../../node_modules/@ibm/plex/package.json src/styles/vendor/plex
+cp -r ../../node_modules/@ibm/plex/README.md src/styles/vendor/plex
+cp -r ../../node_modules/@ibm/plex/CHANGELOG.md src/styles/vendor/plex
