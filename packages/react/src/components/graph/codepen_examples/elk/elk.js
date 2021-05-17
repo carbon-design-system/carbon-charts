9ffdc3d16f8ebd6e486fa96cf000eb955016f950
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2021-05-17 16:42:52 +0100

    chore: remove custom webpack

--- packages/react/src/components/graph/codepen_examples/elk/elk.js
+++ packages/react/src/components/graph/codepen_examples/elk/elk.js
@@ -1,94 +1,101 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import ELK from 'elkjs/lib/elk.bundled';
 import { path as d3Path } from 'd3-path';
 import Circle from '../../circle/circle';
 import Edge from '../../edge/edge';
 import { User16 } from '@carbon/icons-react';
 
-const Link = ({link}) => {
-  const sections = link.sections[0]; // Todo: clean up assignment
-  const path = d3Path();
+const Link = ({ link }) => {
+	const sections = link.sections[0]; // Todo: clean up assignment
+	const path = d3Path();
 
-  path.moveTo(sections.startPoint.x, sections.startPoint.y);
+	path.moveTo(sections.startPoint.x, sections.startPoint.y);
 
-  if (sections.bendPoints) {
-    sections.bendPoints.forEach(b => {
-      path.lineTo(b.x, b.y);
-    });
-  }
+	if (sections.bendPoints) {
+		sections.bendPoints.forEach((b) => {
+			path.lineTo(b.x, b.y);
+		});
+	}
 
-  path.lineTo(sections.endPoint.x, sections.endPoint.y);
+	path.lineTo(sections.endPoint.x, sections.endPoint.y);
 
-  return (
-    <Edge path={path.toString()} />
-  );
+	return <Edge path={path.toString()} />;
 };
 
-const Node = ({x, y}) => {
-  return (
-      <foreignObject transform={`translate(${x},${y})`} height={48} width={48} style={{ overflow: "visible" }}>
-        <div style={{ height: 48, width: 48 }}>
-			<Circle renderIcon={<User16 />} />
-        </div>
-      </foreignObject>
-  )
+const Node = ({ x, y }) => {
+	return (
+		<foreignObject
+			transform={`translate(${x},${y})`}
+			height={48}
+			width={48}
+			style={{ overflow: 'visible' }}>
+			<div style={{ height: 48, width: 48 }}>
+				<Circle renderIcon={<User16 />} />
+			</div>
+		</foreignObject>
+	);
 };
 
-const Elk = ({nodes, links, layout}) => {
-  const elk = new ELK();
+const Elk = ({ nodes, links, layout }) => {
+	const elk = new ELK();
 
-  const [positions, setPositions] = useState(null);
+	const [positions, setPositions] = useState(null);
 
-  const graph = {
-    id: "root",
-    layoutOptions: {
-      'elk.algorithm': layout,
-      'elk.padding': '[left=50, top=50, right=50, bottom=50]',
-      "separateConnectedComponents": false,
-      "spacing.nodeNode": 100,
-      "spacing.nodeNodeBetweenLayers": 100
-    },
-    children: nodes,
-    edges: links
-  };
+	const graph = {
+		id: 'root',
+		layoutOptions: {
+			'elk.algorithm': layout,
+			'elk.padding': '[left=50, top=50, right=50, bottom=50]',
+			separateConnectedComponents: false,
+			'spacing.nodeNode': 100,
+			'spacing.nodeNodeBetweenLayers': 100,
+		},
+		children: nodes,
+		edges: links,
+	};
 
-  useEffect(()=> {
-    elk.layout(graph)
-    .then(g => setPositions(g))
-    .catch(console.error);
-  }, []);
+	useEffect(() => {
+		elk.layout(graph)
+			.then((g) => setPositions(g))
+			.catch(console.error);
+	}, []);
 
+	if (!positions) return null;
 
-  if (!positions) return null;
+	const buildNodes = () => {
+		const { children } = positions;
 
-  const buildNodes = () => {
-    const { children } = positions;
+		return children.map((node, i) => {
+			return <Node key={`node_${i}`} x={node.x} y={node.y} />;
+		});
+	};
 
-    return children.map((node,i) => {
-      return <Node key={`node_${i}`} x={node.x} y={node.y} />
-    })
-  };
+	const buildLinks = () => {
+		const { edges } = positions;
 
-  const buildLinks = () => {
-    const { edges } = positions;
+		return edges.map((edge, i) => {
+			return <Link key={`link_${i}`} link={edge} />;
+		});
+	};
 
-    return edges.map((edge ,i) => {
-      return <Link key={`link_${i}`} link={edge} />
-    })
+	const nodeElements = buildNodes();
+	const linkElements = buildLinks();
 
-  }
-
-  const nodeElements = buildNodes();
-  const linkElements = buildLinks();
-
-  return (
-    <div className={`force`} style={{ position: `relative`, height: 1000, width: 1000, margin: 32 }}>
-      <svg style={{ height: "100%", width: "100%" }}>
-        {linkElements}
-        {nodeElements}
-      </svg>
-    </div>
-  );
+	return (
+		<div
+			className={`force`}
+			style={{
+				position: `relative`,
+				height: 1000,
+				width: 1000,
+				margin: 32,
+			}}>
+			<svg style={{ height: '100%', width: '100%' }}>
+				{linkElements}
+				{nodeElements}
+			</svg>
+		</div>
+	);
 };
 
 export default Elk;
--- packages/react/src/components/graph/codepen_examples/elk/elk.stories.js
+++ packages/react/src/components/graph/codepen_examples/elk/elk.stories.js
@@ -5,24 +5,24 @@ import Elk from './elk';
 const size = 48;
 
 const nodeData = [
-  { id: "a", height: size, width: size },
-  { id: "b", height: size, width: size },
-  { id: "c", height: size, width: size },
-  { id: "d", height: size, width: size },
-  { id: "e", height: size, width: size },
-  { id: "f", height: size, width: size },
-  { id: "g", height: size, width: size },
-  { id: "h", height: size, width: size },
+	{ id: 'a', height: size, width: size },
+	{ id: 'b', height: size, width: size },
+	{ id: 'c', height: size, width: size },
+	{ id: 'd', height: size, width: size },
+	{ id: 'e', height: size, width: size },
+	{ id: 'f', height: size, width: size },
+	{ id: 'g', height: size, width: size },
+	{ id: 'h', height: size, width: size },
 ];
 
 const linkData = [
-  { id: "1", source: "a", target: "b" },
-  { id: "2", source: "c", target: "b" },
-  { id: "3", source: "d", target: "e" },
-  { id: "4", source: "d", target: "b" },
-  { id: "5", source: "b", target: "f" },
-  { id: "6", source: "g", target: "h" },
-  { id: "7", source: "h", target: "f" },
+	{ id: '1', source: 'a', target: 'b' },
+	{ id: '2', source: 'c', target: 'b' },
+	{ id: '3', source: 'd', target: 'e' },
+	{ id: '4', source: 'd', target: 'b' },
+	{ id: '5', source: 'b', target: 'f' },
+	{ id: '6', source: 'g', target: 'h' },
+	{ id: '7', source: 'h', target: 'f' },
 ];
 
 const stories = storiesOf('Experimental|Elk', module);
@@ -38,7 +38,6 @@ stories.add('Force', () => (
 stories.add('Layered', () => (
 	<Elk nodes={nodeData} links={linkData} layout="layered" />
 ));
-
 
 stories.add('Tree', () => (
 	<Elk nodes={nodeData} links={linkData} layout="mrtree" />
