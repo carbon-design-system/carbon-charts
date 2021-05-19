e46578f00e3c4ef2708c3ef007e93cd69f6a028d
Eliad Moosavi <iliadmoosavi@gmail.com>
2021-05-19 12:20:32 -0400

    work saving
    Former-commit-id: 9921b7535b48af5ac74526b420f691274e4c8b6c

--- packages/core/build-vendor.sh
+++ packages/core/build-vendor.sh
@@ -10,10 +10,10 @@ cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
 cp -r ../../node_modules/carbon-components src/styles/vendor
 
-# plex
-mkdir src/styles/vendor/plex
-cp -r ../../node_modules/@ibm/plex/css src/styles/vendor/plex/css
-cp -r ../../node_modules/@ibm/plex/LICENSE.txt src/styles/vendor/plex
-cp -r ../../node_modules/@ibm/plex/package.json src/styles/vendor/plex
-cp -r ../../node_modules/@ibm/plex/README.md src/styles/vendor/plex
-cp -r ../../node_modules/@ibm/plex/CHANGELOG.md src/styles/vendor/plex
+# # plex
+# mkdir src/styles/vendor/plex
+# cp -r ../../node_modules/@ibm/plex/css src/styles/vendor/plex/css
+# cp -r ../../node_modules/@ibm/plex/LICENSE.txt src/styles/vendor/plex
+# cp -r ../../node_modules/@ibm/plex/package.json src/styles/vendor/plex
+# cp -r ../../node_modules/@ibm/plex/README.md src/styles/vendor/plex
+# cp -r ../../node_modules/@ibm/plex/CHANGELOG.md src/styles/vendor/plex
