762554a07f1143df0eed55224c6b454f50bde8c0
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2021-05-27 12:23:36 +0100

    chore: set up local testing for styles

--- packages/react/.storybook/config.js
+++ packages/react/.storybook/config.js
@@ -10,7 +10,9 @@ withOptions({
 });
 
 // load global styles
-require('!style-loader!css-loader!@carbon/charts/demo/styles.css');
+// require('!style-loader!css-loader!@carbon/charts/demo/styles.css');
+// Temporary override for local style testing
+require('!style-loader!css-loader!sass-loader!../../core/src/styles/styles-g10.scss');
 
 const req = require.context('../', true, /.stories.js$/);
 function loadStories() {
