75ee128632890224b5bfc9a9788bbc70cccc8613
Eliad Moosavi <theiliad@users.noreply.github.com>
2021-05-31 10:47:57 -0400

    release - fix(core): account for threshold values when determining scale domains (#1036)

--- packages/core/src/services/scales-cartesian.ts
+++ packages/core/src/services/scales-cartesian.ts
@@ -505,7 +505,12 @@ export class CartesianScales extends Service {
 		}
 
 		const displayData = this.model.getDisplayData();
-		const { extendLinearDomainBy, mapsTo, percentage } = axisOptions;
+		const {
+			extendLinearDomainBy,
+			mapsTo,
+			percentage,
+			thresholds,
+		} = axisOptions;
 		const {
 			reference: ratioReference,
 			compareTo: ratioCompareTo,
@@ -602,6 +607,14 @@ export class CartesianScales extends Service {
 
 		if (scaleType !== ScaleTypes.TIME && includeZero) {
 			allDataValues.push(0);
+		}
+
+		// Add threshold values into the scale
+		if (thresholds && thresholds.length > 0) {
+			thresholds.forEach(threshold => {
+				const thresholdValue = Tools.getProperty(threshold, 'value');
+				if (thresholdValue !== null) allDataValues.push(thresholdValue);
+			});
 		}
 
 		domain = extent(allDataValues);
