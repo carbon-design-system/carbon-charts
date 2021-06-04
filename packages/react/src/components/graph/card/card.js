aa4ab4a399db17362e46948874ea5a6efb917fd8
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2021-06-04 12:11:23 +0100

    feat: use modular card

--- packages/react/src/components/graph/card/card.js
+++ packages/react/src/components/graph/card/card.js
@@ -3,29 +3,40 @@ import settings from 'carbon-components/src/globals/js/settings';
 import classnames from 'classnames';
 
 const { prefix } = settings;
+const namespace = `${prefix}--cc--card`;
 
-export default ({ color, description, label, onClick = () => {}, renderAction, renderIcon, stacked, title }) => {
-	const namespace = `${prefix}--cc--card`;
-	const icon = renderIcon ? (
-		<div className={`${namespace}__icon`}>{renderIcon}</div>
-	) : null;
+const Card = ({ as = "div", children, color, href = null, onClick = null, stacked }) => {
+	let Component = 'div';
+
+	if (as !== 'div') {
+		Component = as;
+	} else if (href) {
+		Component = 'a';
+	} else if (onClick) {
+		Component = 'button';
+	}
 
 	const cardClasses = classnames(namespace, {
 		[`${namespace}--stacked`]: stacked,
+		[`${namespace}--${Component}`]: Component,
 	});
 
-	const action = renderAction ? <div className={`${namespace}__action`}>{renderAction}</div> : null;
-	const labelElement = label ? <div className={`${namespace}__label`}>{label}</div> : null;
-
 	return (
-		<div className={cardClasses} tabIndex={0} onClick={() => onClick()} style={{ borderColor: color }} >
-			{icon}
-			<div className={`${namespace}__body`}>
-				<div className={`${namespace}__title`}>{title}</div>
-				<div className={`${namespace}__description`}>{description}</div>
-				{labelElement}
-			</div>
-			{action}
-		</div>
+		<Component className={cardClasses} tabIndex={0} onClick={() => onClick()} style={{ borderColor: color }} >
+			{children}
+		</Component>
 	);
 };
+
+const CardColumn = ({children, farsideColumn}) => <div className={`${namespace}__column ${farsideColumn && `${namespace}__column--farside` }`}>{children}</div>;
+const CardTitle = ({children}) => <h2 className={`${namespace}__title`}>{children}</h2>;
+const CardDescription = ({children}) => <p className={`${namespace}__description`}>{children}</p>;
+const CardLabel = ({children}) => <label className={`${namespace}__label`}>{children}</label>;
+
+export {
+	Card as default,
+	CardColumn,
+	CardTitle,
+	CardDescription,
+	CardLabel
+}
--- packages/react/src/components/graph/card/card.stories.js
+++ packages/react/src/components/graph/card/card.stories.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Card from './card';
-import { OverflowMenu, OverflowMenuItem } from 'carbon-components-react';
-
-import { User16 } from '@carbon/icons-react';
+import Card, { CardColumn, CardDescription, CardLabel, CardTitle } from './card';
+import { User16, ChevronDown16 } from '@carbon/icons-react';
 
 const stories = storiesOf('Experimental|Card', module);
 stories.addDecorator((story) => (
@@ -11,33 +9,89 @@ stories.addDecorator((story) => (
 ));
 
 stories.add('Default', () => (
-	<Card title={'Title'} description={'Description'} />
+	<Card>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
 ));
 
 stories.add('Stacked', () => (
-	<Card title={'Title'} description={'Description'} stacked />
+	<Card stacked>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
 ));
 
 stories.add('Color', () => (
-	<Card color={'#8a3ffc'} title={'Title'} description={'Description'} renderIcon={<User16 />} />
+	<Card color={'#8a3ffc'} >
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
 ));
 
 stories.add('With icon', () => (
-	<Card title={'Title'} description={'Description'} renderIcon={<User16 />} />
+	<Card>
+		<CardColumn>
+			<User16 />
+		</CardColumn>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
+));
+
+stories.add('As button', () => (
+	<Card onClick={()=> {}}>
+		<CardColumn>
+			<User16 />
+		</CardColumn>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
+));
+
+stories.add('As link', () => (
+	<Card href="#">
+		<CardColumn>
+			<User16 />
+		</CardColumn>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+	</Card>
 ));
 
 stories.add('With label', () => (
-	<Card title={'Title'} description={'Description'} label={'Label'} />
+	<Card>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+			<CardLabel>Label</CardLabel>
+		</CardColumn>
+	</Card>
 ));
 
-stories.add('With action', () => (
-	<Card title={'Title'} description={'Description'} label={'Label'} renderAction={
-	<OverflowMenu>
-		<OverflowMenuItem itemText="Stop app" />
-		<OverflowMenuItem itemText="Restart app" />
-		<OverflowMenuItem itemText="Rename app" />
-		<OverflowMenuItem itemText="Edit routes and access" requireTitle />
-		<OverflowMenuItem hasDivider isDelete itemText="Delete app" />
-	</OverflowMenu>
-	} />
+stories.add('With third column', () => (
+	<Card>
+		<CardColumn>
+			<User16 />
+		</CardColumn>
+		<CardColumn>
+			<CardTitle>Title</CardTitle>
+			<CardDescription>Description</CardDescription>
+		</CardColumn>
+		<CardColumn farsideColumn>
+			<ChevronDown16 />
+		</CardColumn>
+	</Card>
 ));
