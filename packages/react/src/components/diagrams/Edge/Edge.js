082ab2068081837ae886d08c4c446749f5dad188
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2021-06-05 11:04:37 +0100

    chore: specify prop types for edge

--- packages/react/src/components/diagrams/Edge/Edge.js
+++ packages/react/src/components/diagrams/Edge/Edge.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import settings from 'carbon-components/src/globals/js/settings';
 import { straight } from './buildPath';
 import classnames from 'classnames';
 
 const { prefix } = settings;
 
-export default ({
+const Edge = ({
+	color,
 	markerEnd,
 	markerStart,
 	path,
 	source,
-	color,
 	target,
 	variant = null,
 }) => {
@@ -29,4 +30,49 @@ export default ({
 			<path d={d} className={`${namespace}__inner`} markerEnd={`url(#${markerEnd})`} markerStart={`url(#${markerStart})`} style={{ stroke: color }} />
 		</g>
 	);
+};
+
+export default Edge;
+
+Edge.propTypes = {
+	/**
+	* Specify the edge's color
+	*/
+	color: PropTypes.string,
+
+	/**
+	* Specify an ID for a corresponding end marker
+	*/
+	markerEnd: PropTypes.string,
+
+	/**
+	* Specify an ID for a corresponding start marker
+	*/
+	markerStart: PropTypes.string,
+
+	/**
+	* Specify a path string
+	*/
+	path: PropTypes.string,
+
+	/**
+	* Coordinates for the source element
+	*/
+	source: PropTypes.shape({
+		x: PropTypes.number,
+		y: PropTypes.number
+	}),
+
+	/**
+	* Coordinates for the target element
+	*/
+	target: PropTypes.shape({
+		x: PropTypes.number,
+		y: PropTypes.number
+	}),
+
+	/**
+	* Specify the variant of the edge
+	*/
+	variant: PropTypes.oneOf(['dash-sm', 'dash-md', 'dash-lg', 'dash-xl', 'double', 'tunnel']),
 };
