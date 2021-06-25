e04488fde10f5087c9ae600ce366c433fc700654
Eliad Moosavi <iliadmoosavi@gmail.com>
2021-06-25 11:19:23 -0400

    fix angular diagram iframe example

--- packages/angular/stories/diagrams/0_diagram.stories.ts
+++ packages/angular/stories/diagrams/0_diagram.stories.ts
@@ -78,13 +78,7 @@ stories.add('Start here', () => ({
 
 			<iframe
 				src="https://codesandbox.io/embed/carbon-charts-react-elkjs-diagram-b9xyp?fontsize=14&hidenavigation=1&theme=dark&view=preview"
-				style={{
-					width: '100%',
-					height: '500px',
-					border: 0,
-					borderRadius: '4px',
-					overflow: 'hidden',
-				}}
+				style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden;"
 				title="carbon-charts-react-elkjs-diagram"
 				allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
 				sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
