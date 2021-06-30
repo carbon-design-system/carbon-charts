c359996bdde27632efa44ee2ecfb6a6d974d99df
Eliad Moosavi <iliadmoosavi@gmail.com>
2021-06-29 21:16:12 -0400

    remove plex dependency

--- packages/core/build-vendor.sh
+++ packages/core/build-vendor.sh
@@ -9,6 +9,3 @@ cp -r ../../node_modules/@carbon/type src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
 cp -r ../../node_modules/carbon-components src/styles/vendor
-
-# plex
-cp -r ../../node_modules/@ibm/plex src/styles/vendor
