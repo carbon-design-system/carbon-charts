f32180774720c7d916f6bd441b7d72500b77e7f9
Akshat55 <38994122+Akshat55@users.noreply.github.com>
2021-07-13 13:33:42 -0400

    fix(core): display the first and last data elements when addSpaceOnEdges is set to 0 (#1064)
    fix #763
    
    Co-authored-by: Akshat Patel <Akshat.Patel@ibm.com>

--- packages/core/src/components/graphs/scatter.ts
+++ packages/core/src/components/graphs/scatter.ts
@@ -46,8 +46,8 @@ export class Scatter extends Component {
 		if (zoomDomain !== undefined) {
 			return data.filter(
 				(d) =>
-					d[domainIdentifier].getTime() > zoomDomain[0].getTime() &&
-					d[domainIdentifier].getTime() < zoomDomain[1].getTime()
+					d[domainIdentifier].getTime() >= zoomDomain[0].getTime() &&
+					d[domainIdentifier].getTime() <= zoomDomain[1].getTime()
 			);
 		}
 		return data;
@@ -447,7 +447,7 @@ export class Scatter extends Component {
 									d[groupMapsTo],
 									d[domainIdentifier],
 									d
-								)
+								);
 							}
 							return null;
 						});
