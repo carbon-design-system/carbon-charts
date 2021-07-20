621727515b2b046435d15a7355f634610fb65414
Eliad Moosavi <iliadmoosavi@gmail.com>
2021-07-20 18:17:55 -0400

    fix(core): remove references to process in UMD bundles

--- packages/core/demo/data/index.ts
+++ packages/core/demo/data/index.ts
@@ -1113,7 +1113,7 @@ const mapDemoGroups = (demoGroups) =>
 allDemoGroups = mapDemoGroups(allDemoGroups);
 
 // Only add the high-scale testcases in dev
-if (process.env.NODE_ENV !== 'production') {
+if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'production') {
 	allDemoGroups = allDemoGroups.concat(mapDemoGroups(devOnlyDemoGroups));
 }
 
--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,6 +3,7 @@ import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 
 export default {
 	input: './src/index.ts',
@@ -24,6 +25,9 @@ export default {
 		},
 	},
 	plugins: [
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+		}),
 		resolve(),
 		commonjs(),
 		json(),
