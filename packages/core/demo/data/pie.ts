4d061df2de75e6bcc889460b81b8b1d74f71da0f
Akshat Patel <Akshat.Patel@ibm.com>
2021-08-19 11:48:31 -0400

    docs(core): add examples for mapping value to another key in pie and donut charts
    fix #1110

--- packages/core/demo/data/pie.ts
+++ packages/core/demo/data/pie.ts
@@ -25,6 +25,24 @@ export const pieCenteredOptions = {
 	},
 };
 
+// pie - using a different value key
+export const pieDataMapsTo = [
+	{ group: '2V2N 9KYPM version 1', count: 28000 },
+	{ group: 'L22I P66EP L22I P66EP L22I P66EP', count: 65000 },
+	{ group: 'JQAI 2M4L1', count: 75000 },
+	{ group: 'J9DZ F37AP', count: 3200 },
+	{ group: 'YEL48 Q6XK YEL48', count: 15000 },
+	{ group: 'Misc', count: 25000 },
+];
+
+export const pieMapToOptions = {
+	title: 'Pie (value maps to count)',
+	resizable: true,
+	pie: {
+		mapsTo: 'count',
+	},
+};
+
 // pie - empty state
 export const pieEmptyStateData = [];
 export const pieEmptyStateOptions = {
