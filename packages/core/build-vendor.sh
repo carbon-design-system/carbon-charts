3811e952c735cee395836ecde45411f61ef7c37e
theiliad <iliadmoosavi@gmail.com>
2021-10-20 18:03:58 -0400

    fix(core): hCM issues in the legend component

--- packages/core/build-vendor.sh
+++ packages/core/build-vendor.sh
@@ -8,4 +8,5 @@ cp -r ../../node_modules/@carbon/motion src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/type src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/themes src/styles/vendor/@carbon
 cp -r ../../node_modules/@carbon/colors src/styles/vendor/@carbon
+cp -r ../../node_modules/@carbon/styles src/styles/vendor/@carbon
 cp -r ../../node_modules/carbon-components src/styles/vendor
