3e7fcc2f16d16d67c4f39f4d4389878b5d829aba
Eric Liu <ericyl.us@gmail.com>
2021-11-18 12:12:18 -0800

    feat(svelte): add TypeScript definitions for Alluvial, Histogram, and Tree charts (#1219)
    * feat(svelte): add Alluvial Chart types
    
    * feat(svelte): add Histogram Chart types
    
    * feat(svelte): make BaseChart data property generic
    
    * feat(svelte): add Tree Chart types

---
+++ packages/svelte/types/AlluvialChart.d.ts
@@ -0,0 +1,5 @@
+import { AlluvialChart as AC } from "@carbon/charts";
+import type { AlluvialChartOptions } from "@carbon/charts/interfaces";
+import BaseChart from "./BaseChart";
+
+export default class AlluvialChart extends BaseChart<AC, AlluvialChartOptions> {}
