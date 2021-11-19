f2170b15b78797e0fcced2367b5b60c2b4e3a332
Akshat55 <38994122+Akshat55@users.noreply.github.com>
2021-11-19 17:57:27 -0500

    fix(core): add linear gradient support to alluvial links (#1192)
    * fix(core): add linear gradient support to alluvial links
    
    fix #1189
    
    * docs(core): add gradient alluvial example
    
    * Update colors to use carbon color palette
    
    * Update to add linear gradient and path to single group
    
    * Update alluvial gradient demo
    
    * Replace alluvial main demo
    
    * Remove data points
    
    * Clean up data

--- packages/core/demo/data/alluvial.ts
+++ packages/core/demo/data/alluvial.ts
@@ -1,28 +1,158 @@
 export const alluvialSimpleData = [
-	{ source: 'A', target: 'X', value: 3 },
-	{ source: 'A', target: 'Y', value: 5 },
-	{ source: 'A', target: 'Z', value: 8 },
-	{ source: 'B', target: 'X', value: 6 },
-	{ source: 'B', target: 'Y', value: 1 },
-	{ source: 'B', target: 'Z', value: 7 },
-	{ source: 'C', target: 'X', value: 5 },
-	{ source: 'C', target: 'Y', value: 5 },
-	{ source: 'C', target: 'Z', value: 1 },
+	{
+		source: 'About Modal',
+		target: 'Data and AI, AI Apps',
+		value: 5,
+	},
+	{
+		source: 'About Modal',
+		target: 'Data and AI, Info Architecture',
+		value: 4,
+	},
+	{
+		source: 'About Modal',
+		target: 'Public Cloud',
+		value: 3,
+	},
+	{
+		source: 'About Modal',
+		target: 'Security',
+		value: 4,
+	},
+	{
+		source: 'About Modal',
+		target: 'Automation',
+		value: 8,
+	},
+	{
+		source: 'Cards',
+		target: 'Data and AI, AI Apps',
+		value: 6,
+	},
+	{
+		source: 'Cards',
+		target: 'Data and AI, Info Architecture',
+		value: 15,
+	},
+	{
+		source: 'Cards',
+		target: 'Public Cloud',
+		value: 2,
+	},
+	{
+		source: 'Cards',
+		target: 'Security',
+		value: 10,
+	},
+	{
+		source: 'Cards',
+		target: 'Automation',
+		value: 13,
+	},
+	{
+		source: 'Create Flow',
+		target: 'Data and AI, AI Apps',
+		value: 2,
+	},
+	{
+		source: 'Create Flow',
+		target: 'Data and AI, Info Architecture',
+		value: 15,
+	},
+	{
+		source: 'Create Flow',
+		target: 'Public Cloud',
+		value: 1,
+	},
+	{
+		source: 'Create Flow',
+		target: 'Security',
+		value: 6,
+	},
+	{
+		source: 'Create Flow',
+		target: 'Automation',
+		value: 15,
+	},
+	{
+		source: 'Notifications',
+		target: 'Data and AI, Info Architecture',
+		value: 14,
+	},
+	{
+		source: 'Notifications',
+		target: 'Public Cloud',
+		value: 3,
+	},
+	{
+		source: 'Notifications',
+		target: 'Security',
+		value: 3,
+	},
+	{
+		source: 'Page Header',
+		target: 'Data and AI, AI Apps',
+		value: 4,
+	},
+	{
+		source: 'Page Header',
+		target: 'Data and AI, Info Architecture',
+		value: 8,
+	},
+	{
+		source: 'Page Header',
+		target: 'Automation',
+		value: 13,
+	},
 ];
 
 export const alluvialSimpleOptions = {
 	title: 'Alluvial',
 	alluvial: {
-		units: 'GB',
 		nodes: [
-			{ name: 'A', category: 'Start' },
-			{ name: 'B', category: 'Start' },
-			{ name: 'C', category: 'Start' },
-			{ name: 'X', category: 'Finish' },
-			{ name: 'Y', category: 'Finish' },
-			{ name: 'Z', category: 'Finish' },
+			{
+				name: 'About Modal',
+				category: 'Pattern',
+			},
+			{
+				name: 'Cards',
+				category: 'Pattern',
+			},
+			{
+				name: 'Create Flow',
+				category: 'Pattern',
+			},
+			{
+				name: 'Page Header',
+				category: 'Pattern',
+			},
+			{
+				name: 'Notifications',
+				category: 'Pattern',
+			},
+			{
+				name: 'Data and AI, AI Apps',
+				category: 'Group',
+			},
+			{
+				name: 'Data and AI, Info Architecture',
+				category: 'Group',
+			},
+			{
+				name: 'Public Cloud',
+				category: 'Group',
+			},
+			{
+				name: 'Security',
+				category: 'Group',
+			},
+			{
+				name: 'Automation',
+				category: 'Group',
+			},
 		],
 	},
+	height: '600px',
 };
 
 export const alluvialSimpleCustomColorOptions = {
@@ -33,6 +163,30 @@ export const alluvialSimpleCustomColorOptions = {
 			A: '#d12771',
 			B: '#08bdba',
 			C: '#6fdc8c',
+		},
+	},
+};
+
+export const alluvialGradientData = alluvialSimpleData;
+
+export const alluvialSimpleGradientOptions = {
+	...alluvialSimpleOptions,
+	title: 'Alluvial (gradient)',
+	color: {
+		scale: {
+			Cards: '#da1e28',
+			'About Modal': '#b28600',
+			'Create Flow': '#198038',
+			'Page Header': '#ee538b',
+			Notifications: '#08bdba',
+			'Data and AI, AI Apps': '#1192e8',
+			'Data and AI, Info Architecture': '#a56eff',
+			Security: '#009d9a',
+			Automation: '#fa4d56',
+			'Public Cloud': '#198038',
+		},
+		gradient: {
+			enabled: true,
 		},
 	},
 };
@@ -127,7 +281,17 @@ export const alluvialMultipleCategoryData = [
 	},
 ];
 
-export const alluvialMonochromeData = alluvialSimpleData;
+export const alluvialMonochromeData = [
+	{ source: 'A', target: 'X', value: 3 },
+	{ source: 'A', target: 'Y', value: 5 },
+	{ source: 'A', target: 'Z', value: 8 },
+	{ source: 'B', target: 'X', value: 6 },
+	{ source: 'B', target: 'Y', value: 1 },
+	{ source: 'B', target: 'Z', value: 7 },
+	{ source: 'C', target: 'X', value: 5 },
+	{ source: 'C', target: 'Y', value: 5 },
+	{ source: 'C', target: 'Z', value: 1 },
+];
 
 export const alluvialMonochromeOptions = {
 	title: 'Alluvial (monochrome with custom node padding)',
