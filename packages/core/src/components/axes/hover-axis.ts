fa4be7e09f9049cf53bd793cd179d2f469d079f0
Akshat Patel <akshat@live.ca>
2021-11-28 03:05:36 -0500

    Improve hover accessibility

--- packages/core/src/components/axes/hover-axis.ts
+++ packages/core/src/components/axes/hover-axis.ts
@@ -1,21 +1,12 @@
 // Internal Imports
 import { Axis } from './axis';
-import {
-	AxisPositions,
-	Events,
-	ScaleTypes,
-	Roles,
-	TruncationTypes,
-} from '../../interfaces';
+import { AxisPositions, Events, ScaleTypes } from '../../interfaces';
 import { Tools } from '../../tools';
 import { ChartModel } from '../../model/model';
 import { DOMUtils } from '../../services';
-import { AxisTitleOrientations, TickRotations } from '../../interfaces/enums';
-import * as Configuration from '../../configuration';
 
 // D3 Imports
 import { select } from 'd3-selection';
-import { axisBottom, axisLeft, axisRight, axisTop } from 'd3-axis';
 
 export class HoverAxis extends Axis {
 	constructor(model: ChartModel, services: any, configs?: any) {
@@ -31,15 +22,17 @@ export class HoverAxis extends Axis {
 			`g.axis.${axisPosition}`
 		);
 
+		container.selectAll('g.ticks').attr('tabindex', 0);
+
 		container.selectAll('g.tick').each(function () {
 			const g = select(this);
-			g.classed('tick-hover', true).attr('tabindex', 0);
+			g.classed('tick-hover', true).attr('tabindex', -1);
 			const textNode = g.select('text');
 			const { width, height } = DOMUtils.getSVGElementSize(textNode, {
 				useBBox: true,
 			});
 
-			const rectangle = DOMUtils.appendOrSelect(g, `rect.axis-holder`);
+			const rectangle = DOMUtils.appendOrSelect(g, 'rect.axis-holder');
 
 			let x = 0,
 				y = 0;
@@ -72,8 +65,70 @@ export class HoverAxis extends Axis {
 			rectangle.lower();
 		});
 
+		const self = this;
+
+		container.selectAll('g.ticks').on('focus', function () {
+			const axis = select(this);
+			this.blur();
+
+			if (!axis.classed('invisible')) {
+				// Set focus on intial value in the axis
+				axis.select('g.tick').dispatch('focus');
+
+				axis.selectAll('g.tick').on(
+					'keydown',
+					function (event: KeyboardEvent) {
+						// Choose specific arrow key depending on the axis
+						if (
+							axisPosition === AxisPositions.LEFT ||
+							axisPosition === AxisPositions.RIGHT
+						) {
+							if (event.key && event.key === 'ArrowUp') {
+								self.goNext(this as HTMLElement, event);
+							} else if (event.key && event.key === 'ArrowDown') {
+								self.goPrevious(this as HTMLElement, event);
+							}
+						} else {
+							if (event.key && event.key === 'ArrowLeft') {
+								self.goPrevious(this as HTMLElement, event);
+							} else if (
+								event.key &&
+								event.key === 'ArrowRight'
+							) {
+								self.goNext(this as HTMLElement, event);
+							}
+						}
+					}
+				);
+			}
+		});
+
 		// Add event listeners to elements drawn
 		this.addEventListeners();
+	}
+
+	// Focus on the next HTML element sibling
+	private goNext(element: HTMLElement, event: Event) {
+		if (
+			element.nextElementSibling !== null &&
+			element.nextElementSibling.tagName !== 'path'
+		) {
+			element.nextElementSibling.dispatchEvent(new Event('focus'));
+		}
+
+		event.preventDefault();
+	}
+
+	// Focus on the previous HTML element sibling
+	private goPrevious(element: HTMLElement, event: Event) {
+		if (
+			element.previousElementSibling !== null &&
+			element.previousElementSibling.tagName !== 'path'
+		) {
+			element.previousElementSibling.dispatchEvent(new Event('focus'));
+		}
+
+		event.preventDefault();
 	}
 
 	addEventListeners() {
@@ -131,7 +186,6 @@ export class HoverAxis extends Axis {
 					axisScaleType === ScaleTypes.LABELS &&
 					datum.length > truncationThreshold
 				) {
-					console.log('inside');
 					self.services.events.dispatchEvent(Events.Tooltip.MOVE, {
 						event,
 					});
@@ -162,6 +216,8 @@ export class HoverAxis extends Axis {
 		container
 			.selectAll('g.tick.tick-hover')
 			.on('focus', function (event) {
+				// Focus element since we are using arrow keys
+				event.target.focus();
 				// Dispatch mouse event
 				self.services.events.dispatchEvent(
 					Events.Axis.LABEL_MOUSEOVER,
