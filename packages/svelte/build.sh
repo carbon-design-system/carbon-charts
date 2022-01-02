e8bcaf538367e2ab1f9d43a965d84cc957bd44ac
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-01-01 16:26:28 -0800

    remove rollup from build file

--- packages/svelte/build.sh
+++ packages/svelte/build.sh
@@ -3,14 +3,12 @@
 set -e
 
 rm -rf dist
+mkdir dist
 
 echo "linking local packages (non-lerna)"
 # we run this outside of lerna, since lerna seems to have issues with correctly linking to the dist
 rm -f node_modules/@carbon/charts
 ln -sf $(pwd)/../core/dist node_modules/@carbon/charts
-
-echo "bundling..."
-rollup -c
 
 # copy src directory for Svelte entry
 cp -r src/ dist/src
