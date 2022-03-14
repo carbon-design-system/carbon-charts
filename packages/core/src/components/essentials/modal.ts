f79c4ac389bf3c1456ff92aa374669a26c743d63
Eliad Moosavi <theiliad@users.noreply.github.com>
2022-03-14 16:30:07 -0400

    feat(core): add v11 classnames to modal & overflow menu (#1329)

--- packages/core/src/components/essentials/modal.ts
+++ packages/core/src/components/essentials/modal.ts
@@ -62,30 +62,35 @@ export class Modal extends Component {
 		const tableArray = this.model.getTabularDataArray();
 
 		return `
-		<div class="bx--modal-container">
-			<div class="bx--modal-header">
-				<p class="bx--modal-header__label bx--type-delta" id="modal-title">Tabular representation</p>
-				<p class="bx--modal-header__heading bx--type-beta" id="modal-description">${
+		<div class="bx--modal-container cds--modal-container">
+			<div class="bx--modal-header cds--modal-header">
+				<p class="bx--modal-header__label bx--type-delta cds--modal-header__label cds--type-delta" id="modal-title">Tabular representation</p>
+
+				<p class="bx--modal-header__heading bx--type-beta cds--modal-header__heading cds--type-beta" id="modal-description">${
 					options.title
 				}</p>
-				<button class="bx--modal-close" type="button" data-modal-close aria-label="close modal"  data-modal-primary-focus>
-					<svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Close" width="20" height="20" viewBox="0 0 32 32" role="img" class="bx--modal-close__icon">
+
+				<button class="bx--modal-close cds--modal-close" type="button" data-modal-close aria-label="close modal"  data-modal-primary-focus>
+					<svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Close" width="20" height="20" viewBox="0 0 32 32" role="img" class="bx--modal-close__icon cds--modal-close__icon">
 						<path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4z"></path>
 					</svg>
 				</button>
 			</div>
-			<div class="bx--modal-content"><table class="bx--data-table bx--data-table--no-border">
+
+			<div class="bx--modal-content cds--modal-content">
+				<table class="bx--data-table bx--data-table--no-border cds--data-table cds--data-table--no-border">
 					<thead>
 						<tr>
 							${get(tableArray, 0)
 								.map(
 									(heading) => `<th scope="col">
-								<div class="bx--table-header-label">${heading}</div>
+								<div class="bx--table-header-label cds--table-header-label">${heading}</div>
 							</th>`
 								)
 								.join('')}
 						</tr>
 					</thead>
+
 					<tbody>${tableArray
 						.slice(1)
 						.map(
@@ -98,9 +103,10 @@ export class Modal extends Component {
 					</tbody>
 				</table>
 			</div>
-			<div class="bx--modal-footer">
+
+			<div class="bx--modal-footer cds--modal-footer">
 			  <div class="${settings.prefix}--${chartprefix}-modal-footer-spacer"></div>
-			  <button class="bx--btn bx--btn--primary" type="button" data-modal-primary-focus>Download as CSV</button>
+			  <button class="bx--btn bx--btn--primary cds--btn cds--btn--primary" type="button" data-modal-primary-focus>Download as CSV</button>
 			</div>
 		</div>`;
 	}
@@ -120,7 +126,7 @@ export class Modal extends Component {
 			this.isEventListenerAdded = true;
 			this.modal
 				.attr('data-modal', true)
-				.attr('class', 'bx--modal')
+				.attr('class', 'bx--modal cds--modal')
 				.attr('role', 'dialog')
 				.attr('aria-modal', true)
 				.attr('aria-labelledby', 'modal-title')
