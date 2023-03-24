635e0f101c338849752bc3295e5734b2b631074b
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-03-24 18:14:05 -0400

    fix(netlify): force package build to ensure consistency. Also no need to emit declarations for demos

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -16,10 +16,10 @@ if [ $CONTEXT == "deploy-preview" ]; then
   # Build core package, demo and demo data
 	lerna run build --scope=@carbon/charts
 
-	# Build package demo if not core
+	# Build package and demo if not core
 	if [ "$APP_TYPE" != "core" ]; then
 	  PKG_DIR="charts-$APP_TYPE"
-		lerna run build:demo --scope="@carbon/$PKG_DIR"
+		lerna run build --scope="@carbon/$PKG_DIR"
   else
 	  # Map package directory for core since name is different
 	  PKG_DIR="charts"
