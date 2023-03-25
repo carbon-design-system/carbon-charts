89e4c465ba38d55468050402081ad74fc97f7a9b
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-03-25 10:13:15 -0400

    fix(lerna): --env deprecated

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -2,7 +2,6 @@ set -e
 
 if [ $CONTEXT == "deploy-preview" ]; then
 	echo "Deploying preview to Netlify for PR..."
-	NODE_ENV=deploypreview
 
 	# Grab netlify app type from the netlify app URL
 	APP_TYPE=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
@@ -15,12 +14,12 @@ if [ $CONTEXT == "deploy-preview" ]; then
 	# vue -> charts-vue
 
   # Build core package, demo and data
-	lerna run build --env "$NODE_ENV" --scope=@carbon/charts
+	NODE_ENV=deploypreview lerna run build --scope=@carbon/charts
 
 	# Build package and demo if not core
 	if [ "$APP_TYPE" != "core" ]; then
 	  PKG_DIR="charts-$APP_TYPE"
-		lerna run build --env "$NODE_ENV" --scope="@carbon/$PKG_DIR"
+		NODE_ENV=deploypreview lerna run build --scope="@carbon/$PKG_DIR"
   else
 	  # Map package directory for core since name is different
 	  PKG_DIR="charts"
