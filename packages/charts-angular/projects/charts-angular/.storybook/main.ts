1d5dfe2c807d4ecb4ad2e9a692358cf63423b0f1
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-03-25 12:59:24 -0400

    test netlify mem setting

--- packages/charts-angular/projects/charts-angular/.storybook/main.ts
+++ packages/charts-angular/projects/charts-angular/.storybook/main.ts
@@ -1,55 +1,55 @@
 import type { StorybookConfig } from '@storybook/angular'
 import { resolve } from 'path'
-
 const core = '../../../../charts'
 const demoDist = resolve(__dirname, `${core}/dist/demo`)
 
 const config: StorybookConfig = {
-	stories: [
-		'../src/**/*.mdx',
-		'../src/**/*.stories.@(js|jsx|ts|tsx)',
-		`${core}/stories/tutorials/!(0-api)*.stories.mdx`,
-		`${core}/stories/getting-started/angular.stories.mdx`
-	],
-	addons: [
-		{
-			name: '@storybook/addon-essentials',
-			options: {
-				actions: false,
-				backgrounds: false,
-				controls: true,
-				docs: true,
-				viewport: false,
-				toolbars: true,
-				measure: false,
-				outline: false
-			}
-		}
-	],
-	framework: {
-		name: '@storybook/angular',
-		options: {}
-	},
-	docs: {
-		autodocs: 'tag'
-	},
-	staticDirs: ['assets'],
-	features: {
-		storyStoreV7: false
-	},
-	webpackFinal: async (config, { configType }) => {
-		config?.module?.rules?.push({
-			test: /\.css$/,
-			use: ['style-loader', 'css-loader'],
-			include: [demoDist],
-		})
-
-		config.resolve!.alias = {
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    `${core}/stories/tutorials/!(0-api)*.stories.mdx`,
+    `${core}/stories/getting-started/angular.stories.mdx`
+  ],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+        backgrounds: false,
+        controls: true,
+        docs: true,
+        viewport: false,
+        toolbars: true,
+        measure: false,
+      outline: false
+    }
+    },
+    '@storybook/addon-mdx-gfm'
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  staticDirs: ['assets'],
+  features: {
+    storyStoreV7: false
+  },
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    config?.module?.rules?.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      include: [demoDist]
+    })
+    config.resolve!.alias = {
       ...config.resolve!.alias,
       '@carbon/charts/demo': demoDist
     }
-
-		return config
-	}
+    return config
+  }
 }
 export default config
--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -19,7 +19,7 @@ if [ $CONTEXT == "deploy-preview" ]; then
 	# Build package and demo if not core
 	if [ "$APP_TYPE" != "core" ]; then
 	  PKG_DIR="charts-$APP_TYPE"
-		NODE_ENV=deploypreview NODE_OPTIONS=--max-old-space-size=4096 lerna run build --scope="@carbon/$PKG_DIR"
+		NODE_ENV=deploypreview lerna run build --scope="@carbon/$PKG_DIR"
   else
 	  # Map package directory for core since name is different
 	  PKG_DIR="charts"
