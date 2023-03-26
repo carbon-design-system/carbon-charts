31eb413da9ba6b9c85ad83ddf5d74b99d35935eb
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-03-26 13:13:07 -0400

    chore(netlify): see if yarn uses less mem

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,7 @@
 set -e
 
 if [ $CONTEXT == "deploy-preview" ]; then
-	echo "Deploying preview to Netlify for PR..."
+	echo -e "\033[0;32mDeploying preview to Netlify for PR..."
 
 	# Grab netlify app type from the netlify app URL
 	APP_TYPE=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
@@ -13,25 +13,28 @@ if [ $CONTEXT == "deploy-preview" ]; then
 	# svelte -> charts-svelte
 	# vue -> charts-vue
 
-	echo "Building @carbon/charts and demo dependencies (styles, data)..."
-	NODE_ENV=deploypreview lerna run build:package --scope=@carbon/charts --concurrency=1
+	echo -e "\033[0;32mBuilding @carbon/charts and demo dependencies (styles, data)..."
+	lerna run build:package --scope=@carbon/charts --concurrency=1
 
 	# Build package if not core and map APP_TYPE to PKG_DIR
 	if [ "$APP_TYPE" != "core" ]; then
 	  PKG_DIR="charts-$APP_TYPE"
-		echo "Building @carbon/$PKG_DIR..."
-		NODE_ENV=deploypreview lerna run build:package --scope="@carbon/$PKG_DIR" --concurrency=1
+		echo -e "\033[0;32mBuilding @carbon/$PKG_DIR..."
+		lerna run build:package --scope="@carbon/$PKG_DIR" --concurrency=1
   else
 	  # Map package directory for core since name is different
 	  PKG_DIR="charts"
 	fi
 
-  echo "Running storybook build for @carbon/$PKG_DIR..."
-	NODE_ENV=deploypreview lerna run build:demo --scope="@carbon/$PKG_DIR" --concurrency=1
+  echo -e "\033[0;32mRunning storybook build for @carbon/$PKG_DIR..."
+	cd "packages/$PKG_DIR
+	yarn build:demo
+	cd ../..
+	#NODE_ENV=production lerna run build:demo --scope="@carbon/$PKG_DIR" --concurrency=1
 
-	echo "Copying packages/$PKG_DIR/demo/bundle to pages/..."
+	echo -e "\033[0;32mCopying packages/$PKG_DIR/demo/bundle to pages/..."
 	mkdir -p pages
 	cp -a "packages/$PKG_DIR/demo/bundle/." pages
 else
-	echo "Not a PR... nothing to do."
+	echo -e "\033[0;32mNot a PR... nothing to do."
 fi
