498f45e1a0a8ab0615f2393f4b32c85892becf07
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-03-26 12:47:17 -0400

    chore(netlify): improve logging

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -13,26 +13,25 @@ if [ $CONTEXT == "deploy-preview" ]; then
 	# svelte -> charts-svelte
 	# vue -> charts-vue
 
-  # Build core package and demo dependencies (styles, data)
+	echo "Building @carbon/charts and demo dependencies (styles, data)..."
 	NODE_ENV=deploypreview lerna run build:package --scope=@carbon/charts --concurrency=1
 
 	# Build package if not core and map APP_TYPE to PKG_DIR
 	if [ "$APP_TYPE" != "core" ]; then
 	  PKG_DIR="charts-$APP_TYPE"
+		echo "Building @carbon/$PKG_DIR..."
 		NODE_ENV=deploypreview lerna run build:package --scope="@carbon/$PKG_DIR" --concurrency=1
   else
 	  # Map package directory for core since name is different
 	  PKG_DIR="charts"
 	fi
 
-  # Build demo
+  echo "Running storybook build for @carbon/$PKG_DIR..."
 	NODE_ENV=deploypreview lerna run build:demo --scope="@carbon/$PKG_DIR" --concurrency=1
 
-	# Create folder we'll deploy to netlify
+	echo "Copying packages/$PKG_DIR/demo/bundle to pages/..."
 	mkdir -p pages
-
-  # Copy demo bundle from package (even core) to pages
 	cp -a "packages/$PKG_DIR/demo/bundle/." pages
 else
-	echo "Not a PR (do nothing)"
+	echo "Not a PR... nothing to do."
 fi
