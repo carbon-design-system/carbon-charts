cd1a81924d274210d7a318cc9dca703d24b4941c
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-04-01 09:19:00 -0400

    chore(charts-vue): better typing for base

--- packages/charts-vue/src/components/CcvBaseChart.vue
+++ packages/charts-vue/src/components/CcvBaseChart.vue
@@ -2,30 +2,36 @@
 import { defineComponent } from 'vue'
 import type { ChartTabularData } from '@carbon/charts'
 
+export interface CoreChart<T> {
+  model: {
+    setData: (data: ChartTabularData) => void;
+    setOptions: (options: T) => void;
+  } | null;
+}
 
 export default defineComponent({
-	data() {
-		return {
-			coreChart: null as any | null // would be improved with a base type
-		}
-	},
-	props: {
-		data: { type: Object as () => ChartTabularData, required: true },
-		options: { type: Object, required: true }
-	},
-	watch: {
-		data: {
-			handler: function (newData) {
-				this.coreChart?.model.setData(newData)
-			},
-			deep: true
-		},
-		options: {
-			handler: function (newOptions) {
-				this.coreChart?.model.setOptions(newOptions)
-			},
-			deep: true
-		}
-	}
+  data() {
+    return {
+      coreChart: null as CoreChart<any> | null
+    }
+  },
+  props: {
+    data: { type: Object as () => ChartTabularData, required: true },
+    options: { type: Object, required: true }
+  },
+  watch: {
+    data: {
+      handler: function (newData: ChartTabularData) {
+        this.coreChart?.model.setData(newData)
+      },
+      deep: true
+    },
+    options: {
+      handler: function (newOptions) {
+        this.coreChart?.model.setOptions(newOptions)
+      },
+      deep: true
+    }
+  }
 })
 </script>
