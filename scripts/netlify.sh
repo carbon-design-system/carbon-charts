99c8e4e1c084006bf9f1158b09d40c584ba224d8
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-05-03 21:05:31 -0400

    fix(netlify): script

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -6,25 +6,25 @@ if [ $CONTEXT == "deploy-preview" ]; then
 
 	# Grab netlify app type from the netlify app URL
 	# e.g. core, angular or react (translate to ./packages/NAME
-	APP_TYPE=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
+	PKG_DIR=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
 
 	# Build core package first
 	echo -e "${GREEN}Building @carbon/charts and demo dependencies (styles, data)..."
 	npx lerna run build:package --scope=@carbon/charts --concurrency=1
 
 	# Build package if not core and map APP_TYPE to PKG_DIR
-	if [ "$APP_TYPE" != "core" ]; then
-	  PKG_DIR="charts-$APP_TYPE"
-		echo -e "${GREEN}Building @carbon/$PKG_DIR..."
-		npx lerna run build:package --scope="@carbon/$PKG_DIR" --concurrency=1
+	if [ "$PKG_DIR" != "core" ]; then
+	  PKG="charts-$PKG_DIR"
+		echo -e "${GREEN}Building @carbon/$PKG..."
+		npx lerna run build:package --scope="@carbon/$PKG" --concurrency=1
   else
 	  # Map package directory for core since name is different
-	  PKG_DIR="core"
+	  PKG="charts"
 	fi
 
 	# Storybook build for selected package
-  echo -e "${GREEN}Running storybook build for @carbon/$PKG_DIR..."
-  npx lerna run build:demo --scope="@carbon/$PKG_DIR" --concurrency=1
+  echo -e "${GREEN}Running storybook build for @carbon/$PKG..."
+  npx lerna run build:demo --scope="@carbon/$PKG" --concurrency=1
 
 	echo -e "${GREEN}Copying packages/$PKG_DIR/demo/bundle to pages/..."
 	mkdir -p pages
