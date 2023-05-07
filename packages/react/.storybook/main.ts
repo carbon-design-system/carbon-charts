d76a1b36329f9e58db3190ca688bc7813b3b804d
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-05-07 11:54:52 -0400

    test(react): disable export of Alluvial in core

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -23,26 +23,26 @@ const config: StorybookConfig = {
 			config.build.chunkSizeWarningLimit = 1800
 		}
 
-		// Remove vite:dts - no need for declarations
-		config.plugins = config.plugins!.filter((plugin) => plugin!.name !== 'vite:dts' /* || plugin!.name !== 'storybook:react-docgen-plugin'*/)
-		// config.plugins = [
-		// 	Inspect({
-    //   	build: true,
-    //   	outputDir: 'demo/bundle/inspect'
-    // 	}),
-		// 	...config.plugins
-		// ]
+	// 	// Remove vite:dts - no need for declarations
+	// 	config.plugins = config.plugins!.filter((plugin) => plugin!.name !== 'vite:dts' /* || plugin!.name !== 'storybook:react-docgen-plugin'*/)
+	// 	// config.plugins = [
+	// 	// 	Inspect({
+  //   //   	build: true,
+  //   //   	outputDir: 'demo/bundle/inspect'
+  //   // 	}),
+	// 	// 	...config.plugins
+	// 	// ]
 
-		const index = config.plugins?.findIndex(plugin => plugin?.name === 'storybook:react-docgen-plugin');
-		if (index !== -1) {
-			config.plugins[index].enforce = 'post'
-			// const targetPlugin = config.plugins?.splice(index, 1)[0]
-			// targetPlugin.enforce = 'post'
-			// config.plugins?.push(targetPlugin)
-		}
+	// 	const index = config.plugins?.findIndex(plugin => plugin?.name === 'storybook:react-docgen-plugin');
+	// 	if (index !== -1) {
+	// 		config.plugins[index].enforce = 'post'
+	// 		// const targetPlugin = config.plugins?.splice(index, 1)[0]
+	// 		// targetPlugin.enforce = 'post'
+	// 		// config.plugins?.push(targetPlugin)
+	// 	}
 
-		// In case we need to disable storybook:react-docgen-plugin
-		// config.plugins = config.plugins!.filter((plugin) => plugin!.name !== 'storybook:react-docgen-plugin')
+	// 	// In case we need to disable storybook:react-docgen-plugin
+	// 	// config.plugins = config.plugins!.filter((plugin) => plugin!.name !== 'storybook:react-docgen-plugin')
 		return config
 	},
 	addons: [
@@ -56,9 +56,15 @@ const config: StorybookConfig = {
 	framework: {
 		name: '@storybook/react-vite',
 		options: {
-			strictMode: false
 		}
 	},
+	// typescript: {
+	// 	reactDocgenTypescriptOptions: {
+	// 		exclude: [],
+	// 		shouldRemoveUndefinedFromOptional: true,
+	// 	},
+  //   reactDocgen: 'react-docgen',
+  // },
 	core: {
     // disableTelemetry: true
   },
