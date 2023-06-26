6827c72f0133340c26bfb7f8af4833e7cc93ca3f
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-06-26 15:39:03 -0400

    fix(package.jsons, deploy scripts)

--- scripts/next.sh
+++ scripts/next.sh
@@ -11,19 +11,24 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
 
 # Get git into the right state, ensure local branch is up-to-date
 git stash
-git checkout pr-1554
+git checkout issue-1579
 git pull
 
 # Use latest dependencies
 yarn install
 
-# Create next prerelease version (1.9.0-next.2, 1.9.0-next.3, etc.)
+# At this point, there could be a new yarn.lock (install-state.gz is now gitignored)
+git add yarn.lock
+git commit --allow-empty -m "chore(repo): yarn lock preventative commit"
+
+# Create next prerelease version (1.11.0-next.0, etc.)
 npx lerna version prerelease --preid next --force-publish
 
 # For angular package, copies the version to dist/package.json using ng-packagr
 yarn build
 
-node scripts/update-angular-dependency-version.cjs
+# Should not be needed
+# node scripts/update-angular-dependency-version.cjs
 
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
 
