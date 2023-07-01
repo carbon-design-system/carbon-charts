cf770cfda4bbc67dce800580f3c60472cbfc1c1d
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-07-01 15:56:29 -0400

    fix(vite): dom-to-image-more

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
 			'@carbon/utils-position',
 			'carbon-components',
 			'date-fns', // make peerDependency and externalize in next major
-			'dom-to-image',
+			'dom-to-image-more',
 			'lodash-es' // make peerDependency and externalize in next major (or replace with modern TypeScript alternatives)
 		],
 		exclude: [
