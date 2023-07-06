85cb7ca51c4b6689c5eaa69d17fa35afe4282d0e
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-07-06 11:21:01 -0400

    fix(prettier), fix(vite.config - optimize html-to-image) (#1608)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,8 @@
-module.exports = {
+import config from 'prettier-config-carbon'
+
+export default {
 	$schema: 'https://json.schemastore.org/prettierrc',
-	...require('prettier-config-carbon'), // Carbon prettier plus overrides...
+	...config,
 	semi: false,
 	tabWidth: 2,
 	singleQuote: true,
