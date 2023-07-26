31ddf30fae9700e01137760d9764abdba91ffb0d
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-07-25 23:49:53 -0400

    chore(yarn): make sure everything is hoisted by default

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,12 @@
 module.exports = {
 	$schema: 'https://json.schemastore.org/prettierrc',
 	...require('prettier-config-carbon'), // Carbon prettier plus overrides...
+	arrowParens: 'avoid',
 	semi: false,
 	tabWidth: 2,
 	singleQuote: true,
 	printWidth: 100,
 	trailingComma: 'none',
+	useTabs: true,
 	pluginSearchDirs: ['.'] // Find plugins in node_modules
 }
