9e95e5cc03c5351fa1afa5d594c5aaeafbe263c2
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-07-26 09:45:04 -0400

    chore(storybook): demo.scss had white as default theme - change to g100, remove comments

--- packages/core/.storybook/carbon-charts-theme.ts
+++ packages/core/.storybook/carbon-charts-theme.ts
@@ -4,5 +4,4 @@ export default create({
 	base: 'dark',
 	brandTitle: 'Carbon Charts - Vanilla JavaScript',
 	brandTarget: '_self'
-	// fontBase: '"IBM Plex Sans", Arial, sans-serif' // throws off Storybook's icon alignment in left navigation frame
 })
