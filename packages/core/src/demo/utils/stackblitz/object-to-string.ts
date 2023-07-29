5f9aef0443b617ba13c8bc6b0e53e210b39f4c43
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-07-29 10:19:37 -0400

    fix(object-to-string): remove import handling

--- packages/core/src/demo/utils/stackblitz/object-to-string.ts
+++ packages/core/src/demo/utils/stackblitz/object-to-string.ts
@@ -1,50 +1,50 @@
 type GenericObject = { [key: string]: any }
 
-export function objectToString(obj: GenericObject | any[] | Date, indent: number = 0, isTopLevel: boolean = true): string {
-    let str = indent === 0 ? 'export default ' : ''
-    const padding = ' '.repeat(indent * 2)
-    let importStatement = '';
+export function objectToString(
+	obj: GenericObject | any[] | Date,
+	indent: number = 0,
+	isTopLevel: boolean = true
+): string {
+	let str = isTopLevel ? 'export default ' : ''
+	const padding = ' '.repeat(indent * 2)
 
-    if (Array.isArray(obj)) {
-        str += '[\n'
+	if (Array.isArray(obj)) {
+		str += '[\n'
 
-        obj.forEach((item: any, index: number) => {
-            str += padding + '  ' + objectToString(item, indent + 1, false)
-            if (index < obj.length - 1) str += ','
-            str += '\n'
-        })
+		obj.forEach((item: any, index: number) => {
+			str += padding + '  ' + objectToString(item, indent + 1, false)
+			if (index < obj.length - 1) str += ','
+			str += '\n'
+		})
 
-        str += padding + ']'
-    } else if (typeof obj === 'object' && obj !== null) {
-        if (obj instanceof Date) {
-            str += `new Date(${obj.getFullYear()}, ${obj.getMonth()}, ${obj.getDate()})`
-        } else {
-            str += '{\n'
+		str += padding + ']'
+	} else if (typeof obj === 'object' && obj !== null) {
+		if (obj instanceof Date) {
+			str += `new Date(${obj.getFullYear()}, ${obj.getMonth()}, ${obj.getDate()})`
+		} else {
+			str += '{\n'
 
-            const keys = Object.keys(obj)
-            keys.forEach((key: string, index: number) => {
-                const formattedKey = /^[a-z$_][a-z0-9$_]*$/i.test(key) ? key : `'${key}'`
-                if (typeof obj[key] === 'function' && obj[key].name === 'timeFormat') {
-                    importStatement = 'import { timeFormat } from \'d3\'\n';
-                    str += padding + '  ' + formattedKey + ': ' + obj[key].name + '(' + obj[key].arguments + ')';
-                } else {
-                    str += padding + '  ' + formattedKey + ': ' + objectToString(obj[key], indent + 1, false)
-                }
-                if (index < keys.length - 1) str += ','
-                str += '\n'
-            })
+			const keys = Object.keys(obj)
+			keys.forEach((key: string, index: number) => {
+				const formattedKey = /^[a-z$_][a-z0-9$_]*$/i.test(key) ? key : `'${key}'`
+				str += padding + '  ' + formattedKey + ': '
+				if (typeof obj[key] === 'function') {
+					str += obj[key].toString().replace(/\\(['"`])/g, '$1') // Unescape ' , " and `
+				} else {
+					str += objectToString(obj[key], indent + 1, false)
+				}
+				if (index < keys.length - 1) str += ','
+				str += '\n'
+			})
 
-            str += padding + '}'
-        }
-    } else if (typeof obj === 'string') {
-        str += `'${obj}'`
-    } else {
-        str += obj
-    }
+			str += padding + '}'
+		}
+	} else if (typeof obj === 'string') {
+		const objStr = obj as string
+		str += `'${objStr.replace(/(['"`])/g, '\\$1')}'` // Escape ' , " and `
+	} else {
+		str += obj
+	}
 
-    if (isTopLevel) {
-        str = importStatement + str;
-    }
-
-    return str
+	return str
 }
