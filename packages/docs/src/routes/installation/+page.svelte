c59f204c9644a00bd0ac2d5573560110f8cfc33f
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-08-02 10:08:25 -0400

    docs(installation): Add information about styling for charts

--- packages/docs/src/routes/installation/+page.svelte
+++ packages/docs/src/routes/installation/+page.svelte
@@ -39,17 +39,26 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.5/d3.min.js" defer integrity="sha512-M7nHCiNUOwFt6Us3r8alutZLm9qMt4s9951uo8jqO4UwJ1hziseL6O3ndFyigx6+LREfZqnhHxYjKRJ8ZQ69DQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.min.js" defer integrity="sha512-HjKxWye8lJGPu5q1u/ZYkHlJrJdm6KGr89E6tOrXeKm1mItb1xusPU8QPcKVhP8F9LjpZT7vsu1Fa+dQywP4eg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-sankey/0.12.3/d3-sankey.min.js" defer integrity="sha512-KK15oKpabNDaLpWinMtNfTqy/V7pzlc2FRG174PfASes7RRx6TAsua8HJdRTKo8+BLvPBKNIkL7kXWcz5HoqqA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Condensed:300,400|IBM+Plex+Sans:400,600&display=swap" rel="stylesheet" crossorigin="anonymous" />`
-      }/>
+<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Condensed:300,400|IBM+Plex+Sans:400,600&display=swap" rel="stylesheet" crossorigin="anonymous" />`}/>
 
     <h3>Styles &amp; Fonts</h3>
     <p>
       Carbon Charts requires a minimum of one stylesheet:
       <CodeSnippet code="import '@carbon/charts/styles.css'" />
-      If you are using toolbars with your charts, you also have to include <strong>@carbon/styles</strong>:
+      If you are using toolbars with your charts, you also have to include <strong>@carbon/styles</strong>.
       <CodeSnippet code="import '@carbon/styles/css/styles.css'" />
+      Using the CSS version of <strong>@carbon/styles</strong> will apply styles to the html body that may interfere with other global styling
+      schemes such as for Bootstrap and Material Design. A way around this is to use SCSS and set a variable before <strong>@carbon/styles</strong> is loaded...
+      <CodeSnippet code="$css--body: false; // Do not emit styles for body element" /><br/>
+      More information on using <strong>@carbon/styles</strong> with SCSS can be found <a href="https://github.com/carbon-design-system/carbon/blob/HEAD/packages/styles/docs/sass.md#files" target="_blank">here</a>.
+
       The default font family for Carbon Charts is <a href="https://fonts.google.com/specimen/IBM+Plex+Sans" target="_blank">IBM Plex Sans</a>. If you prefer
-      a different font, you will either have to override the font-family for 12 CSS classes or you can use SCSS and set a single variable. 
+      a different font, you can override two custom CSS properties for the CSS class that is automatically associated with the chart container like this...
+      <CodeSnippet type="multi" code={
+`.cds--cc--chart-wrapper {
+  --cds-charts-font-family: Roboto;
+  --cds-charts-font-family-condensed: 'Roboto Condensed';
+}`}/>
     </p>
 
     </TabContent>
@@ -58,8 +67,6 @@
     <TabContent>React content</TabContent>
     <TabContent>Vue content</TabContent>
     <TabContent>Angular content</TabContent>
-
-
 
   </svelte:fragment>
 </Tabs>
