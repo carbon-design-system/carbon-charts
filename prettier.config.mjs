dfa3fa5dd6c634b73dff195e81bdddfdcf5a0e7c
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-08-05 20:12:54 -0400

    chore(repo): Bump storybook, angular, svelte, sveltekit, carbon-components and several devDependencies within semver (#1640)
    * chore(repo): bump storybook, angular, svelte, sveltekit, carbon-components, @vitejs/plugin-react, sass, vite, @typescript*, add prettier-plugin-svelte to top-level config (drop one in svelte package)
    
    * chore(app): bump angular, storybook, eslint-config-prettier, prettier, cds, vite-plugin-dts
    
    * chore(app): bump eslint-config-prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,12 +3,20 @@ import carbonPrettierSettings from 'prettier-config-carbon'
 export default {
 	$schema: 'https://json.schemastore.org/prettierrc',
 	...carbonPrettierSettings, // Carbon prettier plus overrides...
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte'
+			}
+		}
+	],
 	arrowParens: 'avoid',
 	semi: false,
 	tabWidth: 2,
 	singleQuote: true,
 	printWidth: 100,
 	trailingComma: 'none',
-	useTabs: true,
-	pluginSearchDirs: ['.'] // Find plugins in node_modules
+	useTabs: true
 }
