09577a28df53dc6aa1c02d6edeae6630f1a332f6
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-08-11 15:00:26 -0400

    fix(umd): bundle creation

--- packages/core/vite.umd.config.ts
+++ packages/core/vite.umd.config.ts
@@ -1,8 +1,22 @@
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
 	build: {
+		rollupOptions: {
+			external: ['warning'],
+			output: {
+        globals: {
+          'warning': 'Warning' // assuming 'warning' exposes a global `Warning` when used outside a module system
+        }
+      },
+			plugins: [
+				replace({
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        })
+			]
+		},
 		outDir: 'dist/umd',
 		sourcemap: true,
 		chunkSizeWarningLimit: 600,
