b44cad8a559f9d50eea713fdd4cc9d3e813b5cf6
Nate Stuyvesant <nstuyvesant@gmail.com>
2023-12-18 11:28:50 -0500

    chore(react): minor cleanup

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -25,11 +25,12 @@ const config: StorybookConfig = {
 	typescript: {
 		reactDocgen: false // Required to overcome https://github.com/storybookjs/storybook/issues/25247
 	},
-	async viteFinal(config: InlineConfig) {
+	async viteFinal(config) {
 		config.plugins = config.plugins!.filter((plugin) => plugin!.name !== 'vite:dts')
 		const newConfig: InlineConfig = mergeConfig(config, {
 			build: {
 				chunkSizeWarningLimit: 1800,
+				// Leave commented properties here until Storybook has a solution for this non-blocking error
 				// rollupOptions: {
 					// Avoid error Failed to load url /sb-preview/runtime.js (resolved id: /sb-preview/runtime.js). Does the file exist?
           // external: [
