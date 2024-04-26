33bbe13d56f9375329ed1ba6fbb61931d68e7741
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-04-25 21:54:05 -0400

    feat(diagrams): build

--- packages/docs/src/routes/Diagram.tsx
+++ packages/docs/src/routes/Diagram.tsx
@@ -4,13 +4,31 @@ import {
 	StructuredListRow,
 	StructuredListCell,
 	StructuredListBody,
-	CodeSnippet
+	CodeSnippet,
+	Tag
 } from '@carbon/react'
+import {
+	CardNode,
+	CardNodeColumn,
+	CardNodeTitle,
+	CardNodeSubtitle,
+	CardNodeLabel,
+	ArrowLeftMarker,
+	ArrowRightMarker,
+	CircleMarker,
+	DiamondMarker,
+	Edge,
+	SquareMarker,
+	TeeMarker,
+	ShapeNode
+} from '@carbon/charts-react'
+import { User } from '@carbon/icons-react'
 import PageHeader from '../components/PageHeader'
 import StackBlitzLauncher from '../components/StackBlitzLauncher'
 import StackBlitzLauncherExplanation from '../components/StackBlitzLauncherExplanation'
 import DiagramSimpleStatic from '../components/DiagramSimpleStatic'
 import '@carbon/charts-react/styles.css'
+import './Diagram.scss'
 
 export default function Diagram() {
 	return (
@@ -18,8 +36,9 @@ export default function Diagram() {
 			<PageHeader title="Diagrams" />
 
 			<p>
-				Carbon Charts includes a number of components for React and Angular that enable you to
-				create diagrams using the Carbon Design System styling.
+				Carbon Charts Diagrams for <Tag type="blue">React</Tag> and{' '}
+				<Tag type="magenta">Angular</Tag> includes components that enable you to create diagrams
+				using the Carbon Design System styling.
 			</p>
 
 			<p>These include:</p>
@@ -27,62 +46,244 @@ export default function Diagram() {
 				<StructuredListHead>
 					<StructuredListRow head>
 						<StructuredListCell head>Component</StructuredListCell>
-						<StructuredListCell head>Purpose</StructuredListCell>
+						<StructuredListCell head>Details</StructuredListCell>
 					</StructuredListRow>
 				</StructuredListHead>
 				<StructuredListBody>
 					<StructuredListRow>
-						<StructuredListCell noWrap>CardNode</StructuredListCell>
-						<StructuredListCell>A rectangular container that typically contains an icon, title and subtitle. Properties: tag ('div' | 'a' | 'button'), href (only for tag = 'a'), stacked, color and position.</StructuredListCell>
-					</StructuredListRow>
-					<StructuredListRow>
-						<StructuredListCell noWrap>CardNodeColumn</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">CardNode</Tag> <Tag type="magenta">ibm-diagram-card-node</Tag>
+						</StructuredListCell>
 						<StructuredListCell>
-							Used to divide a CardNode typically to place an icon on the left and title and subtitle on the right. Rendered as a DIV. Properties: farsideColumn.
+							A rectangular container that typically contains an icon, title and subtitle.
+							Properties: tag ('div' | 'a' | 'button'), href (only for tag = 'a'), stacked, color
+							and position. This example uses CardNode and its child components: CardNodeColumn,
+							CardNodeTitle, CardNodeSubtitle and CardNodeLabel.
+							<div className="example">
+								<CardNode onClick={() => console.log('CardNode was clicked')}>
+									<CardNodeColumn>
+										<User size={20} />
+									</CardNodeColumn>
+									<CardNodeColumn>
+										<CardNodeTitle>Title</CardNodeTitle>
+										<CardNodeSubtitle>Description</CardNodeSubtitle>
+										<CardNodeLabel>Label</CardNodeLabel>
+									</CardNodeColumn>
+								</CardNode>
+							</div>
 						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>CardNodeTitle</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">CardNodeColumn</Tag>{' '}
+							<Tag type="magenta">ibm-diagram-card-node-column</Tag>
+						</StructuredListCell>
 						<StructuredListCell>
-							DIV containing the title for a CardNode typically displayed in the right CardNodeColumn typically displayed with a font-weight of 700.
+							Used to divide a CardNode typically to place an icon on the left and title and
+							subtitle on the right. Rendered as a DIV. Properties: farsideColumn.
 						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>CardNodeSubtitle</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">CardNodeTitle</Tag>{' '}
+							<Tag type="magenta">ibm-diagram-card-node-title</Tag>
+						</StructuredListCell>
 						<StructuredListCell>
-							DIV containing the subtitle for a CardNode typically displayed in the right CardNodeColumn below the CardNodeTitle.
+							DIV containing the title for a CardNode typically displayed in the right
+							CardNodeColumn typically displayed with a font-weight of 700.
 						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>CardNodeLabel</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">CardNodeSubtitle</Tag>{' '}
+							<Tag type="magenta">ibm-diagram-card-node-subtitle</Tag>
+						</StructuredListCell>
 						<StructuredListCell>
-							LABEL tag optionally used within a CardNode that is typically positioned below the CardNodeTitle and CardNodeSubtitle using a muted color.
+							DIV containing the subtitle for a CardNode typically displayed in the right
+							CardNodeColumn below the CardNodeTitle.
 						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>Marker</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">CardNodeLabel</Tag>{' '}
+							<Tag type="magenta">ibm-diagram-card-node-label</Tag>
+						</StructuredListCell>
 						<StructuredListCell>
-							Shape to attach to the start and/or end of an Edge. Renders as an SVG marker tag. Properties: id, d (SVG path data), color, position ('start' | 'end'), orient, height, width, refX, refY. See <a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/marker" target="_blank">MDN documentation</a> for more details about the properties.
+							LABEL tag optionally used within a CardNode that is typically positioned below the
+							CardNodeTitle and CardNodeSubtitle using a muted color.
 						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>Edge</StructuredListCell>
-						<StructuredListCell>An SVG group (g) element representing a connecting line typically between CardNodes. Properties: color, markerStart (name of marker style), markerEnd, source (Coordinates), target (Coordinates), path (SVG path) and variant ( 'dash-sm' | 'dash-md' | 'dash-lg' | 'dash-xl' | 'double' | 'tunnel').</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">Marker</Tag> <Tag type="magenta">ibm-graph-marker</Tag>
+						</StructuredListCell>
+						<StructuredListCell>
+							Shape to attach to the start and/or end of an Edge. Renders as an SVG marker tag.
+							Properties: id, d (SVG path data), color, position ('start' | 'end'), orient, height,
+							width, refX, refY. See{' '}
+							<a
+								href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/marker"
+								target="_blank">
+								MDN documentation
+							</a>{' '}
+							for more details about the properties. Examples of Markers used with variants of the
+							Edge component:
+							<div className="example">
+								<svg height={30} width={200}>
+									<defs>
+										<ArrowLeftMarker id="marker" height={8} width={7.5} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										variant={'dash-sm'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<ArrowRightMarker id="marker" height={8} width={7.5} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerEnd={'marker'}
+										variant={'dash-md'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<CircleMarker id="marker" height={10} width={10} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										variant={'dash-lg'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<DiamondMarker id="marker" height={10} width={10} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										variant={'dash-xl'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<SquareMarker id="marker" height={10} width={10} color={'green'} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										variant={'double'}
+										color={'green'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<TeeMarker id="marker" height={10} width={10} color={'blue'} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										color={'blue'}
+									/>
+								</svg>
+								<br />
+
+								<svg height={30} width={200}>
+									<defs>
+										<TeeMarker id="marker" color={'#FB4B53'} height={10} width={10} />
+									</defs>
+
+									<Edge
+										source={{ x: 10, y: 15 }}
+										target={{ x: 190, y: 15 }}
+										markerStart={'marker'}
+										color={'#FB4B53'}
+										variant={'tunnel'}
+									/>
+								</svg>
+							</div>
+						</StructuredListCell>
 					</StructuredListRow>
 					<StructuredListRow>
-						<StructuredListCell noWrap>ShapeNode</StructuredListCell>
-						<StructuredListCell>A shape such as a circle, square or rounded square rendered as a DIV, A or BUTTON typically containing an icon. Properties: tag ('a', 'button', 'div'), title, shape ('circle', 'square', 'rounded-square'), renderIcon, description, size, stacked, position ('fixed'), bodyPosition ('absolute').</StructuredListCell>
+						<StructuredListCell noWrap>
+							<Tag type="blue">Edge</Tag> <Tag type="magenta">ibm-graph-edge</Tag>
+						</StructuredListCell>
+						<StructuredListCell>
+							An SVG group (g) element representing a connecting line typically between CardNodes.
+							Properties: color, markerStart (name of marker style), markerEnd, source
+							(Coordinates), target (Coordinates), path (SVG path) and variant ( 'dash-sm' |
+							'dash-md' | 'dash-lg' | 'dash-xl' | 'double' | 'tunnel').
+						</StructuredListCell>
+					</StructuredListRow>
+					<StructuredListRow>
+						<StructuredListCell noWrap>
+							<Tag type="blue">ShapeNode</Tag> <Tag type="magenta">ibm-diagram-shape-node</Tag>
+						</StructuredListCell>
+						<StructuredListCell>
+							A shape such as a circle, square or rounded square rendered as a DIV, A or BUTTON
+							typically containing an icon. Properties: tag ('a', 'button', 'div'), title, shape
+							('circle', 'square', 'rounded-square'), renderIcon, description, size, stacked,
+							position ('fixed'), bodyPosition ('absolute'). Examples:
+							<br />
+							<ShapeNode title={'Title'} renderIcon={<User />} position={'relative'} />
+							<br />
+							<br />
+							<ShapeNode
+								title={'Title'}
+								renderIcon={<User />}
+								position={'relative'}
+								shape="rounded-square"
+							/>
+							<br />
+							<br />
+							<ShapeNode
+								title={'Title'}
+								renderIcon={<User />}
+								position={'relative'}
+								shape="square"
+							/>
+						</StructuredListCell>
 					</StructuredListRow>
 				</StructuredListBody>
 			</StructuredListWrapper>
 
-			<h3>1. A simple composed diagram, using statically defined x and y coordinates</h3>
+			<h3>Putting it all together</h3>
 
-			<DiagramSimpleStatic />
-
-			<StackBlitzLauncherExplanation />
-
-			<h2>Examples for supported frameworks</h2>
+			<iframe
+				src="https://codesandbox.io/embed/carbon-charts-react-elkjs-diagram-b9xyp?fontsize=14&hidenavigation=1&theme=dark&view=preview"
+				style={{
+					width: '100%',
+					height: '47rem',
+					marginTop: '1em',
+					border: 0,
+					borderRadius: '.25rem'
+				}}></iframe>
 		</>
 	)
 }
