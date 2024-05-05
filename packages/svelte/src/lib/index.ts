60e10bf829b0d906e1ba2b9e667cae485447afba
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-05 13:15:30 -0400

    chore(types): provide all enums instead of just a limited set

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -53,8 +53,60 @@ export type {
 	WordCloudChartOptions
 } from '@carbon/charts'
 
-// Commonly-used enums
-export { Alignments, ChartTheme, ScaleTypes } from '@carbon/charts'
+// All enums
+export {
+	Alignments,
+	AreaEvent,
+	ArrowDirections,
+	AxisEvent,
+	AxisFlavor,
+	AxisPositions,
+	AxisTitleOrientations,
+	BarEvent,
+	BoxplotEvent,
+	CalloutDirections,
+	CanvasZoomEvent,
+	CartesianOrientations,
+	ChartEvent,
+	ChartTheme,
+	ChartTypes,
+	ColorClassNameTypes,
+	ColorLegendType,
+	DividerStatus,
+	DominantBaseline,
+	GaugeEvent,
+	GaugeTypes,
+	LayoutAlignItems,
+	LayoutDirection,
+	LayoutGrowth,
+	LegendItemType,
+	LegendOrientations,
+	LegendPositions,
+	LineEvent,
+	ModalEvent,
+	ModelEvent,
+	PieEvent,
+	Projection,
+	RadarEvent,
+	RenderTypes,
+	ScaleTypes,
+	ScatterEvent,
+	Skeletons,
+	Statuses,
+	TextAnchor,
+	ThresholdEvent,
+	TickRotations,
+	ToolbarControlTypes,
+	TooltipEvent,
+	TruncationTypes,
+	TreeEvent,
+	TreemapEvent,
+	TreeTypes,
+	WordCloudEvent,
+	ZoombarEvent,
+	ZoomDomainEvent,
+	ZoomBarTypes
+} from '@carbon/charts'
 
 export {
 	AlluvialChart,
