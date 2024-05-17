6299af81eb23c5f21ebb6b561432184be3ca43b7
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-16 22:31:13 -0400

    docs(jsdocs): add

--- packages/core/src/interfaces/layout.ts
+++ packages/core/src/interfaces/layout.ts
@@ -1,23 +1,51 @@
 import type { LayoutAlignItems, LayoutDirection, RenderTypes } from './enums'
 
+/**
+ * Configuration for layout settings.
+ */
 export interface LayoutConfigs {
 	/**
-	 * Direction/orientation of the layout
+	 * Direction/orientation of the layout.
+	 * @type {LayoutDirection | string}
 	 */
 	direction?: LayoutDirection | string
 	/**
-	 * Whether to render through SVG or HTML
+	 * Whether to render through SVG or HTML.
+	 * @type {RenderTypes | string}
 	 */
 	renderType?: RenderTypes | string
 	/**
-	 * how the layout will align its children
+	 * How the layout will align its children.
+	 * @type {LayoutAlignItems | string}
 	 */
 	alignItems?: LayoutAlignItems | string
 }
 
+/**
+ * Interface representing coordinates in a 2D space.
+ */
 export interface Coordinates {
+	/**
+	 * The x-coordinate.
+	 * @type {number}
+	 */
 	x: number
+
+	/**
+	 * The y-coordinate.
+	 * @type {number}
+	 */
 	y: number
+
+	/**
+	 * The optional initial x-coordinate.
+	 * @type {number}
+	 */
 	x0?: number
+
+	/**
+	 * The optional initial y-coordinate.
+	 * @type {number}
+	 */
 	y0?: number
 }
