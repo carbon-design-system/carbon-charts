8e29c950fc4910446f49d354bad74e5d0f139554
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-18 10:51:15 -0400

    chore(eslint): move angular eslint from deprecated format to mjs

---
+++ packages/angular/eslint.config.mjs
@@ -0,0 +1,45 @@
+export default [
+	{
+		root: true,
+		ignorePatterns: ['!**/*'],
+		overrides: [
+			{
+				files: ['*.ts'],
+				extends: [
+					'eslint:recommended',
+					'plugin:@typescript-eslint/recommended',
+					'plugin:@angular-eslint/recommended',
+					'plugin:@angular-eslint/template/process-inline-templates',
+					'plugin:prettier/recommended'
+				],
+				rules: {
+					'@angular-eslint/directive-selector': [
+						'error',
+						{
+							type: 'attribute',
+							prefix: 'lib',
+							style: 'camelCase'
+						}
+					],
+					'@angular-eslint/component-selector': [
+						'error',
+						{
+							type: 'element',
+							prefix: 'ibm',
+							style: 'kebab-case'
+						}
+					]
+				}
+			},
+			{
+				files: ['*.html'],
+				extends: [
+					'plugin:@angular-eslint/template/recommended',
+					'plugin:@angular-eslint/template/accessibility',
+					'plugin:prettier/recommended'
+				],
+				rules: {}
+			}
+		]
+	}
+]
