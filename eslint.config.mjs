6c1ab4b4404b84721a4df64035941f03e006e875
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-18 14:14:16 -0400

    chore(eslint): add jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,14 @@
+import jsdoc from 'eslint-plugin-jsdoc'
+
 export default [
+	{
+		files: ['**/*.js', '**/*.ts'],
+		plugins: {
+			jsdoc
+		},
+		rules: {
+			'jsdoc/require-description': 'warn',
+			'jsdoc/check-values': 'warn'
+		}
+	}
 ]
