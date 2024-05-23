a3ea152a11dd5241e8a937319d2710e676ef09d8
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-22 23:38:01 -0400

    chore(deps): simplify

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
 	build: {
