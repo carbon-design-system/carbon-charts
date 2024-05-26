2c1160ccd97080458ee95360fa2db544974e14c9
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-26 11:01:37 -0400

    chore(precommit): husky update

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-	'*.{md,json,yml,css,scss,html})': 'prettier --write',
+	'*.{md,json,yml,css,scss,html}': 'prettier --write',
 	'*.{ts,tsx,svelte,vue,mjs,js}': ['eslint --fix', 'prettier --write']
 }
