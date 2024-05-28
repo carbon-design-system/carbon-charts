4e3a479d4d394f25d65682ebb775dfe222550d7d
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-05-28 09:49:10 -0400

    docs(svelte): improve comments

--- packages/svelte/src/lib/BaseChart.svelte
+++ packages/svelte/src/lib/BaseChart.svelte
@@ -3,19 +3,21 @@
 	import { onMount, afterUpdate, onDestroy, createEventDispatcher } from 'svelte'
 	import type { Charts, ChartConfig, BaseChartOptions, ChartTabularData } from '@carbon/charts'
 
-	const chartHolderCssClass = 'cds--chart-holder'
-	export let data: ChartTabularData = []
-	export let options: T = {} as T // AlluvialChartOptions, BarChartOptions, etc.
-	export let Chart: new (holder: HTMLDivElement, chartConfigs: ChartConfig<T>) => Charts
-	export let chart: Charts | undefined // instance of the class passed to the Chart property
-	export let ref: HTMLDivElement // expose to parent so the div can be manipulated, if desired
-	export let id = `chart-${Math.random().toString(36)}` // id for chart holder element
-
 	interface DispatchedEvents {
 		load: null
 		update: { data: ChartTabularData; options: T }
 		destroy: null
 	}
+
+	type CoreChartClass = new (holder: HTMLDivElement, chartConfigs: ChartConfig<T>) => Charts
+
+	const chartHolderCssClass = 'cds--chart-holder' // Used by Carbon Charts CSS
+	export let data: ChartTabularData = [] // Chart data, default is empty
+	export let options: T = {} as T // Specific chart option type, default is empty
+	export let Chart: CoreChartClass // Used to instantiate next property
+	export let chart: Charts | undefined // Instance of the chart class
+	export let ref: HTMLDivElement // Binding to chart 'holder' in template below
+	export let id = `chart-${Math.random().toString(36)}` // id for chart holder element
 
 	const dispatch = createEventDispatcher<DispatchedEvents>()
 
@@ -43,7 +45,7 @@
 	onDestroy(() => {
 		if (chart) {
 			dispatch('destroy')
-			// Almost the same as core's Chart.destroy() but without getting rid of the chart holder
+			// Like core's Chart.destroy() but keeps div chart holder bound to ref as it's part of the template below
 			chart.components.forEach(component => component.destroy())
 			chart.model.set({ destroyed: true }, { skipUpdate: true })
 			chart = undefined
