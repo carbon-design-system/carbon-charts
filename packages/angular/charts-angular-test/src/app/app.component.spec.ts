dd88e8146f6840fca5fd874dd8862d8536ad368b
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-06-02 09:35:24 -0400

    fix(test app angular): minor tweaks

--- packages/angular/charts-angular-test/src/app/app.component.spec.ts
+++ packages/angular/charts-angular-test/src/app/app.component.spec.ts
@@ -14,18 +14,10 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy()
 	})
 
-	it(`should have as title 'charts-angular-test'`, () => {
-		const fixture = TestBed.createComponent(AppComponent)
-		const app = fixture.componentInstance
-		expect(app.title).toEqual('charts-angular-test')
-	})
-
 	it('should render title', () => {
 		const fixture = TestBed.createComponent(AppComponent)
 		fixture.detectChanges()
 		const compiled = fixture.nativeElement as HTMLElement
-		expect(compiled.querySelector('.content span')?.textContent).toContain(
-			'charts-angular-test app is running!'
-		)
+		expect(compiled.querySelector('h1')?.textContent).toContain('Carbon Charts Angular')
 	})
 })
