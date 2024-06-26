229918399c4adecacf35cf7c8e88d981dbd42ef7
Eliad Moosavi <theiliad@users.noreply.github.com>
2024-06-26 11:49:20 -0400

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -10,30 +10,37 @@ set -e
 
 GREEN="\033[0;32m"
 if [ $CONTEXT == "deploy-preview" ]; then
-  # Netlify URL examples: https://carbon-charts-core.netlify.app, https://carbon-charts-react.netlify.app, etc.
-	# The URL drives which test site to 
-	echo -e "${GREEN}Deploying test page to ${URL} on Netlify for pull request..."
+	if [[ "$SITE_NAME" == "carbon-charts-docs" ]]; then
+	  echo -e "Deploying docs site"
 
-	# Grab netlify app type from the netlify app URL
-	# e.g. core, angular or react (translate to ./packages/NAME)
-	PKG_DIR=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
-
-	# Build core package first
-	echo -e "${GREEN}Building @carbon/charts and styles..."
-	npx lerna run build --scope=@carbon/charts --concurrency=1
-
-	# Build package if not core (angular or react) and map APP_TYPE to PKG_DIR
-	if [ "$PKG_DIR" != "core" ]; then
-	  PKG="charts-$PKG_DIR"
-		echo -e "${GREEN}Building @carbon/$PKG..."
-		npx lerna run build --scope="@carbon/$PKG" --concurrency=1
-  else
-	  # Map package directory for core since name is different
-	  PKG="charts"
+		cd ./packages/docs
+		yarn build:netlify-dp
+	else
+	  # Netlify URL examples: https://carbon-charts-core.netlify.app, https://carbon-charts-react.netlify.app, etc.
+		# The URL drives which test site to 
+		echo -e "${GREEN}Deploying test page to ${URL} on Netlify for pull request..."
+	
+		# Grab netlify app type from the netlify app URL
+		# e.g. core, angular or react (translate to ./packages/NAME)
+		PKG_DIR=`echo $URL | sed s/"https:\/\/carbon-charts-"// | sed s/"\..*"//`
+	
+		# Build core package first
+		echo -e "${GREEN}Building @carbon/charts and styles..."
+		npx lerna run build --scope=@carbon/charts --concurrency=1
+	
+		# Build package if not core (angular or react) and map APP_TYPE to PKG_DIR
+		if [ "$PKG_DIR" != "core" ]; then
+		  PKG="charts-$PKG_DIR"
+			echo -e "${GREEN}Building @carbon/$PKG..."
+			npx lerna run build --scope="@carbon/$PKG" --concurrency=1
+	  else
+		  # Map package directory for core since name is different
+		  PKG="charts"
+		fi
+	
+	  echo -e "${GREEN}Creating test page for @carbon/$PKG..."
+		npx lerna run build:test --scope="@carbon/$PKG" --concurrency=1
 	fi
-
-  echo -e "${GREEN}Creating test page for @carbon/$PKG..."
-	npx lerna run build:test --scope="@carbon/$PKG" --concurrency=1
 else
 	echo -e "${GREEN}Not a PR... nothing to do."
 fi
