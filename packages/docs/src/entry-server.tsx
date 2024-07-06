591f4d0c5e9f013d45c2968effb36c55eaafe065
Nate Stuyvesant <nstuyvesant@gmail.com>
2024-07-06 09:48:53 -0400

    fix(ssr): add jsdom

--- packages/docs/src/entry-server.tsx
+++ packages/docs/src/entry-server.tsx
@@ -1,6 +1,14 @@
+import { JSDOM } from 'jsdom'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import App from './main' // Ensure this path points to your main.tsx
+
+const { window } = new JSDOM(`<!DOCTYPE html><html><body></body></html>`)
+global.window = window
+global.document = window.document
+global.navigator = {
+	userAgent: 'node.js'
+}
 
 export function render(url: string) {
 	return renderToString(
