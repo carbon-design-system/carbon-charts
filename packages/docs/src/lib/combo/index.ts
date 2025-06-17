9a5c567ee0b5f8cd8b3c8812d3d2e7882731a518
Eliad Moosavi <theiliad@users.noreply.github.com>
2025-06-17 11:35:23 -0400

    fix(Legend): handling of longer labels in vertical orientation (#2005)
    * fix(Legend): handling of longer labels in vertical orientation
    
    * fix(Legend): add nowrap css

--- packages/docs/src/lib/combo/index.ts
+++ packages/docs/src/lib/combo/index.ts
@@ -377,6 +377,12 @@ const comboStackedAreaLineOptions: ComboChartOptions = {
 			titleOrientation: 'left'
 		}
 	},
+	legend: {
+		position: 'right',
+		truncation: {
+			type: 'none'
+		}
+	},
 	curve: 'curveMonotoneX',
 	comboChartTypes: [
 		{
@@ -386,7 +392,11 @@ const comboStackedAreaLineOptions: ComboChartOptions = {
 					enabled: false
 				}
 			},
-			correspondingDatasets: ['Dataset 1', 'Dataset 2', 'Dataset 3']
+			correspondingDatasets: [
+				'Dataset 1 with a very long name',
+				'Dataset 2 with a very long name',
+				'Dataset 3 with a very long name'
+			]
 		},
 		{
 			type: 'line',
@@ -573,21 +583,21 @@ const comboEmptyData: ChartTabularData = []
 const comboLoadingData = comboLineScatterData
 
 const comboStackedAreaLine: ChartTabularData = [
-	{ group: 'Dataset 1', date: '2023-01-01', value: 10000 },
-	{ group: 'Dataset 1', date: '2023-01-05', value: 65000 },
-	{ group: 'Dataset 1', date: '2023-01-08', value: 10000 },
-	{ group: 'Dataset 1', date: '2023-01-13', value: 49213 },
-	{ group: 'Dataset 1', date: '2023-01-17', value: 51213 },
-	{ group: 'Dataset 2', date: '2023-01-01', value: 20000 },
-	{ group: 'Dataset 2', date: '2023-01-05', value: 25000 },
-	{ group: 'Dataset 2', date: '2023-01-08', value: 60000 },
-	{ group: 'Dataset 2', date: '2023-01-13', value: 30213 },
-	{ group: 'Dataset 2', date: '2023-01-17', value: 55213 },
-	{ group: 'Dataset 3', date: '2023-01-01', value: 30000 },
-	{ group: 'Dataset 3', date: '2023-01-05', value: 20000 },
-	{ group: 'Dataset 3', date: '2023-01-08', value: 40000 },
-	{ group: 'Dataset 3', date: '2023-01-13', value: 60213 },
-	{ group: 'Dataset 3', date: '2023-01-17', value: 25213 },
+	{ group: 'Dataset 1 with a very long name', date: '2023-01-01', value: 10000 },
+	{ group: 'Dataset 1 with a very long name', date: '2023-01-05', value: 65000 },
+	{ group: 'Dataset 1 with a very long name', date: '2023-01-08', value: 10000 },
+	{ group: 'Dataset 1 with a very long name', date: '2023-01-13', value: 49213 },
+	{ group: 'Dataset 1 with a very long name', date: '2023-01-17', value: 51213 },
+	{ group: 'Dataset 2 with a very long name', date: '2023-01-01', value: 20000 },
+	{ group: 'Dataset 2 with a very long name', date: '2023-01-05', value: 25000 },
+	{ group: 'Dataset 2 with a very long name', date: '2023-01-08', value: 60000 },
+	{ group: 'Dataset 2 with a very long name', date: '2023-01-13', value: 30213 },
+	{ group: 'Dataset 2 with a very long name', date: '2023-01-17', value: 55213 },
+	{ group: 'Dataset 3 with a very long name', date: '2023-01-01', value: 30000 },
+	{ group: 'Dataset 3 with a very long name', date: '2023-01-05', value: 20000 },
+	{ group: 'Dataset 3 with a very long name', date: '2023-01-08', value: 40000 },
+	{ group: 'Dataset 3 with a very long name', date: '2023-01-13', value: 60213 },
+	{ group: 'Dataset 3 with a very long name', date: '2023-01-17', value: 25213 },
 	{ group: 'Temperature', date: '2023-01-01', temp: 77 },
 	{ group: 'Temperature', date: '2023-01-05', temp: 65 },
 	{ group: 'Temperature', date: '2023-01-08', temp: 80 },
@@ -654,6 +664,16 @@ export const examples: Example[] = [
 	},
 	{
 		options: comboStackedAreaLineOptions,
+		data: comboStackedAreaLine,
+		tags: ['test']
+	},
+	{
+		options: {
+			...comboStackedAreaLineOptions,
+			legend: {
+				position: 'right'
+			}
+		},
 		data: comboStackedAreaLine,
 		tags: ['test']
 	},
